ccf4af6e87e4383cb744d14e7f1acddff8745aab
GitHubForDmitry <dmitrykoka@gmail.com>
2019-07-09 23:18:01 +0300

    sol

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
 const goodsFromServer = [
   'Dumplings',
@@ -12,11 +13,146 @@ const goodsFromServer = [
   'Jam',
   'Garlic',
 ];
+const getReverse = ({ goods }) => {
+  return [...goods].reverse();
+};
+const getSortAlphabetically = ({ goods }) => {
+  return [...goods].sort();
+};
+const getSortByLength = ({ goods }) => {
+  return [...goods]
+    .sort((a, b) => a.length - b.length)
+    .reverse();
+};
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      goods: [...goodsFromServer],
+      loadGoods: false,
+      isLoading: false,
+      wordLength: 1,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+    loadGoods = async() => {
+      this.setState({
+        isLoading: true,
+      });
+      this.setState(prevState => ({
+        loadGoods: true,
+        isLoading: false,
+      }));
+    };
+
+  handleClickReverse = () => {
+    this.setState(prevState => {
+      return {
+        goods: getReverse(prevState),
+      };
+    });
+  };
+
+  handleClickAlphabetically = () => {
+    this.setState(prevState => {
+      return {
+        goods: getSortAlphabetically(prevState),
+      };
+    });
+  };
+
+  handleReset = () => {
+    this.setState({ goods: [...goodsFromServer], wordLength: 1 });
+  };
+
+  handleSortByLength = () => {
+    this.setState(prevState => {
+      return {
+        goods: getSortByLength(prevState),
+      };
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      wordLength: event.target.value,
+    });
+  };
+
+  render() {
+    const { goods, wordLength } = this.state;
+    const result = goods
+      .filter(good => good.length >= wordLength)
+      .map(good => {
+        return <li key={good.toString()}>{good}</li>;
+      });
+    if (!this.state.loadGoods) {
+      return (
+        <button
+          type="submit"
+          className="btn btn-success mt-5"
+          onClick={this.loadGoods}
+          disabled={this.state.isLoading}
+        >
+          {this.state.isLoading ? 'Loading...' : 'Load' }
+        </button>
+      );
+    }
+    console.log(this.state.goods);
+    return (
+      <div className="goods">
+        <button
+          type="submit"
+          className="btn btn-primary mt-5 mb-5 mr-3"
+          onClick={this.handleClickReverse}>
+          Reverse
+        </button>
+        <button
+          type="submit"
+          className="btn btn-primary mt-5 mb-5 mr-3"
+          onClick={this.handleClickAlphabetically}>
+          Alphabetically
+        </button>
+        <button
+          type="submit"
+          className="btn btn-primary mt-5 mb-5 mr-3"
+          onClick={this.handleReset}>
+          Resset
+        </button>
+        <button
+          type="submit"
+          className="btn btn-primary mt-5 mb-5 mr-3"
+          onClick={this.handleSortByLength}>
+          SortByLength
+        </button>
+        <label className="btn btn-warning">length
+          <select
+            value={this.state.wordLength}
+            onChange={this.handleChange}
+            name="length"
+            className="form-control form-control-sm"
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </label>
+        <h1>Goods 1</h1>
+        <ul className="goods__list">
+          {result}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
