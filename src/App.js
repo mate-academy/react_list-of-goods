bbb29690159a709c94d0377d2e0e674631099825
mgrinko <glukym@gmail.com>
2019-07-09 15:49:52 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const goodsFromServer = [
+  'Dumplings',
+  'Carrot',
+  'Eggs',
+  'Ice cream',
+  'Apple',
+  'Bread',
+  'Fish',
+  'Honey',
+  'Jam',
+  'Garlic',
+];
+
+const App = () => (
+  <div className="App">
+    <h1>Goods 1</h1>
+  </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App test={123} />, document.getElementById('root'));
