b2d7f013cb25e56beed5eba1d979807b01ee307c
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-07-10 09:11:57 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,91 @@
 import React from 'react';
 
-const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+import goodsFromServer from './Api/goods';
+class App extends React.Component {
+  state = {
+    goodsTemplate: [],
+    goodsCurrent: [],
+    isLoaded: false,
+    currentValue: 1,
+  }
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+ handleLoad = () => {
+   this.setState({
+     goodsTemplate: goodsFromServer,
+     goodsCurrent: goodsFromServer,
+     isLoaded: true,
+   });
+ }
+
+handleReverse = () => {
+  this.setState({
+    goodsCurrent: [...this.state.goodsCurrent].reverse(),
+  });
+}
+
+handleSortByAlphabet = () => {
+  this.setState({
+    goodsCurrent: this.state.goodsCurrent.sort((a, b) => a.localeCompare(b)),
+  });
+}
+
+handleSortByLength = () => {
+  this.setState({
+    goodsCurrent: this.state.goodsCurrent.sort((a, b) => a.length - b.length),
+  });
+}
+
+handleReset = () => {
+ this.setState({
+    goodsCurrent: this.state.goodsTemplate,
+    currentValue: 1,
+  });
+}
+
+filterByLength = (e) => {
+  const { value } = e.target
+  this.setState({
+    currentValue: value,
+    goodsCurrent: this.state.goodsCurrent.filter((item) => item.length >= value),
+  });
+}
+
+render() {
+  if (!this.state.isLoaded) {
+    return (
+     <button onClick={this.handleLoad} type="button">Load</button>
+    );
+  }
+  return (
+      <>
+       <button onClick={this.handleReverse}>Reverse</button>
+       <button onClick={this.handleSortByAlphabet}>Sort by 'Alphabet'</button>
+       <button onClick={this.handleSortByLength}>Sort by 'Length'</button>
+       <button onClick={this.handleReset}>Reset</button>
+
+      <select onChange={this.filterByLength} defaultVlue="this.state.currentValue">
+        <option value="1">. 1 .</option>
+        <option value="2">. 2 .</option>
+        <option value="3">. 3 .</option>
+        <option value="4">. 4 .</option>
+        <option value="5">. 5 .</option>
+        <option value="6">. 6 .</option>
+        <option value="7">. 7 .</option>
+        <option value="8">. 8 .</option>
+        <option value="9">. 9 .</option>
+        <option value="10">. 10 .</option>
+      </select>
+
+       <div>
+       <ul>
+         {this.state.goodsCurrent.map(item => (
+           <li>{item}</li>
+         ))}
+       </ul>
+     </div>
+     </>
+  );
+}
+}
 
 export default App;
