ee8293d4ba0a951bf162e51da616df2de3b4f9e3
Пользователь <Sviatoslv>
2019-07-10 11:19:23 +0300

    1-3 parts Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 const goodsFromServer = [
   'Dumplings',
@@ -13,10 +14,61 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+class App extends React.Component {
+  state={
+    isGoodsLoaded: false,
+    goods: [...goodsFromServer],
+  }
+
+  shownClick = () => {
+    this.setState({
+      isGoodsLoaded: true,
+    });
+  }
+
+  reverseClick = () => {
+    this.setState(prevState => ({
+      goods: prevState.goods.reverse(),
+    }));
+  }
+
+  render() {
+    const listOfGoodGenerator = this.state.isGoodsLoaded
+      && this.state.goods.map(item => <li>{item}</li>);
+
+    const showListButton = this.state.isGoodsLoaded
+      || (
+        <button
+          type="button"
+          className="button"
+          onClick={this.shownClick}
+        >
+        Start
+        </button>
+      );
+
+    const reverseButton = this.state.isGoodsLoaded
+      && (
+        <button
+          type="button"
+          className="button button--reverse"
+          onClick={this.reverseClick}
+        >
+        Reverse
+        </button>
+      );
+
+    return (
+      <div className="App">
+        <h1>Goods 1</h1>
+        {reverseButton}
+        {showListButton}
+        <ul className="list">
+          {listOfGoodGenerator}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
