46581d1d0b5e35df2d68c5636fc3a0c1f4755519
Sergii Kasian <kasyansv1@gmail.com>
2019-07-10 20:38:58 +0300

    1-2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 const goodsFromServer = [
   'Dumplings',
@@ -13,10 +14,38 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isStarted: false,
+      goodsCurrent: [],
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      isStarted: true,
+      goodsCurrent: goodsFromServer,
+    });
+  }
+
+  render() {
+    if (this.state.isStarted === false) {
+      return (
+        <button onClick={this.handleClick} type="button">Start</button>
+      );
+    }
+    return (
+
+      <div className="App">
+        <ul>
+          {this.state.goodsCurrent.map(item => (<li>{item}</li>))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
