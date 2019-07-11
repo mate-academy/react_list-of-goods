e573a10218f74d9da70551ac3f2f8c1448eb283a
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-11 14:10:55 +0300

    1-5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import GoodsList from './GoodsList';
+
 const goodsFromServer = [
   'Dumplings',
   'Carrot',
@@ -13,10 +15,66 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    isLoaded: false,
+    goodsList: [],
+    currentList: [],
+    direction: 1,
+    length: 1,
+  }
+
+  goodsLoad = () => {
+    this.setState({
+      isLoaded: true,
+      goodsList: [...goodsFromServer],
+      currentList: [...goodsFromServer],
+    });
+  }
+
+  alphabetSort = () => {
+    this.setState(state => ({
+      direction: state.direction === 1 ? -1 : 1,
+      currentList: [...state.currentList].sort((a, b) => (
+        a.localeCompare(b) * state.direction
+      )),
+    }));
+  }
+
+  reverseSort = () => {
+    this.setState(state => ({
+      direction: state.direction === 1 ? -1 : 1,
+      currentList: state.currentList.reverse(),
+    }));
+  }
+
+  resetSort = () => {
+    this.setState( state => ({
+      currentList: [...state.goodsList],
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Goods 1</h1>
+        {
+          (!this.state.isLoaded)
+            ? <button onClick={this.goodsLoad}>Start</button>
+            : (
+              <>
+                <button onClick={this.alphabetSort}>alpha</button>
+                <button onClick={this.reverseSort}>reverse</button>
+                <button onClick={this.resetSort}>reset</button>
+                <ul>
+                  <GoodsList allGoods={this.state.currentList} />
+                </ul>
+              </>
+            )
+        }
+      </div>
+    )
+  }
+}
 
 export default App;
