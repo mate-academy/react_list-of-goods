ae6f741657909f4d1ec37a95e7138af49ff7b08d
DimaKashyrin <kashyrindima@gmail.com>
2019-07-15 04:33:24 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-
-const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+import GoodsList from './GoodsList';
 
 const App = () => (
   <div className="App">
-    <h1>Goods 1</h1>
+    <GoodsList />
   </div>
 );
 
