8078f327f007d9590adebf77acbe94aa70a531cc
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-09-12 08:22:51 +0300

    Select function

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,34 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Select.scss';
 import { SelectTypes } from '../../constants/proptypes';
 
-export default class Select extends Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Select = (props) => {
+  const { list, selected, onChange } = props;
 
-  handleClick(e) {
-    this.props.onChange(e);
-  }
-
-  render() {
-    const { list, selected } = this.props;
-
-    return (
-      <select className="select" onChange={this.handleClick}>
-        {list.map(item => (
-          <option
-            value={item.val}
-            selected={item.val === selected}
-          >
-            {item.name}
-          </option>
-        ))}
-      </select>
-    );
-  }
-}
+  return (
+    <select className="select" onChange={e => onChange(e)}>
+      {list.map(item => (
+        <option value={item.val} selected={item.val === selected}>
+          {item.name}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 Select.propTypes = SelectTypes;
+
+export default Select;
