fc96b0cd75c4931e0b50b47329d41e7ba4372e35
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-09-15 09:15:37 +0300

    divided structure between App and Goods

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-import reverseGoods from './actions/reverseGoods';
-import sortAlphabetically from './actions/sortAlphabetically';
-import sortByLength from './actions/sortByLength';
-import goodsWithLength from './actions/goodsWithLength';
-import ProductList from './components/ProductList/ProductList';
 import Goods from './components/Goods/Goods';
 
 const goodsFromServer = [
@@ -21,7 +16,6 @@ const goodsFromServer = [
 
 class App extends Component {
   state = {
-    goodsList: goodsWithLength(goodsFromServer),
     isStartClicked: false,
   };
 
@@ -31,103 +25,26 @@ class App extends Component {
     })
   );
 
-  sortListAZ = () => this.setState(
-    prevState => ({
-      goodsList: sortAlphabetically(prevState.goodsList),
-    })
-  );
-
-  sortListByLength = () => this.setState(
-    prevState => ({
-      goodsList: sortByLength(prevState.goodsList),
-    })
-  );
-
-  reverseList = () => this.setState(
-    prevState => ({
-      goodsList: reverseGoods(prevState.goodsList),
-    })
-  );
-
-  resetList = () => this.setState(
-    () => ({
-      goodsList: goodsWithLength(goodsFromServer),
-    })
-  );
-
-  selectNumber = (event) => {
-    const { value } = event.target;
-
-    this.setState(
-      () => ({
-        goodsList: goodsWithLength(goodsFromServer, Number(value)),
-      })
-    );
-  }
-
   render() {
-    const { goodsList, isStartClicked } = this.state;
+    const { isStartClicked } = this.state;
 
-    return isStartClicked
-      ? (
-        <div className="App">
-          <Goods goods={goodsFromServer} />
-          <hr />
-          <h1>List of Goods</h1>
-          <div>
-            <select onChange={this.selectNumber}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-            <button
-              type="submit"
-              className="button"
-              onClick={this.sortListAZ}
-            >
-              Sort A - Z
-            </button>
-            <button
-              type="submit"
-              className="button"
-              onClick={this.sortListByLength}
-            >
-              Sort by length
-            </button>
-            <button
-              type="submit"
-              className="button"
-              onClick={this.reverseList}
-            >
-              Reverse
-            </button>
-            <button
-              type="submit"
-              className="button"
-              onClick={this.resetList}
-            >
-              Reset
-            </button>
-          </div>
-          <ProductList productList={goodsList} />
-        </div>
-      )
-      : (
-        <button
-          type="submit"
-          className="button button--start"
-          onClick={this.changeStartState}
-        >
-          Start
-        </button>
-      );
+    return (
+      <div className="App">
+        {
+          isStartClicked
+            ? <Goods goods={goodsFromServer} />
+            : (
+              <button
+                type="submit"
+                className="button button--start"
+                onClick={this.changeStartState}
+              >
+                Start
+              </button>
+            )
+        }
+      </div>
+    );
   }
 }
 
--- src/components/Goods/Goods.js
+++ src/components/Goods/Goods.js
@@ -1,6 +1,10 @@
-/* eslint-disable no-console */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ProductList from '../ProductList/ProductList';
+import sortAlphabetically from '../../actions/sortAlphabetically';
+import reverseGoods from '../../actions/reverseGoods';
+import sortByLength from '../../actions/sortByLength';
+import goodsWithLength from '../../actions/goodsWithLength';
 
 class Goods extends Component {
   constructor(props) {
@@ -10,10 +14,92 @@ class Goods extends Component {
     };
   }
 
-  render() {
-    console.log(this.state);
+  sortListAZ = () => this.setState(
+    prevState => ({
+      goodsList: sortAlphabetically(prevState.goodsList),
+    })
+  );
 
-    return <h1>Hello</h1>;
+  sortListByLength = () => this.setState(
+    prevState => ({
+      goodsList: sortByLength(prevState.goodsList),
+    })
+  );
+
+  reverseList = () => this.setState(
+    prevState => ({
+      goodsList: reverseGoods(prevState.goodsList),
+    })
+  );
+
+  resetList = () => this.setState(
+    () => ({
+      goodsList: [...this.props.goods],
+    })
+  );
+
+  selectNumber = (event) => {
+    const { value } = event.target;
+
+    this.setState(
+      () => ({
+        goodsList: goodsWithLength([...this.props.goods], Number(value)),
+      })
+    );
+  }
+
+  render() {
+    const { goodsList } = this.state;
+
+    return (
+      <div className="App">
+        <hr />
+        <h1>List of Goods</h1>
+        <div>
+          <select onChange={this.selectNumber}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+          <button
+            type="submit"
+            className="button"
+            onClick={this.sortListAZ}
+          >
+            Sort A - Z
+          </button>
+          <button
+            type="submit"
+            className="button"
+            onClick={this.sortListByLength}
+          >
+            Sort by length
+          </button>
+          <button
+            type="submit"
+            className="button"
+            onClick={this.reverseList}
+          >
+            Reverse
+          </button>
+          <button
+            type="submit"
+            className="button"
+            onClick={this.resetList}
+          >
+            Reset
+          </button>
+        </div>
+        <ProductList productList={goodsList} />
+      </div>
+    );
   }
 }
 
