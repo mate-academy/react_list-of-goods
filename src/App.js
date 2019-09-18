2e0d141b1e7a3a7c22ac40ab26915774602ec565
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-18 12:17:39 +0300

    fix comments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import GoodsList from './components/GoodsList/GoodsList';
-import FiltersList from './components/FiltersList/FiltersList';
+import ListOfGoods from './components/ListOfGoods';
 import './App.css';
-
-const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
 
 class App extends React.Component {
   state = {
-    goods: [...goodsFromServer],
-    originalGoods: [...goodsFromServer],
-    selectedOption: null,
     isHidden: true,
   }
 
@@ -30,47 +13,16 @@ class App extends React.Component {
     }));
   }
 
-  handleClickReverse = () => {
-    this.setState(prevState => ({
-      goods: [...prevState.goods].reverse(),
-    }));
-  }
-
-  handleClickSortABC = () => {
-    this.setState(prevState => ({
-      goods: [...prevState.goods].sort(),
-    }));
-  }
-
-  handleClickReset = () => {
-    this.setState(prevState => ({
-      goods: prevState.originalGoods,
-    }));
-  }
-
-  handleClickSortLength = () => {
-    this.setState(prevState => ({
-      goods: [...prevState.goods]
-        .sort((a, b) => a.length - b.length),
-    }));
-  }
-
-  handleSelectChange = (selectedOption) => {
-    this.setState(prevState => ({
-      selectedOption,
-      goods: [...prevState.originalGoods]
-        .filter(item => item.length >= selectedOption.value),
-    }));
-  }
-
   render() {
+    const { isHidden } = this.state;
+
     return (
       <div className="App">
         <button
           type="button"
           onClick={this.handleStart}
           className={
-            this.state.isHidden
+            isHidden
               ? 'button-active'
               : 'button-hidden'
           }
@@ -80,31 +32,12 @@ class App extends React.Component {
 
         <div
           className={
-            this.state.isHidden
+            isHidden
               ? 'content-hidden'
               : 'content-show'
           }
         >
-          <div className="filters">
-            <FiltersList
-              reverse={this.handleClickReverse}
-              sortABC={this.handleClickSortABC}
-              reset={this.handleClickReset}
-              sortLength={this.handleClickSortLength}
-              selectLength={this.handleSelectChange}
-              selectedOption={this.state.selectedOption}
-            />
-          </div>
-
-          <div className="goods">
-            {this.state.goods
-              .map(good => (
-                <GoodsList
-                  key={good}
-                  good={good}
-                />
-              ))}
-          </div>
+          <ListOfGoods />
         </div>
       </div>
     );
---
+++ src/components/ListOfGoods.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import GoodsList from './GoodsList/GoodsList';
+import FiltersList from './FiltersList/FiltersList';
+
+const goodsFromServer = [
+  'Dumplings',
+  'Carrot',
+  'Eggs',
+  'Ice cream',
+  'Apple',
+  'Bread',
+  'Fish',
+  'Honey',
+  'Jam',
+  'Garlic',
+];
+
+class ListOfGoods extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+    originalGoods: [...goodsFromServer],
+    selectedOption: null,
+  }
+
+  handleClickReverse = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].reverse(),
+    }));
+  }
+
+  handleClickSortABC = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort(),
+    }));
+  }
+
+  handleClickReset = () => {
+    this.setState(prevState => ({
+      goods: prevState.originalGoods,
+    }));
+  }
+
+  handleClickSortLength = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods]
+        .sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  handleSelectChange = (selectedOption) => {
+    this.setState(prevState => ({
+      selectedOption,
+      goods: [...prevState.originalGoods]
+        .filter(item => item.length >= selectedOption.value),
+    }));
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <div className="filters">
+          <FiltersList
+            reverse={this.handleClickReverse}
+            sortABC={this.handleClickSortABC}
+            reset={this.handleClickReset}
+            sortLength={this.handleClickSortLength}
+            selectLength={this.handleSelectChange}
+            selectedOption={this.state.selectedOption}
+          />
+        </div>
+
+        <div className="goods">
+          {goods
+            .map(good => (
+              <GoodsList
+                key={good}
+                good={good}
+              />
+            ))}
+        </div>
+      < />
+    );
+  }
+}
+
+export default ListOfGoods;
