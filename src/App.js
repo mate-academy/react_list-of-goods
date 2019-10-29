a4be813099d3cfafed09f31649856f4224d07410
VadimK <djvadya16@gmail.com>
2019-10-29 19:33:37 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import GoodsList from './components/goodlist/GoodList';
+import { Button } from 'semantic-ui-react';
 
 const goodsFromServer = [
   'Dumplings',
@@ -13,10 +15,32 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showGoodList: false,
+    }
+  }
+
+  showGoodList = () =>
+    this.setState((prev) => prev.showGoodList = true);
+
+
+  render() {
+
+    return (
+      <div>
+        {
+          this.state.showGoodList
+          ? <GoodsList goods={goodsFromServer} />
+          : <Button inverted color='pink' type='button' onClick={this.showGoodList}>Start</Button>
+        }
+      </div>
+    )
+  }
+}
 
 export default App;
