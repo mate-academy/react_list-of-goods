b4bb483923153271a8c824ffb6d683800084878e
Anna Kazantseva <annetastara@gmail.com>
2019-10-30 00:00:18 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ListOfGoods from './components/ListOfGoods/ListOfGoods';
+import './app.css';
+import { Button } from 'semantic-ui-react';
 
 const goodsFromServer = [
   'Dumplings',
@@ -13,10 +16,77 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    list: [],
+    loaded: false,
+    visibleList: [],
+  };
+
+  handler = () => {
+    this.setState({
+      list: [...goodsFromServer],
+      loaded: true,
+      visibleList: [...goodsFromServer],
+    });
+  };
+
+  reverseList = () => {
+    this.setState(prevState => ({
+      visibleList: [...prevState.visibleList].reverse(),
+    }));
+  };
+
+  sortAlphabetical = () => {
+    this.setState(prevState => ({
+      visibleList: [...prevState.visibleList].sort(),
+    }));
+  };
+
+  sortListLength = () => {
+    this.setState(prevState => ({
+      visibleList: [...prevState.visibleList].sort((a, b) => a.length - b.length)
+    }));
+  };
+
+  resetList = () => {
+    this.setState(prevState => ({
+      visibleList: [...prevState.list],
+    }));
+  };
+
+  render() {
+    return (
+      <div className="goods_block">
+        {
+          (!this.state.loaded ? (
+            <button onClick={this.handler} class="ui purple inverted button">
+                Start
+            </button>
+          ) : (
+            <>
+              <div className="buttons">
+                <button onClick={this.reverseList} class="ui purple inverted button">
+                    Reverse
+                </button>
+                <button onClick={this.sortAlphabetical} class="ui purple inverted button">
+                    Sort A-Z
+                </button>
+                <button onClick={this.sortListLength} class="ui purple inverted button">
+                    Sort by length
+                </button>
+                <button onClick={this.resetList} class="ui purple inverted button">
+                    Reset
+                </button>
+              </div>
+              <ListOfGoods list={this.state.visibleList} />
+              </>
+          )
+          )
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
