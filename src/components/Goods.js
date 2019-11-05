b8a6fc53b0568ba34eaa08c5a5fae8eaeab8ff0c
Veronika D <donets.veronika@yahoo.com>
2019-11-06 01:27:57 +0200

    fixed

--- src/components/Goods.js
+++ src/components/Goods.js
@@ -9,6 +9,7 @@ function Goods(props) {
     showByLength,
     goods,
     selectedOption,
+    initialValue,
   } = props;
 
   return (
@@ -26,16 +27,8 @@ function Goods(props) {
         Sort by length
       </button>
       <select onClick={showByLength} value={selectedOption} className="ui basic button green dropdown">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
+        {initialValue.map((item, index) => (
+          <option key={index + 1} value={index + 1}>{index + 1}</option>))}
       </select>
       <ol className="ui list">
         {goods.map(item => <li value="-" key={item}>{item}</li>)}
