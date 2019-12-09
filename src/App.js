b14de6324dcc072fe4ca1a88277760eab62f29a0
Inna <inna.tereshchenko1@gmail.com>
2019-12-09 16:31:28 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GoodsList from './GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -13,10 +14,89 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    isStarted: false,
+    visibleGoods: [...goodsFromServer],
+  };
+
+  start = () => {
+    this.setState({ isStarted: true });
+  };
+
+  reset = () => {
+    this.setState({ visibleGoods: [...goodsFromServer] });
+  };
+
+  reverse = () => {
+    this.setState(
+      state => ({ visibleGoods: [...state.visibleGoods].reverse() })
+    );
+  };
+
+  sortByName = () => {
+    this.setState(
+      state => ({
+        visibleGoods: [...state.visibleGoods]
+          .sort((a, b) => a.localeCompare(b)),
+      }),
+    );
+  };
+
+  sortByLength = () => {
+    this.setState(
+      state => ({
+        visibleGoods: [...state.visibleGoods]
+          .sort((a, b) => a.length - b.length),
+      }),
+    );
+  };
+
+  selectOptions = () => {
+    const arr = [];
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 1; i < 11; i++) {
+      arr.push(<option value={i}>{i}</option>);
+    }
+
+    return arr;
+  };
+
+  render() {
+    const { isStarted, visibleGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>List of Goods</h1>
+        {!isStarted
+          ? <button type="button" onClick={this.start}>Start</button>
+          : (
+            <section>
+              <button type="button" onClick={this.reset}>Reset</button>
+              <button type="button" onClick={this.reverse}>Reverse</button>
+              <button
+                type="button"
+                onClick={this.sortByName}
+              >
+                Sort by name
+              </button>
+              <button
+                type="button"
+                onClick={this.sortByLength}
+              >
+                Sort by length
+              </button>
+              <select>
+                {this.selectOptions()}
+              </select>
+              <GoodsList goods={visibleGoods} />
+            </section>
+          )
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
