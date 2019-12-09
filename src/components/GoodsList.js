250695c50ec75aa3279e0ebd4b62f7958a2fa146
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-09 19:33:27 +0200

    Aidinov Alexii

---
+++ src/components/GoodsList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GoodsList = ({ goodsList }) => (
+  <ul className="goods__list">
+    {goodsList.map(item => (
+      <li
+        key={item}
+        className="goods__item"
+      >
+        {item}
+      </li>
+    ))}
+  </ul>
+);
+
+GoodsList.propTypes = {
+  goodsList: PropTypes.arrayOf(
+    PropTypes.string
+  ).isRequired,
+};
+
+export default GoodsList;
