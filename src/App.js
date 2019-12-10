8fa4c755148feebd8a3121a34bcd408f55a6e88d
art1shok <kvitkaart554@gmail.com>
2019-12-10 14:08:36 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const goodsFromServer = [
   'Dumplings',
@@ -13,10 +14,119 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
+class App extends React.Component {
+  state = {
+    isStart: false,
+    goods: [...goodsFromServer],
+    index: '',
+  };
+
+  start = () => {
+    this.setState(
+      { isStart: true }
+    );
+  };
+
+  sort = () => {
+    this.setState(state => (
+      { goods: [...state.goods].sort((a, b) => a.length - b.length) }
+    ));
+  };
+
+  sortAlphabetically = () => {
+    this.setState(state => ({
+      goods: [...state.goods]
+        .sort((a, b) => a.localeCompare(b)),
+    }));
+  };
+
+  reverse = () => {
+    this.setState(state => ({ goods: [...state.goods].reverse() }));
+  };
+
+  reset = () => {
+    this.setState({
+      goods: [...goodsFromServer],
+      index: '1',
+    });
+  };
+
+  filter = (event) => {
+    this.setState({ index: event.target.value });
+    this.setState(prev => (
+      {
+        goods: goodsFromServer
+          .filter(item => item.length >= prev.index),
+      }
+    ));
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="h1">Goods</h1>
+        <div className="buttons">
+          {this.state.isStart ? (
+            <section>
+              <select
+                value={this.state.index}
+                onChange={this.filter}
+                className="select"
+              >
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => (
+                  <option value={i}>{i}</option>
+                ))}
+              </select>
+              <button
+                type="button"
+                onClick={this.reverse}
+              >
+                Reverse
+              </button>
+              <button
+                type="button"
+                onClick={this.sort}
+              >
+                Sort
+              </button>
+              <button
+                type="button"
+                onClick={this.sortAlphabetically}
+              >
+                Sort-abc
+              </button>
+              <button
+                type="button"
+                onClick={this.reset}
+              >
+                Reset
+              </button>
+              <GoodsList goods={goods} />
+            </section>
+          )
+            : (
+              <button
+                className="start"
+                type="button"
+                onClick={this.start}
+              >
+                Start
+              </button>
+            )}
+        </div>
+      </div>
+    );
+  }
+}
+
+const GoodsList = props => (
+  <ul className="ul">
+    {props.goods.map(item => <li className="li">{item}</li>)}
+  </ul>
 );
+
+GoodsList.propTypes = { goods: PropTypes.arrayOf(PropTypes.string).isRequired };
 
 export default App;
