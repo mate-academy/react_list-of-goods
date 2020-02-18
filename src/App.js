9576cffdb1037a2910ba38d64ff5fec1f1035039
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-02-18 16:45:58 +0200

    My solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './App.scss';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +16,106 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends Component {
+  state = {
+    isLoaded: false,
+    goodsList: [...goodsFromServer],
+    isSorted: true,
+    originalGoods: [...goodsFromServer],
+    minLength: 1,
+  }
 
-export default App;
+  handleStart = () => {
+    this.setState({
+      isLoaded: true,
+    });
+  }
+
+  reverseList = () => {
+    this.setState(prevState => ({
+      goodsList: prevState.goodsList.reverse(),
+    }));
+  }
+
+  sortList = () => {
+    this.setState(prevState => ({
+      goodsList: prevState.goodsList.sort((a, b) => (
+        prevState.isSorted ? a.localeCompare(b) : b.localeCompare(a)
+      )),
+      isSorted: !prevState.isSorted,
+    }));
+  }
+
+  resetList = () => {
+    this.setState(prevState => ({
+      goodsList: [...prevState.originalGoods],
+      minLength: 1,
+    }));
+  }
+
+  sortListByLength = () => {
+    this.setState(prevState => ({
+      goodsList: prevState.goodsList.sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  handleSelectChange = ({ target }) => {
+    this.setState(prevState => ({
+      minLength: target.value,
+      goodsList: prevState.originalGoods.filter(item => (
+        item.length >= target.value)),
+    }));
+  }
+
+  render() {
+    const { goodsList, isLoaded, minLength } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        { isLoaded
+          ? (
+            <>
+              <button
+                type="button"
+                onClick={this.reverseList}
+              >
+                reverse
+              </button>
+              <button
+                type="button"
+                onClick={this.sortList}
+              >
+                sort
+              </button>
+              <button
+                type="button"
+                onClick={this.resetList}
+              >
+                reset
+              </button>
+              <button
+                type="button"
+                onClick={this.sortListByLength}
+              >
+                sortByLength
+              </button>
+              <select value={minLength} onChange={this.handleSelectChange}>
+                {new Array(10).fill(1).map((_, i) => (
+                  <option key={_} value={i + 1}>{i + 1}</option>))}
+              </select>
+              <GoodsList goods={goodsList} />
+            </>
+          )
+          : (
+            <button type="button" onClick={this.handleStart}>
+              Start
+            </button>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+GoodsList.propTypes = PropTypes.func.isRequired;
---
+++ src/components/GoodsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoodsList = ({ goods }) => (
+  <ul>
+    {goods.map(item => <li key={item}>{item}</li>)}
+  </ul>
+);
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf.isRequired,
+};
