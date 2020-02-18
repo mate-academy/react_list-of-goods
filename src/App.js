25a2e30b489f6060876c7276c2dfa1dc68ebd4fd
Natalia Lypovyk <cardinnata@gmail.com>
2020-02-18 18:57:37 +0200

    List of Goods

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,84 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends Component {
+  state = {
+    isStartButton: false,
+    goods: [...goodsFromServer],
+    initialGoods: [...goodsFromServer],
+  };
+
+  handleStart = () => {
+    this.setState({
+      isStartButton: true,
+    });
+  }
+
+  handleReverse = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].reverse(),
+    }));
+  }
+
+  handleSortAZ = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods]
+        .sort((a, b) => a.localeCompare(b)),
+    }));
+  }
+
+  handleSortByLength = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods]
+        .sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  handleReset = () => {
+    this.setState(prevState => ({
+      goods: prevState.initialGoods,
+    }));
+  }
+
+  handleSelect = (e) => {
+    const list = [...goodsFromServer];
+    const { value } = e.target;
+
+    this.setState(prevState => ({
+      goods: list.filter(item => item.length <= value),
+    }));
+  };
+
+  render() {
+    const { isStartButton, goods } = this.state;
+
+    return (
+      <div className="App">
+        {
+          isStartButton
+            ? (
+              <GoodsList
+                goods={goods}
+                handleReverse={this.handleReverse}
+                handleSortAZ={this.handleSortAZ}
+                handleReset={this.handleReset}
+                handleSortByLength={this.handleSortByLength}
+                handleSelect={this.handleSelect}
+              />
+            )
+            : (
+              <button
+                type="button"
+                onClick={this.handleStart}
+                className="start-btn"
+              >
+                Start
+              </button>
+            )
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
