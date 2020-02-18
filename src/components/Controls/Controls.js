04888ef49d40349db635e29855ec1637ec94a626
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-02-18 20:41:52 +0200

    solution

---
+++ src/components/Controls/Controls.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Controls.css';
+import uuid from 'uuid';
+
+export const Controls = (
+  { reversed,
+    sortedAlpha,
+    resetedGoods,
+    sortByLength,
+    selectAction,
+    length,
+    selectedArray },
+) => (
+  <div className="controls">
+    <button
+      type="button"
+      className="controls__button"
+      onClick={reversed}
+    >
+  Reverse
+    </button>
+    <button
+      type="button"
+      className="controls__button"
+      onClick={sortedAlpha}
+    >
+Sort Alpha
+    </button>
+    <button
+      type="button"
+      className="controls__button"
+      onClick={resetedGoods}
+    >
+Reset
+    </button>
+    <button
+      type="button"
+      className="controls__button"
+      onClick={sortByLength}
+    >
+Sort by length
+    </button>
+    <select className="select" value={length} onChange={selectAction}>
+      {selectedArray.map((item, index) => (
+        <option key={uuid.v4()} value={index + 1}>{index + 1}</option>
+      ))}
+    </select>
+  </div>
+);
+
+Controls.propTypes = {
+  reversed: PropTypes.func.isRequired,
+  sortedAlpha: PropTypes.func.isRequired,
+  resetedGoods: PropTypes.func.isRequired,
+  sortByLength: PropTypes.func.isRequired,
+  selectAction: PropTypes.func.isRequired,
+  length: PropTypes.number.isRequired,
+  selectedArray: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+};
---
+++ src/components/Goods/Goods.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Goods.css';
+import uuid from 'uuid';
+
+export const Goods = ({ goods }) => (
+  <ul className="list">
+    {goods.map(item => <li key={uuid.v4()} className="list__item">{item}</li>)}
+  </ul>
+);
+
+Goods.propTypes = {
+  goods: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+};
