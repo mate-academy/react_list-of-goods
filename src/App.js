4d8fd7852aeea47590e2a101df16a3b0f424bd30
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-19 16:34:15 +0200

    added filter to changeHandler method

--- src/App.js
+++ src/App.js
@@ -18,53 +18,88 @@ const goodsFromServer = [
 
 class App extends React.Component {
   state = {
+    initialGoods: [],
     goods: [],
     isLoaded: false,
     value: '1',
+    sortGoods: '',
   }
 
   componentDidMount() {
-    this.setState({ goods: goodsFromServer });
+    this.setState({ initialGoods: goodsFromServer });
   }
 
   showGoods = () => {
-    this.setState({ isLoaded: true });
+    this.setState(prevState => ({
+      isLoaded: true,
+      goods: [...prevState.initialGoods],
+    }));
   }
 
   reverse = () => {
     this.setState(prevState => ({
+      sortGoods: 'reverse',
       goods: [...prevState.goods].reverse(),
     }));
   }
 
   sortByAlphabet = () => {
     this.setState(prevState => ({
+      sortGoods: 'alphabet',
       goods: [...prevState.goods].sort((a, b) => a.localeCompare(b)),
     }));
   }
 
   sortByLength = () => {
     this.setState(prevState => ({
+      sortGoods: 'length',
       goods: [...prevState.goods].sort((a, b) => a.length - b.length),
     }));
   }
 
   reset = () => {
-    this.setState({
-      goods: goodsFromServer,
+    this.setState(prevState => ({
+      goods: [...prevState.initialGoods],
       value: '1',
-    });
+    }));
   }
 
   changeHandler = (event) => {
-    this.setState({
-      value: event.target.value,
+    event.persist();
+    this.setState((prevState) => {
+      let goods = [...prevState.initialGoods];
+
+      switch (prevState.sortGoods) {
+        case 'length': {
+          goods = [...prevState.initialGoods]
+            .sort((a, b) => a.length - b.length);
+          break;
+        }
+
+        case 'alphabet': {
+          goods = [...prevState.initialGoods]
+            .sort((a, b) => a.localeCompare(b));
+          break;
+        }
+
+        case 'reverse': {
+          goods = [...prevState.initialGoods].reverse();
+          break;
+        }
+
+        default: break;
+      }
+
+      return ({
+        value: event.target.value,
+        goods: goods
+          .filter(good => good.length >= event.target.value),
+      });
     });
   };
 
   render() {
     const { goods, isLoaded, value } = this.state;
-    const filteredGoods = goods.filter(good => good.length >= value);
 
     return (
       <div className="App">
@@ -108,7 +143,7 @@ class App extends React.Component {
                 <option value={9}>9</option>
                 <option value={10}>10</option>
               </select>
-              <GoodsList goods={filteredGoods} />
+              <GoodsList goods={goods} />
             </>
           )
           : (
