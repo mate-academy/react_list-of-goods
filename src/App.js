82b141409452d10fec5cd1661eeaebdc1c11922e
AlexanderShpilka <sandro.bot.evil@gmail.com>
2020-02-20 21:07:08 +0200

    Fix comment

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import { Button } from './components/Button/Button';
 
 export class App extends React.Component {
   state = {
+    originalGoods: [],
     goods: [],
     showGoods: false,
     minGoodLength: '1',
+    mode: 'default',
   };
 
   componentDidMount() {
     this.setState({
+      originalGoods: [...goodsFromServer],
       goods: [...goodsFromServer],
     });
   }
@@ -24,12 +27,14 @@ export class App extends React.Component {
   handleReverseButtonClick = () => {
     this.setState(prevState => ({
       goods: [...prevState.goods.reverse()],
+      mode: 'reverse',
     }));
   };
 
   handleSortAlphabeticallyButtonClick = () => {
     this.setState(prevState => ({
       goods: [...prevState.goods.sort()],
+      mode: 'alphabetical',
     }));
   };
 
@@ -37,6 +42,7 @@ export class App extends React.Component {
     this.setState({
       goods: [...goodsFromServer],
       minGoodLength: '1',
+      mode: 'default',
     });
   };
 
@@ -47,15 +53,45 @@ export class App extends React.Component {
           (goodA, goodB) => (goodA.length >= goodB.length ? 1 : -1),
         ),
       ],
+      mode: 'byLength',
     }));
+  };
+
+  prepareGoods = (length) => {
+    const { mode, originalGoods } = this.state;
+
+    switch (mode) {
+      case 'reverse': {
+        return originalGoods
+          .filter(good => good.length >= length)
+          .reverse();
+      }
+
+      case 'alphabetical': {
+        return originalGoods
+          .filter(good => good.length >= length)
+          .sort();
+      }
+
+      case 'byLength':
+        return originalGoods
+          .filter(good => good.length >= length)
+          .sort((goodA, goodB) => (goodA.length >= goodB.length ? 1 : -1));
+
+      default: {
+        return originalGoods.filter(good => good.length >= length);
+      }
+    }
   };
 
   handleSelectChange = (evt) => {
     const { value } = evt.target;
 
+    const newGoods = this.prepareGoods(Number(value));
+
     this.setState({
       minGoodLength: value,
-      goods: [...goodsFromServer.filter(good => good.length >= value)],
+      goods: newGoods,
     });
   };
 
