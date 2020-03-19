c745fa201fb0aa355e65356bcc0d279e34022ee4
AnnieMarkina <anniemurkina@gmail.com>
2020-03-19 18:00:59 +0200

    fixed

--- src/Components/ButtonsList/ButtonsList.js
+++ src/Components/ButtonsList/ButtonsList.js
@@ -3,80 +3,85 @@ import PropTypes from 'prop-types';
 import GoodsList from '../GoodsList/GoodsList';
 
 class ButtonsList extends React.Component {
-    state = { list: this.props.goodsList };
+  state = {
+    list: this.props.goodsList,
+  };
 
-    reverseClick = () => {
-      this.setState({ list: this.props.goodsList.reverse() });
-    }
+  handleClick = () => {
+    this.setState({ list: this.props.goodsList.reverse() });
+  }
 
-    sortlength = () => {
-      this.setState({ list: [...this.props.goodsList]
-        .sort((a, b) => b.length - a.length) });
-    }
+  handleSortLength = () => {
+    // eslint-disable-next-line max-len
+    const sortedByLength = [...this.props.goodsList].sort((a, b) => a.length - b.length);
 
-    sortAlphb = () => {
-      this.setState({ list: [...this.props.goodsList].sort() });
-    }
+    this.setState({ list: sortedByLength });
+  }
 
-    reset = () => {
-      this.setState({ list: this.props.goodsList });
-    }
+  handleSortAlphabetically = () => {
+    this.setState({ list: [...this.props.goodsList].sort() });
+  }
 
-    selectNum = (itemId) => {
-      const goodIndex = +itemId + 1;
-      // eslint-disable-next-line react/no-access-state-in-setstate
-      const items = [...this.state.list].slice(0, goodIndex);
+  handleReset = () => {
+    this.setState({ list: this.props.goodsList });
+  }
 
-      this.setState({ list: items });
-    }
+  handleSelectChange = (itemId) => {
+    const goodIndex = +itemId + 1;
+    const items = [...this.state.list].slice(0, goodIndex);
 
-    render() {
-      return (
-        <>
-          <button
-            type="button"
-            onClick={this.reverseClick}
-          >
+    this.setState(prevState => ({ list: items }));
+  }
+
+  render() {
+    const goodsArr = this.state.list;
+
+    return (
+      <>
+        <button
+          type="button"
+          onClick={this.handleClick}
+        >
         Reverse
-          </button>
+        </button>
 
-          <button
-            type="button"
-            onClick={this.sortlength}
-          >
+        <button
+          type="button"
+          onClick={this.handleSortLength}
+        >
         Sort by length
-          </button>
+        </button>
 
-          <button
-            type="button"
-            onClick={this.sortAlphb}
-          >
+        <button
+          type="button"
+          onClick={this.handleSortAlphabetically}
+        >
         Sort alphabetically
-          </button>
+        </button>
 
-          <button
-            type="button"
-            onClick={this.reset}
-          >
+        <button
+          type="button"
+          onClick={this.handleReset}
+        >
         Reset
-          </button>
+        </button>
 
-          <select onChange={event => this.selectNum(event.target.value)}>
-            {this.state.list.map((item, index) => (
-              <option
-                // eslint-disable-next-line react/no-array-index-key
-                key={index}
-                value={index}
-              >
-                {index + 1}
-              </option>
-            ))}
-          </select>
+        <select onChange={event => this.handleSelectChange(event.target.value)}>
+          {goodsArr.map((item, index) => (
+            <option
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              value={index}
+            >
+              {index + 1}
+            </option>
+          ))}
+        </select>
 
-          <GoodsList list={this.state.list} />
-        </>
-      );
-    }
+        <GoodsList list={this.state.list} />
+      </>
+    );
+  }
 }
 
 ButtonsList.propTypes = {
