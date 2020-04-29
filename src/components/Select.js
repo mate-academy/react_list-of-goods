3abf5f74fec9e1ad3f77fc1a4fd4d8818f0dc311
crash2bash <sachagurov@gmail.com>
2020-04-29 21:30:05 +0300

    fix

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const selectItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 const Select = ({ stringLength, selectButton }) => (
   <select onChange={selectButton} value={stringLength}>
     {
-      new Array(10).fill('').map((element, index) => (
-        <option value={index + 1}>{index + 1}</option>
+      selectItems.map((element, index) => (
+        <option key={element} value={index + 1}>{index + 1}</option>
       ))
     }
   </select>
