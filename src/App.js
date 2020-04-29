1e49854a0c492993575461dc2fe71d43edb8ffb3
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-29 16:29:30 +0300

    1-3

--- src/App.js
+++ src/App.js
@@ -14,11 +14,67 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    clicked: false,
+  }
+
+  StartClick = () => {
+    this.setState({ clicked: true });
+  }
+
+  render() {
+    const startButtonClicked = this.state.clicked;
+
+    return (
+      <div className="App">
+        {startButtonClicked
+          ? <GoodsList goods={goodsFromServer} />
+          : (
+            <button
+              type="button"
+              onClick={this.StartClick}
+            >
+              Start
+            </button>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+class GoodsList extends React.Component {
+  state = {
+    goodsCopy: [...goodsFromServer],
+  }
+
+  ReverseGoods = () => {
+    this.setState(({ goodsCopy }) => ({
+      goodsCopy: goodsCopy.reverse(),
+    }));
+  }
+
+  render() {
+    const listOfGoods = this.state.goodsCopy;
+
+    return (
+      <>
+        <h1>Goods</h1>
+        <button
+          type="button"
+          onClick={this.ReverseGoods}
+        >
+          Reverse Goods
+        </button>
+        <ul className="list">
+          {listOfGoods.map(good => (
+            <li className="list__item" key={good}>{good}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
 
 export default App;
