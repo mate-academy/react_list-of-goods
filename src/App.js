d3a2cdbdaf90fe67697a54e40875a7df1643216c
Kostya Peniyavskiy <kornelius9898@gmail.com>
2020-04-29 21:56:56 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import { GoodsList } from './GoodsList/GoodsList';
+
 const goodsFromServer = [
   'Dumplings',
   'Carrot',
@@ -14,11 +16,34 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    init: false,
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {this.state.init
+          ? (
+            <GoodsList
+              goods={goodsFromServer}
+            />
+          )
+          : (
+            <button
+              type="button"
+              onClick={() => (
+                this.setState({ init: true })
+              )}
+            >
+              Start
+            </button>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
