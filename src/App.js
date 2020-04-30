5fb4bb60669ef68910f094dd725663acf8c96ec1
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-30 17:37:02 +0300

    Solution in one file with no css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+// import PropTypes from 'prop-types';
 
 const goodsFromServer = [
   'Dumplings',
@@ -47,50 +48,81 @@ class App extends React.Component {
 class GoodsList extends React.Component {
   state = {
     goodsCopy: [...goodsFromServer],
+    select: 1,
   }
 
-  ReverseGoods = () => {
+  reverseGoods = () => {
     this.setState(({ goodsCopy }) => ({
       goodsCopy: [...goodsCopy].reverse(),
     }));
   }
 
-  SortGoods = () => {
+  sortGoods = () => {
     this.setState(({ goodsCopy }) => ({
       goodsCopy: [...goodsCopy].sort(),
     }));
   }
 
-  ResetGoods = () => {
+  resetGoods = () => {
     this.setState(() => ({
       goodsCopy: [...goodsFromServer],
     }));
   }
 
+  sortByLengthGoods = () => {
+    this.setState(({ goodsCopy }) => ({
+      goodsCopy: [...goodsCopy].sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  sortBySelectedLength = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      select: value,
+      goodsCopy: [...goodsFromServer].filter(el => el.length >= value),
+    });
+  }
+
   render() {
     const listOfGoods = this.state.goodsCopy;
+    const selectedArr = Array.from(Array(10).keys());
 
     return (
       <>
         <h1>Goods</h1>
         <button
           type="button"
-          onClick={this.ReverseGoods}
+          onClick={this.reverseGoods}
         >
-          Reverse Goods
+          Reverse
         </button>
         <button
           type="button"
-          onClick={this.SortGoods}
+          onClick={this.sortGoods}
         >
-          Sort Goods
+          Sort alphabetically
         </button>
         <button
           type="button"
-          onClick={this.ResetGoods}
+          onClick={this.resetGoods}
         >
-          Reset Goods
+          Reset
         </button>
+        <button
+          type="button"
+          onClick={this.sortByLengthGoods}
+        >
+          Sort by length
+        </button>
+        <select
+          value={this.state.select}
+          onChange={this.sortBySelectedLength}
+        >
+          {selectedArr.map(item => (
+            <option value={item} key={item}>{item}</option>
+          ))}
+        </select>
         <ul className="list">
           {listOfGoods.map(good => (
             <li className="list__item" key={good}>{good}</li>
@@ -100,5 +132,9 @@ class GoodsList extends React.Component {
     );
   }
 }
+
+// GoodsList.propTypes = {
+//   goodsCopy: PropTypes.arrayOf(PropTypes.string).isRequired,
+// };
 
 export default App;
