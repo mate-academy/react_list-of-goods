4b5aa477d75b67fe11028484ed4f445e3da5224c
romarios1987 <romios047@gmail.com>
2020-04-30 10:15:36 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ListOfGoods from './components/ListOfGoods';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,96 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    list: [],
+    loaded: false,
+    visibleList: [],
+    selectedValue: '1',
+  };
+
+  handler = () => {
+    this.setState({
+      list: [...goodsFromServer],
+      loaded: true,
+      visibleList: [...goodsFromServer],
+    });
+  };
+
+  reverseList = () => {
+    this.setState(prevState => ({
+      visibleList: [...prevState.visibleList].reverse(),
+    }));
+  };
+
+  sortAlphabetical = () => {
+    this.setState(prevState => ({
+      visibleList: [...prevState.visibleList].sort(),
+    }));
+  };
+
+  sortListLength = () => {
+    this.setState(prevState => ({
+      visibleList: [...prevState.visibleList]
+        .sort((a, b) => a.length - b.length),
+    }));
+  };
+
+  resetList = () => {
+    this.setState(prevState => ({
+      visibleList: [...prevState.list],
+    }));
+  };
+
+  onSelectChange = ({ target }) => {
+    const { value } = target;
+
+    this.setState(() => ({
+      selectedValue: value,
+      visibleList: goodsFromServer
+        .filter(good => good.length >= value),
+    }));
+  };
+
+  render() {
+    return (
+      <div className="goods_block">
+        {
+          (!this.state.loaded ? (
+            <button onClick={this.handler} type="button">
+              Start
+            </button>
+          ) : (
+            <div>
+              <button onClick={this.reverseList} type="button">
+                Reverse
+              </button>
+              <button onClick={this.sortAlphabetical} type="button">
+                Sort A-Z
+              </button>
+              <button onClick={this.sortListLength} type="button">
+                Sort by length
+              </button>
+              <button onClick={this.resetList} type="button">
+                Reset
+              </button>
+
+              <select
+                value={this.state.selectedValue}
+                onChange={event => this.onSelectChange(event)}
+              >
+                {[...Array(10).keys()].map(i => i + 1)
+                  .map(item => <option value={item} key={item}>{item}</option>)}
+              </select>
+
+              <ListOfGoods list={this.state.visibleList} />
+            </div>
+          )
+          )
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
