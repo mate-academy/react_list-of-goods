47cb1ebeaa0890d5dbee13830e51615dd991e600
Igor Kulish <igormagnetic@gmail.com>
2020-05-03 19:04:53 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GoodsList from './GoodList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,25 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    isVisible: true,
+  }
+
+  start = () => {
+    this.setState({ isVisible: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {(this.state.isVisible)
+          ? <button type="button" onClick={this.start}>Push the button</button>
+          : <GoodsList goods={goodsFromServer} />}
+      </div>
+    );
+  }
+}
 
 export default App;
