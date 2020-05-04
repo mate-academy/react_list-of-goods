3edc2ce3fa5901dd1b2c6a6373a58b87b49d55dd
Vlad <oneviddi@gmail.com>
2020-05-04 16:05:50 +0300

    1-3 completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
 const goodsFromServer = [
   'Dumplings',
@@ -17,8 +18,65 @@ const goodsFromServer = [
 const App = () => (
   <div className="App">
     <h1>Goods</h1>
-    {goodsFromServer.length}
+    <GoodsList goods={goodsFromServer} />
   </div>
 );
+
+// @@@@  ########################   @@@@
+
+class GoodsList extends React.Component {
+  state = {
+    listIsHidden: true,
+    goods: this.props.goods,
+  }
+
+  changeListVisibility = () => {
+    this.setState({
+      listIsHidden: false,
+    });
+  }
+
+  showListReversed = () => {
+    this.setState(prevValue => ({
+      goods: prevValue.goods.reverse(),
+    }));
+  }
+
+  render() {
+    const { goods, listIsHidden } = this.state;
+
+    return (
+      <div>
+        <button
+          hidden={!listIsHidden}
+          type="button"
+          onClick={this.changeListVisibility}
+        >
+          Start
+        </button>
+
+        <div hidden={listIsHidden}>
+          <ul>
+            {goods.map(good => (
+              <li key={good}>{good}</li>
+            ))}
+          </ul>
+          <div className="btns-section">
+            <button
+              type="button"
+              onClick={this.showListReversed}
+            >
+              REVERSE
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default App;
