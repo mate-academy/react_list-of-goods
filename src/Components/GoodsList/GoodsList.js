0f6f45de43d077d137314521fb907f2bada93a4c
solaryasha <solaryasha@gmail.com>
2020-07-01 19:40:06 +0300

    First working solution without css

--- src/Components/GoodsList/GoodsList.js
+++ src/Components/GoodsList/GoodsList.js
@@ -6,6 +6,7 @@ export class GoodsList extends Component {
   state = {
     isShown: false,
     goods: this.props.things,
+    selectedValue: 1,
   }
 
   clickHandler = () => {
@@ -15,6 +16,34 @@ export class GoodsList extends Component {
   reverseHandler = () => {
     this.setState(prevState => ({
       goods: [...prevState.goods].reverse(),
+    }));
+  }
+
+  sortAlphabeticallyHandler = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort((a, b) => a.localeCompare(b)),
+    }));
+  }
+
+  resetHandler = () => {
+    this.setState(() => ({
+      goods: this.props.things,
+      selectedValue: 1,
+    }));
+  }
+
+  sortLengthHandler = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  changeHandler = ({ target }) => {
+    const { value } = target;
+
+    this.setState(prevState => ({
+      goods: [...this.props.things].filter(thing => thing.length >= value),
+      selectedValue: value,
     }));
   }
 
@@ -33,6 +62,24 @@ export class GoodsList extends Component {
           : null
         }
         <button type="button" onClick={this.reverseHandler}>Reverse</button>
+        <button type="button" onClick={this.sortAlphabeticallyHandler}>
+          Sort alphabetically
+        </button>
+        <button type="button" onClick={this.resetHandler}>
+          Reset
+        </button>
+        <button type="button" onClick={this.sortLengthHandler}>
+          Sort by length
+        </button>
+        <select
+          onChange={this.changeHandler}
+          value={this.state.selectedValue}
+        >
+          {
+            Array.from(Array(10), (_, i) => i + 1)
+              .map(element => (<option>{element}</option>))
+          }
+        </select>
       </>
     );
   }
