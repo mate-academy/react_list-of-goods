b45884cdd79e38715428958b65d452a6d3980bae
Vovasef <vovasef@gmail.com>
2020-07-02 04:01:42 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +15,136 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: goodsFromServer,
+    show: false,
+  }
+
+  start = () => {
+    this.setState(prev => ({
+      show: !prev.show,
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        <button
+          type="button"
+          onClick={this.start}
+          style={!this.state.show
+            ? { display: 'initial' }
+            : { display: 'none' }
+          }
+        >
+          Start
+        </button>
+        <div style={this.state.show
+          ? { display: 'initial' }
+          : { display: 'none' }
+        }
+        >
+          <GoodsList goods={this.state.goods} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class GoodsList extends React.Component {
+  state = {
+    minL: 1,
+    goods: [...this.props.goods],
+  }
+
+  mixLength = (event) => {
+    const newMinL = event.target.value;
+
+    this.setState(prev => ({
+      minL: newMinL,
+    }));
+  }
+
+  reverse = () => {
+    this.setState(prev => ({
+      goods: prev.goods.reverse(),
+    }));
+  }
+
+  sortAlphabetically = () => {
+    this.setState(prev => ({
+      goods: prev.goods.sort(),
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(prev => ({
+      goods: prev.goods.sort((a, b) => b.length - a.length),
+    }));
+  }
+
+  render() {
+    const selections = [];
+
+    for (let i = 1; i <= 10; i) {
+      selections.push((<option value={i} key={i}>{i}</option>));
+      i += 1;
+    }
+
+    return (
+      <>
+        <ul>
+          {this.state.goods
+            .filter(x => x.length >= this.state.minL)
+            .map(good => (
+              <li key={good}>
+                {good}
+              </li>
+            ))}
+        </ul>
+        <button type="button" onClick={this.reverse}>
+          Reverse
+        </button>
+        <button type="button" onClick={this.sortAlphabetically}>
+          Sort alphabetically
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ goods: [...this.props.goods] });
+          }}
+        >
+          Reset
+        </button>
+        <br />
+        <button type="button" onClick={this.sortByLength}>
+          Sort by length
+        </button>
+        <select
+          name="minL"
+          value={this.state.minL}
+          onChange={this.mixLength}
+        >
+          {selections}
+        </select>
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({ minL: 1 });
+          }}
+        >
+          Reset
+        </button>
+        <br />
+      </>
+    );
+  }
+}
 
 export default App;
+
+GoodsList.propTypes = {
+  goods: PropTypes.instanceOf(Array).isRequired,
+};
