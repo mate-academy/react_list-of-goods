78148402b0b7b4e27e93c60cda3b519dce8167db
Elena-brv <leena.brv@gmail.com>
2020-09-04 11:13:04 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -17,52 +18,14 @@ const goodsFromServer = [
 class App extends React.Component {
   state = {
     isClicked: false,
-    goods: goodsFromServer,
-    value: '1',
   }
 
   mainClick = () => {
     this.setState({ isClicked: true });
   }
 
-  reverse = () => {
-    this.setState(state => ({
-      goods: [...state.goods].reverse(),
-    }));
-  }
-
-  sortByAlphabet = () => {
-    this.setState(state => ({
-      goods: [...state.goods].sort(),
-    }));
-  }
-
-  reset = () => {
-    this.setState(state => ({
-      goods: goodsFromServer,
-      value: '1',
-    }));
-  }
-
-  sortByLength = () => {
-    this.setState(state => ({
-      goods: [...state.goods].sort(
-        (a, b) => a.length - b.length,
-      ),
-    }));
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      value: event.target.value,
-      goods: goodsFromServer.filter(
-        item => item.length >= event.target.value,
-      ),
-    });
-  }
-
   render() {
-    const { isClicked, goods, value } = this.state;
+    const { isClicked } = this.state;
 
     return (
       <div className="App">
@@ -76,39 +39,7 @@ class App extends React.Component {
         </button>
 
         <div hidden={!isClicked}>
-          <ul className="list">
-            {goods.map(item => (
-              <li className="list__item" key={item}>
-                {item}
-              </li>
-            ))}
-          </ul>
-
-          <button type="button" onClick={this.reverse}>
-            Reverse
-          </button>
-          <button type="button" onClick={this.sortByAlphabet}>
-            Sort alphabetically
-          </button>
-          <button type="button" onClick={this.reset}>
-            Reset
-          </button>
-          <button type="button" onClick={this.sortByLength}>
-            Sort by length
-          </button>
-
-          <select value={value} onChange={this.handleChange}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
+          <GoodsList goods={goodsFromServer} />
         </div>
       </div>
     );
---
+++ src/components/GoodsList/GoodsList.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export class GoodsList extends React.Component {
+  state = {
+    goods: this.props.goods,
+    value: '1',
+  }
+
+  reverse = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  }
+
+  sortByAlphabet = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort(),
+    }));
+  }
+
+  reset = () => {
+    this.setState({
+      goods: this.props.goods,
+      value: '1',
+    });
+  }
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort(
+        (a, b) => a.length - b.length,
+      ),
+    }));
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value,
+      goods: this.props.goods.filter(
+        item => item.length >= event.target.value,
+      ),
+    });
+  }
+
+  render() {
+    const { goods, value } = this.state;
+
+    return (
+      <>
+        <ul className="list">
+          {goods.map(item => (
+            <li className="list__item" key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
+
+        <button type="button" onClick={this.reverse}>
+          Reverse
+        </button>
+        <button type="button" onClick={this.sortByAlphabet}>
+          Sort alphabetically
+        </button>
+        <button type="button" onClick={this.reset}>
+          Reset
+        </button>
+        <button type="button" onClick={this.sortByLength}>
+          Sort by length
+        </button>
+
+        <select value={value} onChange={this.handleChange}>
+          {new Array(10)
+            .fill()
+            .map((_, i) => (
+              <option value={i + 1}>
+                {i + 1}
+              </option>
+            ))
+          }
+        </select>
+      </>
+    );
+  }
+}
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
