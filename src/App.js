8eb778e2cec1958c353d3f4c4636f752c952e705
Zarva <zarva6596@gmail.com>
2020-09-04 18:56:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -14,11 +14,132 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    n: 1,
+    stan: false,
+    list: goodsFromServer,
+  };
+
+  toStart = () => {
+    const { stan } = this.state;
+
+    this.setState({ stan: !stan });
+  }
+
+  toReset = () => {
+    this.setState(state => ({
+      list: goodsFromServer,
+      n: 1,
+    }));
+  }
+
+  toReverse = () => {
+    this.setState(state => ({
+      list: [...state.list].reverse(),
+    }));
+  }
+
+  toSortAl = () => {
+    this.setState(state => ({
+      list: [...state.list].sort(),
+    }));
+  }
+
+  toSortLen = () => {
+    this.setState(state => ({
+      list: [...state.list].sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  toChange = (event) => {
+    const i = event.target.value;
+
+    this.toReset();
+
+    this.setState(state => ({
+      n: i,
+      list: state.list.filter(a => a.length > i),
+    }));
+  }
+
+  render() {
+    const { stan, list, n } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          className={stan ? 'start--none' : 'start'}
+          type="button"
+          onClick={this.toStart}
+        >
+          START
+        </button>
+
+        <div
+          className={stan ? 'button-box' : 'button-box--none'}
+        >
+          <button
+            className="button-box__button"
+            type="button"
+            onClick={this.toReset}
+          >
+            RESET
+          </button>
+
+          <button
+            className="button-box__button"
+            type="button"
+            onClick={this.toSortAl}
+          >
+            SORT ALPHABETICALLY
+          </button>
+
+          <button
+            className="button-box__button"
+            type="button"
+            onClick={this.toSortLen}
+          >
+            SORT BY LENGTH
+          </button>
+
+          <button
+            className="button-box__button"
+            type="button"
+            onClick={this.toReverse}
+          >
+            REVERSE
+          </button>
+
+          <input
+            name="input"
+            className="button-box__button button-box__button--input"
+            type="number"
+            placeholder="10"
+            value={n}
+            max="10"
+            min="1"
+            onChange={this.toChange}
+          />
+        </div>
+
+        <div
+          className={stan ? 'list-box' : 'list-box--none'}
+        >
+          <ul>
+            {list.map(good => (
+              <li
+                key={Math.random()}
+                className="list-box__item"
+              >
+                {good}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
