902571a979ce10cfa197e1285d5f21f8619ec7d3
Anton <tsymbal044@gmail.com>
2020-09-04 18:22:13 +0300

    solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { List } from './List';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,33 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    listVisibility: false,
+  }
 
-export default App;
+  makeVisible = () => {
+    this.setState({
+      listVisibility: true,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.listVisibility && (
+          <button
+            className="button"
+            type="button"
+            onClick={this.makeVisible}
+          >
+            Start
+          </button>
+        )}
+        {this.state.listVisibility && (
+          <List goods={goodsFromServer} />
+        )}
+      </div>
+    );
+  }
+}
