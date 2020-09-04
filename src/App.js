2d54dd04a9745c179d84eee37c81df59ed4b542b
Roman <romikkus@gmail.com>
2020-09-04 10:38:25 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,110 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [],
+    hideButton: false,
+  }
+
+  start = () => {
+    this.setState(prevState => ({
+      goods: [...goodsFromServer],
+      hideButton: !prevState.hideButton,
+    }));
+  }
+
+  reverse = () => {
+    this.setState(prevState => ({
+      goods: [
+        ...prevState.goods.reverse(),
+      ],
+    }));
+  }
+
+  sortByAlphabet = () => {
+    this.setState(prevState => ({
+      goods: [
+        ...prevState.goods.sort((a, b) => a.localeCompare(b)),
+      ],
+    }));
+  }
+
+  reset = () => {
+    this.setState({
+      goods: [...goodsFromServer],
+    });
+  }
+
+  sortByLength = () => {
+    this.setState(prevState => ({
+      goods: [
+        ...prevState.goods
+          .sort((a, b) => a.replace(/\s/g, '').length - b.replace(/\s/g, '')
+            .length),
+      ],
+    }));
+  }
+
+  render() {
+    const { hideButton } = this.state;
+
+    return (
+      <div className="App">
+        {hideButton || (
+          <button
+            type="button"
+            onClick={this.start}
+          >
+            Show goods
+          </button>
+        )}
+        <GoodsList goods={this.state.goods} />
+
+        <div className="buttons">
+          {hideButton && (
+            <button
+              className="button"
+              type="button"
+              onClick={this.reverse}
+            >
+              Reverse
+            </button>
+          )}
+
+          {hideButton && (
+            <button
+              className="button"
+              type="button"
+              onClick={this.sortByAlphabet}
+            >
+              Sort alphabetically
+            </button>
+          )}
+
+          {hideButton && (
+            <button
+              className="button"
+              type="button"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+          )}
+
+          {hideButton && (
+            <button
+              className="button"
+              type="button"
+              onClick={this.sortByLength}
+            >
+              Sort by words length
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
