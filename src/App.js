e053bc8a4025847e8ca838e94e85d1bfab2b2008
Anton <a.v.sorokin95@gmail.com>
2020-09-04 15:35:30 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import classNames from 'classnames';
+
+import GoodsList from './components/GoodsList';
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +17,72 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+    visibility: false,
+  }
+
+  reverseList = () => {
+    this.setState(state => ({
+      goods: [...state.goods.reverse()],
+    }));
+  }
+
+  sortList = () => {
+    this.setState(state => ({
+      goods: [...state.goods.sort((a, b) => a.localeCompare(b))],
+    }));
+  }
+
+  resetList = () => {
+    this.setState({
+      goods: [...goodsFromServer],
+    });
+  }
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods.sort((a, b) => a.length - b.length)],
+    }));
+  }
+
+  render() {
+    const { goods } = this.state;
+    const { visibility } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          type="button"
+          onClick={() => this.setState({ visibility: true })}
+          className={classNames({
+            startButton: visibility,
+          })}
+        >
+          Start
+        </button>
+        {!visibility || <GoodsList listItems={goods} />}
+        <div className={classNames({
+          buttons: !visibility,
+        })}
+        >
+          <button type="button" onClick={this.reverseList}>
+            Reverse
+          </button>
+          <button type="button" onClick={this.sortList}>
+            Sort alphabetically
+          </button>
+          <button type="button" onClick={this.resetList}>
+            Reset
+          </button>
+          <button type="button" onClick={this.sortByLength}>
+            Sort by length
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GoodsList = ({ listItems }) => (
+  <div>
+    <h1>Goods</h1>
+    <ul>
+      {listItems.map(goods => (
+        <li key={goods}>
+          {goods}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default GoodsList;
+
+GoodsList.propTypes = {
+  listItems: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
