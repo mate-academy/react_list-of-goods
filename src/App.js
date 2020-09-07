69de3b24378a0187e9f7dcbeff1bf83a79a43ad7
yuriimovchko <movchko.yuriy@gmail.com>
2020-09-07 12:28:28 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,78 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+  }
+
+  Start = (event) => {
+    document.querySelector('.list').classList.add('active');
+    event.target.classList.add('inactive');
+  }
+
+  Reverse = () => {
+    this.setState(state => ({
+      goods: state.goods.reverse(),
+    }));
+  }
+
+  Sort = () => {
+    this.setState(state => ({
+      goods: state.goods.sort(),
+    }));
+  }
+
+  Reset = () => {
+    this.setState(state => ({
+      goods: [...goodsFromServer],
+    }));
+  }
+
+  SortLength = () => {
+    this.setState(state => ({
+      goods: state.goods.sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        <button
+          type="button"
+          onClick={this.Start}
+        >
+          Start
+        </button>
+        <button
+          type="button"
+          onClick={this.Reverse}
+        >
+          Reverse
+        </button>
+        <button
+          type="button"
+          onClick={this.Sort}
+        >
+          Sort
+        </button>
+        <button
+          type="button"
+          onClick={this.Reset}
+        >
+          Reset
+        </button>
+        <button
+          type="button"
+          onClick={this.SortLength}
+        >
+          Sort by length
+        </button>
+        <GoodsList goods={this.state.goods} />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoodsList = ({ goods }) => (
+  <ul className="list">
+    {goods.map(good => (
+      <li key={good}>
+        {good}
+      </li>
+    ))}
+  </ul>
+);
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+};
