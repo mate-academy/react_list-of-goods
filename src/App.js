6609a5a9c759fec213fbf570da19d0fdad009d82
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-09-30 06:33:11 +0300

    add hidden for elements

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ export default class App extends React.Component {
 state = {
   goodsList: [],
   valueDefaultSelected: 'Dumplings',
-  hidden: false,
+  isHidden: false,
 }
 
 showListAndShowButtons = () => {
   this.setState({
     goodsList: [...goodsFromServer],
-    hidden: true,
+    isHidden: true,
   });
 }
 
@@ -60,63 +60,66 @@ filterList = (event) => {
 }
 
 render() {
-  const { goodsList, hidden, valueDefaultSelected } = this.state;
+  const { goodsList, isHidden, valueDefaultSelected } = this.state;
 
   return (
     <div className="App">
       <h1>Goods</h1>
-      <button
-        type="button"
-        onClick={this.showListAndShowButtons}
-        hidden={hidden}
-      >
-        Start
-      </button>
-      <button
-        type="button"
-        onClick={this.reverseList}
-        hidden={!hidden}
-      >
-        Reverse
-      </button>
-      <button
-        type="button"
-        onClick={this.sortABCList}
-        hidden={!hidden}
-      >
-        Sort alphabetically
-      </button>
-      <button
-        type="button"
-        onClick={this.resetList}
-        hidden={!hidden}
-      >
-        Reset
-      </button>
-      <button
-        type="button"
-        onClick={this.sortListByLength}
-        hidden={!hidden}
-      >
-        Sort by length
-      </button>
-      <List
-        goodsList={goodsList}
-      />
-      <select
-        value={valueDefaultSelected}
-        onChange={this.filterList}
-        hidden={!hidden}
-      >
-        {this.state.goodsList.map((good, index) => (
-          <option
-            key={good}
-            value={index + 1}
+
+      {(this.state.isHidden === false)
+        ? (
+          <button
+            type="button"
+            onClick={this.showListAndShowButtons}
+            hidden={isHidden}
           >
-            {index + 1}
-          </option>
-        ))}
-      </select>
+            Start
+          </button>
+        ) : (
+          <>
+            <button
+              type="button"
+              onClick={this.reverseList}
+            >
+              Reverse
+            </button>
+            <button
+              type="button"
+              onClick={this.sortABCList}
+            >
+              Sort alphabetically
+            </button>
+            <button
+              type="button"
+              onClick={this.resetList}
+            >
+              Reset
+            </button>
+            <button
+              type="button"
+              onClick={this.sortListByLength}
+            >
+              Sort by length
+            </button>
+            <List
+              goodsList={goodsList}
+            />
+            <select
+              value={valueDefaultSelected}
+              onChange={this.filterList}
+            >
+              {this.state.goodsList.map((good, index) => (
+                <option
+                  key={good}
+                  value={index + 1}
+                >
+                  {index + 1}
+                </option>
+              ))}
+            </select>
+          </>
+
+        )}
     </div>
   );
 }
