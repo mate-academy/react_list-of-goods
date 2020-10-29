f8db8f7b8495e9d088a707a64ce0eb9916e76b25
Lancaelot <Lancaelot@gmail.com>
2020-10-29 15:31:50 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import GoodsList from './components/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +14,104 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const preperedGoods = goodsFromServer.map((good, index) => ({
+  id: index,
+  name: good,
+}));
+
+class App extends Component {
+  state = {
+    goods: [...preperedGoods],
+    isVisible: false,
+    isSorted: false,
+  }
+
+  Start = () => {
+    this.setState(state => ({
+      isVisible: !state.isVisible,
+    }));
+  }
+
+  Reverse = () => {
+    this.setState(state => ({
+      goods: state.goods.reverse(),
+      isSorted: false,
+    }));
+  }
+
+  Sort = () => {
+    this.setState(state => ({
+      // eslint-disable-next-line
+      goods: state.goods.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)),
+      isSorted: true,
+    }));
+  }
+
+  Reset = () => {
+    this.setState(state => ({
+      goods: [...preperedGoods],
+    }));
+  }
+
+  SortbyLength = () => {
+    this.setState(state => ({
+      goods: state.goods.sort((a, b) => a.name.length - b.name.length),
+      isSorted: true,
+    }));
+  }
+
+  render() {
+    const { goods, isVisible, isSorted } = this.state;
+
+    return (
+      <div className="goods">
+        <button
+          type="button"
+          className="positive ui button"
+          onClick={this.Start}
+        >
+          {isVisible ? 'Hide' : 'Show'}
+        </button>
+        <button
+          type="button"
+          disabled={isVisible ? '' : 'disabled'}
+          className="ui secondary button"
+          onClick={isSorted ? this.Reverse : this.Sort}
+        >
+          {isSorted ? 'Sort DESC' : 'Sort ASC'}
+        </button>
+        <button
+          type="button"
+          disabled={isVisible ? '' : 'disabled'}
+          className="ui secondary button"
+          onClick={this.Reverse}
+        >
+          Reverse
+        </button>
+        <button
+          type="button"
+          disabled={isVisible ? '' : 'disabled'}
+          className="ui secondary button"
+          onClick={this.Reset}
+        >
+          Reset
+        </button>
+        <button
+          type="button"
+          disabled={isVisible ? '' : 'disabled'}
+          className="ui secondary button"
+          onClick={isSorted ? this.Reverse : this.SortbyLength}
+        >
+          { isSorted ? 'Sort by Length DESC' : 'Sort by Length ASC' }
+        </button>
+        <ul
+          style={isVisible ? { display: 'block' } : { display: 'none' }}
+        >
+          <GoodsList goods={goods} />
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GoodsList = ({ goods }) => {
+  return (
+    goods.map(item => (
+    <li key={item.id}>{item.name}</li>))
+  );
+};
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  })).isRequired
+};
+
+export default GoodsList;
