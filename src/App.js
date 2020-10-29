5bf36e590ba3b924044329268e4fa8409e72eb3d
Dmitry Morozov <10mdv11@gmail.com>
2020-10-29 21:56:03 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { ButtonList } from './components/ButtonList';
+
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +16,78 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends PureComponent {
+  state = {
+    hidden: true,
+    goods: goodsFromServer,
+  }
+
+  start = () => {
+    this.setState(({ hidden }) => ({
+      hidden: !hidden,
+    }));
+  }
+
+  reverseGoods = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].reverse(),
+    }));
+  }
+
+  sortAlphabet = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort((a, b) => (
+        a.localeCompare(b)
+      )),
+    }));
+  }
+
+  reset = () => {
+    this.setState({
+      goods: goodsFromServer,
+    });
+  }
+
+  sortLength = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort((a, b) => (
+        a.length - b.length
+      )),
+    }));
+  }
+
+  render() {
+    const { hidden, goods } = this.state;
+
+    return (
+      <div className="container text-center my-5">
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={this.start}
+          hidden={!hidden}
+        >
+          Start
+        </button>
+        <div hidden={hidden} className="btn-group my-5">
+          <ButtonList
+            reverseGoods={this.reverseGoods}
+            sortAlphabet={this.sortAlphabet}
+            reset={this.reset}
+            sortLength={this.sortLength}
+          />
+        </div>
+
+        <ul className="list-group  mx-auto w-25" hidden={hidden}>
+          {goods.map(item => (
+            <li key={item} className="list-group-item text-center">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
