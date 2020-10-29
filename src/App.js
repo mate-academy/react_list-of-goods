2fd2e8a10cfcf48debefcf986664e517db96804a
Liza <elizabethhoncharenko@gmail.com>
2020-10-29 15:54:48 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,95 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    isActive: false,
+    goods: goodsFromServer,
+  }
+
+  activeList = () => {
+    this.setState(prevState => ({
+      isActive: !prevState.isActive,
+    }));
+  }
+
+  reverseList = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].reverse(),
+    }));
+  }
+
+  sortByAlphabet = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort((a, b) => a.localeCompare(b)),
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods].sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  reset = () => {
+    this.setState({ goods: goodsFromServer });
+  };
+
+  render() {
+    const { isActive, goods } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          className={isActive ? 'hidden' : ''}
+          type="button"
+          onClick={this.activeList}
+        >
+          Start
+        </button>
+
+        {
+          isActive
+            ? (
+              <ul>
+                {goods.map(item => (
+                  <li key={item}>{item}</li>
+                ))}
+              </ul>
+            )
+            : ''
+        }
+
+        <button
+          type="button"
+          onClick={this.reverseList}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          onClick={this.sortByAlphabet}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          onClick={this.reset}
+        >
+          Reset
+        </button>
+
+        <button
+          type="button"
+          onClick={this.sortByLength}
+        >
+          Sort by length
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
