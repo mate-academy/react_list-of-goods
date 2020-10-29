851a786bc6a716f62592b63fe3e37c019b5bc05f
milla-romankova <milla-romankova@gmail.com>
2020-10-29 15:54:02 +0200

    remove sort from render

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -4,75 +4,80 @@ import PropTypes from 'prop-types';
 import { GoodsList } from '../GoodsList';
 import { Buttons } from '../Buttons';
 
+function sortGoods(goodsList, sortBy) {
+  return goodsList.sort((good1, good2) => {
+    switch (sortBy) {
+      case 'name':
+        return good1[sortBy].localeCompare(good2[sortBy]);
+
+      case 'length':
+        return good1.name.length - good2.name.length;
+
+      default:
+        return 0;
+    }
+  });
+}
+
 export class Page extends React.PureComponent {
   state = {
     goods: this.props.goods,
-    isListReversed: false,
-    sortBy: '',
     lengthLimit: 1,
+    sortBy: null,
   }
 
   reverseList = () => {
     this.setState(state => ({
-      isListReversed: !state.isListReversed,
+      goods: [...state.goods].reverse(),
     }));
   }
 
   sortByName = () => {
+    if (this.state.sortBy !== 'name') {
+      this.setState(state => ({
+        sortBy: 'name',
+      }));
+    }
+
     this.setState(state => ({
-      isListReversed: false,
-      sortBy: 'name',
+      goods: sortGoods([...state.goods], state.sortBy),
     }));
   }
 
   sortByLength = () => {
+    if (this.state.sortBy !== 'length') {
+      this.setState(state => ({
+        sortBy: 'length',
+      }));
+    }
+
     this.setState(state => ({
-      isListReversed: false,
-      sortBy: 'length',
+      goods: sortGoods([...state.goods], state.sortBy),
     }));
   }
 
-  selectByLength = (event) => {
+  selectLengthLimit = (event) => {
     this.setState({
       lengthLimit: event.target.value,
     });
   }
 
+  selectByLength = () => {
+    const { goods, lengthLimit } = this.state;
+
+    return goods.filter(good => good.name.length >= lengthLimit);
+  }
+
   reset = () => {
     this.setState({
-      isListReversed: false,
-      sortBy: '',
+      goods: this.props.goods,
       lengthLimit: 1,
+      sortBy: null,
     });
   }
 
   render() {
-    const {
-      lengthLimit,
-      sortBy,
-      isListReversed,
-    } = this.state;
-
-    const visibleGoods = this.state.goods.filter(
-      good => (good.name.length >= lengthLimit),
-    );
-
-    visibleGoods.sort((good1, good2) => {
-      switch (sortBy) {
-        case 'name':
-          return good1[sortBy].localeCompare(good2[sortBy]);
-
-        case 'length':
-          return good1.name.length - good2.name.length;
-
-        default:
-          return 0;
-      }
-    });
-
-    if (isListReversed) {
-      visibleGoods.reverse();
-    }
+    const { lengthLimit } = this.state;
 
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -80,14 +85,14 @@ export class Page extends React.PureComponent {
       <div>
         <h1 className="card-header text-center">Goods</h1>
         <GoodsList
-          goods={visibleGoods}
+          goods={this.selectByLength()}
         />
 
         <Buttons
           reverseList={this.reverseList}
           sortByName={this.sortByName}
           sortByLength={this.sortByLength}
-          selectByLength={this.selectByLength}
+          selectByLength={this.selectLengthLimit}
           reset={this.reset}
           numbers={numbers}
           lengthLimit={lengthLimit}
