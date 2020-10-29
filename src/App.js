3e92c5e711192988567e26fdeb47bc8b0e2a7aa0
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-29 12:32:09 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import GoodList from './components/GoodList/GoodList';
+import Button from './components/Button/Button';
+
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +17,86 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const goodsPrepared = goodsFromServer.map((good, index) => ({
+  id: index,
+  good,
+}));
+
+class App extends React.PureComponent {
+  state = {
+    isListVisible: false,
+    isStartVisible: true,
+    goods: goodsPrepared,
+  }
+
+  start = () => {
+    this.setState({
+      isListVisible: true,
+      isStartVisible: false,
+    });
+  }
+
+  reverse = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+
+    }));
+  }
+
+  sort = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.good.localeCompare(b.good)),
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.good.length - b.good.length),
+    }));
+  }
+
+  reset = () => {
+    this.setState(state => ({
+      goods: goodsPrepared,
+    }));
+  }
+
+  render() {
+    const { isListVisible, isStartVisible, goods } = this.state;
+
+    return (
+      <div className="app">
+        {isStartVisible
+          ? (
+            <button
+              className="app__button"
+              type="button"
+              onClick={this.start}
+            >
+              Start
+            </button>
+          )
+          : null
+        }
+
+        {isListVisible
+          ? (
+            <div className="app__content">
+              <div className="app__buttons">
+                <Button text="Reverse" onclick={this.reverse} />
+                <Button text="Sort" onclick={this.sort} />
+                <Button text="Sort by length" onclick={this.sortByLength} />
+                <Button text="Reset" onclick={this.reset} />
+              </div>
+
+              <GoodList goods={goods} />
+            </div>
+          )
+          : null
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
