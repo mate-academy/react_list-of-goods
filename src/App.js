d20b6da9f65483fa0026956cf31f88e1bce72e5f
Yurii <yurastt1@gmail.com>
2020-10-29 05:02:43 +0200

    sol

--- src/App.js
+++ src/App.js
@@ -14,11 +14,103 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    showButton: true,
+    preparedGoods: [...goodsFromServer],
+  }
+
+  showGoodsList = () => {
+    this.setState(prevState => ({
+      showButton: !prevState.showButton,
+    }));
+  }
+
+  reverse = () => {
+    this.setState(prevState => ({
+      preparedGoods: [...prevState.preparedGoods].reverse(),
+    }));
+  }
+
+  sortByAlphabet = () => {
+    this.setState(prevState => ({
+      preparedGoods: [...prevState.preparedGoods].sort(),
+    }));
+  }
+
+  reset = () => {
+    this.setState(prevState => ({
+      preparedGoods: [...goodsFromServer],
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(prevState => ({
+      preparedGoods: [...prevState.preparedGoods]
+        .sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  render() {
+    const { showButton, preparedGoods } = this.state;
+
+    return (
+      <div className="App">
+        {showButton
+          ? (
+            <button
+              type="button"
+              onClick={this.showGoodsList}
+            >
+              Start
+            </button>
+          )
+          : (
+            <>
+              <div className="buttons">
+                <button
+                  type="button"
+                  onClick={this.reverse}
+                >
+                  reverse
+                </button>
+
+                <button
+                  type="button"
+                  onClick={this.sortByAlphabet}
+                >
+                  sort by alphabet
+                </button>
+
+                <button
+                  type="button"
+                  onClick={this.reset}
+                >
+                  reset
+                </button>
+
+                <button
+                  type="button"
+                  onClick={this.sortByLength}
+                >
+                  sort by length
+                </button>
+              </div>
+              <ul>
+                {preparedGoods.map(good => (
+                  <li key={good}>
+                    {good}
+                  </li>
+                ))}
+              </ul>
+            </>
+
+          )
+        }
+
+      </div>
+    );
+  }
+}
 
 export default App;
