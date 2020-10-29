ced23f53873a24e9f8fc25db83c6125535ee07e1
Nikolay Filiuk <Filiuk98@gmail.com>
2020-10-29 19:21:36 +0200

    Move methods

--- src/App.js
+++ src/App.js
@@ -26,42 +26,16 @@ const preparedGoods = goodsFromServer.map((good, index) => ({
 class App extends React.Component {
   state = {
     contentVisible: false,
-    goods: preparedGoods,
   }
 
   toggleVisible = () => {
     this.setState(state => ({
       contentVisible: !state.contentVisible,
-      goods: preparedGoods,
     }));
   };
 
-  reverseList = () => {
-    this.setState(state => ({
-      goods: [...state.goods].reverse(),
-    }));
-  }
-
-  sortByAlph = () => {
-    this.setState(state => ({
-      goods: [...state.goods].sort((a, b) => a.name.localeCompare(b.name)),
-    }));
-  }
-
-  reset = () => {
-    this.setState(state => ({
-      goods: preparedGoods,
-    }));
-  }
-
-  sortByLength = () => {
-    this.setState(state => ({
-      goods: [...state.goods].sort((a, b) => a.name.length - b.name.length),
-    }));
-  }
-
   render() {
-    const { goods, contentVisible } = this.state;
+    const { contentVisible } = this.state;
 
     return (
       <Container className="pt-5">
@@ -70,7 +44,7 @@ class App extends React.Component {
         )
           : (
             <PageContent
-              goods={goods}
+              preparedGoods={preparedGoods}
               reverseList={this.reverseList}
               sortByAlph={this.sortByAlph}
               reset={this.reset}
--- src/components/PageContent/PageContent.js
+++ src/components/PageContent/PageContent.js
@@ -5,23 +5,49 @@ import { AppButtons } from '../AppButtons';
 import { GoodsList } from '../GoodsList';
 import { AppButtonsPropTypes } from '../propTypes/AppButtonsPropTypes';
 
-export const PageContent = ({
-  goods,
-  reverseList,
-  sortByAlph,
-  sortByLength,
-  reset,
-}) => (
-  <>
-    <AppButtons
-      reverseList={reverseList}
-      sortByAlph={sortByAlph}
-      sortByLength={sortByLength}
-      reset={reset}
-    />
-    <GoodsList goods={goods} />
-  </>
-);
+export class PageContent extends React.Component {
+  state = {
+    goods: this.props.preparedGoods,
+  }
+
+  reverseList = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  }
+
+  sortByAlph = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.name.localeCompare(b.name)),
+    }));
+  }
+
+  reset = () => {
+    this.setState(state => ({
+      goods: this.props.preparedGoods,
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.name.length - b.name.length),
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <AppButtons
+          reverseList={this.reverseList}
+          sortByAlph={this.sortByAlph}
+          sortByLength={this.sortByLength}
+          reset={this.reset}
+        />
+        <GoodsList goods={this.state.goods} />
+      </>
+    );
+  }
+}
 
 PageContent.propTypes = PropTypes.shape({
   goods: PropTypes.shape(GoodListPropTypes).isRequired,
