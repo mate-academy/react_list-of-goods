1753588569510452e2551a7fe6b95974052b3733
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-19 20:26:16 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,88 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    stateGoods: goodsFromServer,
+    isStarted: false,
+    isReversed: false,
+    sortBy: '',
+  }
+
+  start = () => {
+    this.setState({ isStarted: true });
+  }
+
+  hide = () => {
+    this.setState({ isStarted: false });
+  }
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  }
+
+  sortAlphabetically = () => {
+    this.setState({ sortBy: 'alphabet' });
+  }
+
+  resetSort = () => {
+    this.setState({ sortBy: '' });
+  }
+
+  sortByLength = () => {
+    this.setState({ sortBy: 'length' });
+  }
+
+  render() {
+    const { stateGoods, isStarted, isReversed, sortBy } = this.state;
+    const goodsCopy = [...stateGoods];
+
+    goodsCopy.sort((value1, value2) => {
+      switch (sortBy) {
+        case 'alphabet':
+          return value1.localeCompare(value2);
+
+        case 'length':
+          return value1.length - value2.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      goodsCopy.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {!isStarted
+          && <button type="button" onClick={this.start}>Start</button>}
+        {isStarted
+          && <button type="button" onClick={this.hide}>Hide</button>}
+        {isStarted
+          && <button type="button" onClick={this.reverse}>Reverse</button>}
+        {isStarted
+          && (
+            <button type="button" onClick={this.sortAlphabetically}>
+              Sort alphabetically
+            </button>
+          )}
+        {isStarted
+          && <button type="button" onClick={this.resetSort}>Reset</button>}
+        {isStarted
+          && (
+            <button type="button" onClick={this.sortByLength}>
+              Sort by length
+            </button>
+          )}
+        {isStarted && <GoodsList goods={goodsCopy} />}
+      </div>
+    );
+  }
+}
 
 export default App;
