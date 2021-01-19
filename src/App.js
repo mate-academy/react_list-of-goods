f9a0cbc51c5b95ceaa52f9c0c47c4633cfa9af1c
Maryna Zinchenko <pandoz16@gmail.com>
2021-01-19 14:24:18 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classnames';
+import { GoodsList } from './components/GoodsList';
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +16,94 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.PureComponent {
+  state = {
+    hideButton: false,
+    showList: true,
+    isReversed: false,
+    sortBy: '',
+  }
+
+  startButton = () => {
+    this.setState(prevState => ({
+      hideButton: !prevState.hideButton,
+      showList: false,
+    }));
+  }
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  }
+
+  sortAlph = () => {
+    this.setState({ sortBy: 'alphabet' });
+  }
+
+  sortLength = () => {
+    this.setState({ sortBy: 'length' });
+  }
+
+  reset = () => {
+    this.setState({
+      sortBy: '', isReversed: false,
+    });
+  }
+
+  render() {
+    const { isReversed, sortBy, showList, hideButton } = this.state;
+
+    return (
+      <div className="App box has-text-centered">
+        <h1 className="title is-1">Goods</h1>
+        <GoodsList
+          showList={showList}
+          goods={goodsFromServer}
+          isReversed={isReversed}
+          sortBy={sortBy}
+        />
+        <div className="has-text-centered">
+          <button
+            type="button"
+            className={classNames('button is-primary is-light m-2',
+              { hidden: hideButton })}
+            onClick={this.startButton}
+          >
+            Start
+          </button>
+          <button
+            type="button"
+            onClick={this.reverse}
+            className="button is-link is-light m-2"
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            onClick={this.sortAlph}
+            className="button is-success is-light m-2"
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            onClick={this.reset}
+            className="button is-danger is-light m-2"
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            onClick={this.sortLength}
+            className="button is-warning is-light m-2"
+          >
+            Sort by length
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+export class GoodsList extends React.PureComponent {
+  render() {
+    const { goods, isReversed, sortBy, showList } = this.props;
+    const goodsList = [...goods];
+
+    goodsList.sort((g1, g2) => {
+      switch (sortBy) {
+        case 'alphabet':
+          return g1.localeCompare(g2);
+
+        case 'length':
+          return g1.length - g2.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      goodsList.reverse();
+    }
+
+    return (
+      <ul className={classNames({ hidden: showList })}>
+        {goodsList.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
+  isReversed: PropTypes.bool.isRequired,
+  sortBy: PropTypes.string.isRequired,
+  showList: PropTypes.bool.isRequired,
+};
