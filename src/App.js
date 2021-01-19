d15f6924e62cf9d7b9a7ade26bb2c9b23ad81f32
5Mountains <V.Piatyhor@gmail.com>
2021-01-20 00:08:14 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +15,108 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    showContent: false,
+    goods: goodsFromServer,
+  }
+
+  showingContent = () => {
+    this.setState({ showContent: true });
+  }
+
+  reversing = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  };
+
+  lengthSorting = () => {
+    this.setState(state => ({
+      goods: [...state.goods]
+        .sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  alphabetSorting = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort(),
+    }));
+  }
+
+  resetting = () => {
+    this.setState({ goods: goodsFromServer });
+  }
+
+  render() {
+    const { showContent, goods } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          type="button"
+          className={
+            classNames('button button__start',
+              { hidden: showContent === true })
+          }
+          onClick={this.showingContent}
+        >
+          START
+        </button>
+        <div className="content">
+          {
+            showContent
+              ? (
+                <>
+                  <h1>Goods:</h1>
+
+                  <ul>
+                    {goods.map(item => (
+                      <li key={item}>{item}</li>
+                    ))}
+                  </ul>
+
+                  <div className="buttons">
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={this.reversing}
+                    >
+                      Reverse
+                    </button>
+
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={this.alphabetSorting}
+                    >
+                      Sort alphabetically
+                    </button>
+
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={this.resetting}
+                    >
+                      Reset
+                    </button>
+
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={this.lengthSorting}
+                    >
+                      Sort by length
+                    </button>
+                  </div>
+                </>
+              )
+              : null
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
