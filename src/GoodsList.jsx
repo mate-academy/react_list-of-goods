7461bc8dca8d458b21341fbea4780147f15a4832
NikitaTudakov <tudakov.nikita@gmail.com>
2021-01-19 16:31:30 +0200

    added type attribute

--- src/GoodsList.jsx
+++ src/GoodsList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes, { string } from 'prop-types';
 
 export class GoodsList extends React.Component {
-
   state = {
     isReversed: false,
     sortBy: '',
@@ -10,7 +9,7 @@ export class GoodsList extends React.Component {
 
   reverse = () => {
     this.setState(state => ({
-      isReversed: !state.isReversed
+      isReversed: !state.isReversed,
     }));
   }
 
@@ -18,48 +17,72 @@ export class GoodsList extends React.Component {
     this.setState({
       isReversed: false,
       sortBy: '',
-    })
+    });
   }
 
   sortByName = () => {
-    this.setState({ sortBy: 'Alphabet' })
+    this.setState({ sortBy: 'Alphabet' });
   }
 
   sortByLength = () => {
-    this.setState({ sortBy: 'Length' })
+    this.setState({ sortBy: 'Length' });
   }
 
   render() {
-    const { isReversed,sortBy } = this.state;
+    const { isReversed, sortBy } = this.state;
     const { goods } = this.props;
     const copy = [...goods];
 
-    copy.sort((g1,g2) => {
-      switch(sortBy){
+    copy.sort((g1, g2) => {
+      switch (sortBy) {
         case 'Alphabet':
           return g1.localeCompare(g2);
 
-          case 'Length':
-            return g1.length - g2.length;
-        
+        case 'Length':
+          return g1.length - g2.length;
+
         default:
           return 0;
       }
     });
 
-    if(isReversed) {
-      copy.reverse()
-    };
+    if (isReversed) {
+      copy.reverse();
+    }
 
     return (
       <>
-        <button className ='btn btn_list' onClick={this.reverse}>Reverse</button>
-        <button className ='btn btn_list' onClick={this.sortByName}>Sort by name</button>
-        <button className ='btn btn_list' onClick={this.reset}>Reset</button>
-        <button className ='btn btn_list' onClick={this.sortByLength}>Sort by length</button>
-        <ul className='list'>
+        <button
+          type="button"
+          className="btn btn_list"
+          onClick={this.reverse}
+        >
+          Reverse
+        </button>
+        <button
+          type="button"
+          className="btn btn_list"
+          onClick={this.sortByName}
+        >
+          Sort by name
+        </button>
+        <button
+          type="button"
+          className="btn btn_list"
+          onClick={this.reset}
+        >
+          Reset
+        </button>
+        <button
+          type="button"
+          className="btn btn_list"
+          onClick={this.sortByLength}
+        >
+          Sort by length
+        </button>
+        <ul className="list">
           {copy.map(good => (
-            <li className='list__item' key={good}>{good}</li>
+            <li className="list__item" key={good}>{good}</li>
           ))}
         </ul>
       </>
@@ -69,4 +92,4 @@ export class GoodsList extends React.Component {
 
 GoodsList.propTypes = {
   goods: PropTypes.arrayOf(string).isRequired,
-}
+};
