88d5e0b49872fbdd4a5b00a8b0c7c6910851a462
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-01-20 14:40:28 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GoodsList from './Components/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,128 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    // eslint-disable-next-line react/no-unused-state
+    goodsdatas: goodsFromServer,
+    // eslint-disable-next-line react/no-unused-state
+    start: true,
+    isReversed: false,
+    sortBy: 'alphabet',
+  }
+
+  start = () => {
+    // eslint-disable-next-line react/no-unused-state
+    this.setState({ start: false });
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortAlphabetically = () => {
+    this.setState({
+      sortBy: 'alphabet',
+      isReversed: false,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      sortBy: 'reset',
+      isReversed: false,
+    });
+  };
+
+  sortByLength = () => {
+    this.setState({
+      sortBy: 'length',
+      isReversed: false,
+    });
+  };
+
+  render() {
+    const { goodsdatas, start, isReversed, sortBy } = this.state;
+
+    const goodsCopy = [...goodsdatas];
+
+    goodsCopy.sort((a, b) => {
+      switch (sortBy) {
+        case 'alphabet':
+          return a.localeCompare(b);
+
+        case 'length':
+          return a.length - b.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      goodsCopy.reverse();
+    }
+
+    return (
+      <div className="App">
+        <button
+          className={start
+            ? 'button__start-visible'
+            : 'button__start-unvisible'
+          }
+          type="button"
+          onClick={this.start}
+        >
+          Start
+        </button>
+        <div
+          className={start
+            ? 'filter-unvisible'
+            : 'filter-visible'
+          }
+        >
+          <h1>Goods:</h1>
+          <GoodsList
+            goods={goodsCopy}
+          />
+
+          <button
+            className="button__reverse"
+            type="button"
+            onClick={this.reverse}
+          >
+            Reverse
+          </button>
+
+          <button
+            className="button__reverse"
+            type="button"
+            onClick={this.sortAlphabetically}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            className="button__reverse"
+            type="button"
+            onClick={this.reset}
+          >
+            Reset
+          </button>
+
+          <button
+            className="button__reverse"
+            type="button"
+            onClick={this.sortByLength}
+          >
+            Sort by length
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
