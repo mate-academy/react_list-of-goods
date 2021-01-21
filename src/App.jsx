d1a48654f2f07fead67d8a9ca92f0d50885abdc8
KirillLutsenko <mnrovski68@gmail.com>
2021-01-21 14:24:25 +0200

    transfer of  all the logic, all operations, and all methods on the upper level & use array method for filling the arrays with some values

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,13 @@ const goodsFromServer = [
   'Garlic',
 ];
 
+const selectItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 class App extends React.Component {
   state = {
     isStartButtonVisible: true,
+    goods: goodsFromServer,
+    selectedValue: 1,
   }
 
   showGoodsList = () => {
@@ -27,14 +31,108 @@ class App extends React.Component {
     });
   }
 
+  reverseGoodsList = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  }
+
+  sortAlphabetically = () => {
+    const sortCallback = (a, b) => a.localeCompare(b);
+
+    this.setState(state => ({
+      goods: [...state.goods].sort(sortCallback),
+    }));
+  }
+
+  sortByLength = () => {
+    const sortCallback = (a, b) => a.length - b.length;
+
+    this.setState(state => ({
+      goods: [...state.goods].sort(sortCallback),
+    }));
+  }
+
+  resetMethod = () => {
+    this.setState({
+      goods: goodsFromServer,
+      selectedValue: 1,
+    });
+  }
+
+  filterByLength = (event) => {
+    const targetValue = Number(event.target.value);
+
+    this.setState({
+      goods: goodsFromServer.filter(good => good.length >= targetValue),
+      selectedValue: targetValue,
+    });
+  }
+
   render() {
-    const { isStartButtonVisible } = this.state;
+    const { isStartButtonVisible, selectedValue } = this.state;
 
     return (
       <div className="app">
         {isStartButtonVisible
-          ? <Start showGoodsList={this.showGoodsList} />
-          : <GoodsList goods={goodsFromServer} />
+          ? (<Start showGoodsList={this.showGoodsList} />)
+          : (
+            <div>
+              <GoodsList goods={this.state.goods} />
+
+              <div className="nav">
+                <button
+                  className="button is-primary is-outlined"
+                  type="button"
+                  onClick={this.reverseGoodsList}
+                >
+                  Reverse
+                </button>
+
+                <button
+                  className="button is-primary is-outlined"
+                  type="button"
+                  onClick={this.sortAlphabetically}
+                >
+                  Sort by Alphabet
+                </button>
+
+                <button
+                  className="button is-primary is-outlined"
+                  type="button"
+                  onClick={this.sortByLength}
+                >
+                  Sort by Length
+                </button>
+
+                <button
+                  className="button is-primary is-outlined"
+                  type="button"
+                  onClick={this.resetMethod}
+                >
+                  Reset
+                </button>
+
+                <div className="field">
+                  <div className="control">
+                    <div className="select is-primary">
+                      <select
+                        className="select"
+                        value={selectedValue}
+                        onChange={this.filterByLength}
+                      >
+                        {selectItems.map(item => (
+                          <option value={item} key={item}>
+                            {item}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
         }
       </div>
     );
