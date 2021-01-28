a15afe6622293ed6ce2a15bdb9e1449b0a0b5d2b
Olga Rubalska <olga.anikeieva@gmail.com>
2021-01-28 16:34:43 +0200

    solution fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-alert */
 /* eslint-disable max-len */
 import React from 'react';
 import './App.css';
-import classNames from 'classnames';
+import cn from 'classnames';
 import { List } from './List';
 
 const goodsFromServer = [
@@ -40,13 +39,15 @@ class App extends React.Component {
 
   sortByAlpha = () => {
     this.setState(state => ({
-      visibleGoods: [...state.visibleGoods].sort((productA, productB) => productA.localeCompare(productB)),
+      visibleGoods: [...state.visibleGoods]
+        .sort((productA, productB) => productA.localeCompare(productB)),
     }));
   }
 
   sortByLength = () => {
     this.setState(state => ({
-      visibleGoods: [...state.visibleGoods].sort((productA, productB) => productA.length - productB.length),
+      visibleGoods: [...state.visibleGoods]
+        .sort((productA, productB) => productA.length - productB.length),
     }));
   }
 
@@ -82,13 +83,13 @@ class App extends React.Component {
         <List goods={visibleGoods} />
         <div className="Container">
           <button
-            className={classNames({ Action: displayClass === true })}
+            className={cn({ Action: displayClass === true })}
             type="button"
             onClick={this.showGoods}
           >
             ShowList
           </button>
-          <div className={classNames({ Action: displayClass === false })}>
+          <div className={cn({ Action: displayClass === false })}>
             <button
               type="button"
               onClick={this.reverse}
@@ -119,16 +120,14 @@ class App extends React.Component {
               <label>
                 Pick the length of the product:
                 <select value={this.state.value} onChange={this.handleChange}>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
+                  {[...Array(11).keys()].slice(1).map(number => (
+                    <option
+                      key={number}
+                      value={number}
+                    >
+                      {number}
+                    </option>
+                  ))}
                 </select>
               </label>
               <input type="submit" value="Submit" />
