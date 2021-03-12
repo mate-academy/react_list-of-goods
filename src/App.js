5c6d4f0caf070a89d82b6db23652842b0c6f8a0c
antoniakstepan <antoniakstepan1@gmail.com>
2021-03-12 14:24:43 +0200

    some Change

--- src/App.js
+++ src/App.js
@@ -15,16 +15,6 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Goods</h1>
-//         <Goods goods={goodsFromServer} />
-//       </div>
-//     );
-//   }
-// }
 function App() {
   return (
     <div className="App">
