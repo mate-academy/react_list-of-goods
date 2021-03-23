d69411d9b680a83242c44ee747f13d82a63fd7af
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-23 13:31:33 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -14,11 +14,103 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    visibleGoods: [],
+    isVisible: true,
+  };
+
+  listVisible = () => {
+    this.setState(state => ({
+      visibleGoods: [...goodsFromServer],
+      isVisible: false,
+    }));
+  }
+
+  reverseList = () => {
+    this.setState(state => ({
+      visibleGoods: state.visibleGoods.reverse(),
+    }));
+  }
+
+  sortByName = () => {
+    this.setState(state => ({
+      visibleGoods: state.visibleGoods.sort(
+        (current, next) => current.localeCompare(next),
+      ),
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(state => ({
+      visibleGoods: state.visibleGoods.sort(
+        (current, next) => current.length - next.length,
+      ),
+    }));
+  }
+
+  resetList = () => {
+    this.setState(state => ({
+      visibleGoods: [...goodsFromServer],
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+
+        <ul className="list">
+          {this.state.visibleGoods.map(good => (
+            <li key="good" className="list__item">
+              {good}
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          className={this.state.isVisible || 'hidden'}
+          onClick={this.listVisible}
+        >
+          Start
+        </button>
+
+        <button
+          type="button"
+          className={!this.state.isVisible || 'hidden'}
+          onClick={this.reverseList}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          className={!this.state.isVisible || 'hidden'}
+          onClick={this.sortByName}
+        >
+          Sort by Name
+        </button>
+
+        <button
+          type="button"
+          className={!this.state.isVisible || 'hidden'}
+          onClick={this.sortByLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className={!this.state.isVisible || 'hidden'}
+          onClick={this.resetList}
+        >
+          Reset
+        </button>
+
+      </div>
+    );
+  }
+}
 
 export default App;
