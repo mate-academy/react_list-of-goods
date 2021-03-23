ce5198ba02d10a6876b89d25fd091ef3bdb489bb
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-23 11:48:19 +0200

    first solution list of goods

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import { GoodsList } from './GoodsList';
+
 const goodsFromServer = [
   'Dumplings',
   'Carrot',
@@ -14,11 +16,96 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    isVissibleList: true,
+    listGoods: goodsFromServer,
+  }
 
-export default App;
+  toggleSwitchList = () => {
+    this.setState(prevState => ({
+      isVissibleList: !prevState.isVissibleList,
+    }));
+  }
+
+  sortAlphabetically = () => {
+    this.setState(prevState => ({
+      listGoods: prevState.listGoods.sort(
+        (current, next) => current.localeCompare(next),
+      ),
+    }));
+  }
+
+  reverse = () => {
+    this.setState(prevState => ({
+      listGoods: [...prevState.listGoods].reverse(),
+    }));
+  }
+
+  SortByLength = () => {
+    this.setState(prevState => ({
+      listGoods: prevState.listGoods.sort(
+        (current, next) => current.length - next.length,
+      ),
+    }));
+  }
+
+  reset = () => {
+    this.setState({
+      listGoods: [...goodsFromServer],
+    });
+  }
+
+  render() {
+    const { isVissibleList, listGoods } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        <button
+          type="button"
+          className="button"
+          onClick={this.toggleSwitchList}
+        >
+          Show / Hide List
+        </button>
+        {
+          isVissibleList
+            && (
+            <div className="goods">
+              <GoodsList goods={listGoods} />
+              <button
+                type="button"
+                className="button"
+                onClick={this.reverse}
+              >
+                Reverse
+              </button>
+              <button
+                type="button"
+                className="button"
+                onClick={this.sortAlphabetically}
+              >
+                Sort alphabetically
+              </button>
+              <button
+                type="button"
+                className="button"
+                onClick={this.SortByLength}
+              >
+                Sort by length
+              </button>
+              <button
+                type="button"
+                className="button"
+                onClick={this.reset}
+              >
+                Reset
+              </button>
+            </div>
+            )
+        }
+      </div>
+    );
+  }
+}
