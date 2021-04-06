2a47519149a727ace4a2db63fd512948b81a0bc6
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-04-06 19:22:26 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
+import { GoodList } from './components/GoodList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -12,13 +14,89 @@ const goodsFromServer = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map(item => ({
+  id: uuidv4(),
+  nameGoods: item,
+}
+));
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    goodsList: goodsFromServer,
+    mainButton: true,
+    buttonSort: true,
+    selectDefault: 1,
+  }
 
-export default App;
+  mainButtonClick = () => (
+    this.setState(prev => ({
+      mainButton: !prev.mainButton,
+    }))
+  )
+
+  buttonReverse = () => (
+    this.setState(prev => ({
+      goodsList: [...prev.goodsList].reverse(),
+    }))
+  )
+
+  buttonSort= () => (
+    this.setState(prev => ({
+      goodsList: [...prev.goodsList].sort((goodA, goodB) => (
+        (prev.buttonSort)
+          ? goodA.nameGoods.localeCompare(goodB.nameGoods)
+          : goodB.nameGoods.localeCompare(goodA.nameGoods)
+      )),
+      buttonSort: !prev.buttonSort,
+    }))
+  )
+
+  buttonReset = () => (
+    this.setState({
+      goodsList: goodsFromServer,
+    })
+  )
+
+  buttonSortByLength = () => (
+    this.setState(prev => ({
+      goodsList: [...prev.goodsList].sort((goodA, goodB) => (
+        (prev.buttonSort)
+          ? goodA.nameGoods.length - goodB.nameGoods.length
+          : goodB.nameGoods.length - goodA.nameGoods.length
+      )),
+      buttonSort: !prev.buttonSort,
+    }))
+  )
+
+  buttonSelect = (event) => {
+    const length = event.target.value;
+
+    this.setState({
+      selectDefault: length,
+      goodsList: [...goodsFromServer]
+        .filter(e => e.nameGoods.length >= +length),
+    });
+  }
+
+  render() {
+    const { goodsList, mainButton, selectDefault } = this.state;
+
+    return (
+      <div>
+        <button type="button" onClick={this.mainButtonClick}>
+          {(mainButton) ? 'start' : 'close'}
+        </button>
+        <GoodList
+          goodsList={goodsList}
+          mainButton={mainButton}
+          buttonReverse={this.buttonReverse}
+          buttonSort={this.buttonSort}
+          buttonReset={this.buttonReset}
+          buttonSortByLength={this.buttonSortByLength}
+          buttonSelect={this.buttonSelect}
+          selectDefault={selectDefault}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/GoodList.jsx
@@ -0,0 +1,51 @@
+import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const countSelect = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const GoodList = React.memo(
+  ({
+    // eslint-disable-next-line react/prop-types
+    goodsList,
+    mainButton,
+    buttonReverse,
+    buttonSort,
+    buttonReset,
+    buttonSortByLength,
+    buttonSelect,
+    selectDefault,
+  }) => (
+    <div hidden={mainButton}>
+      {
+        // eslint-disable-next-line react/prop-types
+        goodsList.map(good => (
+          <div key={good.id}>{good.nameGoods}</div>
+        ))
+      }
+      <button type="button" onClick={buttonReverse}>reverse</button>
+      <button type="button" onClick={buttonSort}>sort</button>
+      <button type="button" onClick={buttonReset}>reset</button>
+      <button type="button" onClick={buttonSortByLength}>Sort by length</button>
+      <select value={selectDefault} onChange={buttonSelect}>
+        {
+        countSelect.map((element, index) => (
+          <option value={index + 1} key={uuidv4()}>
+            {element}
+          </option>
+        ))
+      }
+      </select>
+    </div>
+  ),
+);
+
+GoodList.propTypes = {
+  mainButton: PropTypes.bool.isRequired,
+  buttonReverse: PropTypes.func.isRequired,
+  buttonSort: PropTypes.func.isRequired,
+  buttonReset: PropTypes.func.isRequired,
+  buttonSortByLength: PropTypes.func.isRequired,
+  buttonSelect: PropTypes.func.isRequired,
+  selectDefault: PropTypes.number.isRequired,
+};
