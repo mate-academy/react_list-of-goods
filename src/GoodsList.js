ace10612bf6e1e45bfe9a87571f7c9ef69366bcf
Georgy Dzumenko <erterj05@gmail.com>
2021-04-07 16:50:55 +0300

    Solution

--- src/GoodsList.js
+++ src/GoodsList.js
@@ -5,26 +5,49 @@ import './App.css';
 export class GoodsList extends React.Component {
   state = {
     goods: [...this.props.goodsFromServer],
+    sortBy: '',
+    reversed: false,
   }
 
   render() {
     const reverse = () => {
-      this.setState(state => ({
-        goods: state.goods.reverse(),
-      }));
+      this.setState({ reversed: true });
     };
 
     const sortAlphabetically = () => {
-      this.setState(state => ({
-        goods: state.goods.sort(),
-      }));
+      this.setState({ sortBy: 'Alphabetically' });
     };
 
     const reset = () => {
       this.setState({
         goods: [...this.props.goodsFromServer],
+        sortBy: '',
+        reversed: false,
       });
     };
+
+    const sortByLength = () => {
+      this.setState({ sortBy: 'ByLength' });
+    };
+
+    if (this.state.reversed) {
+      this.state.goods.reverse();
+    }
+
+    this.state.goods.sort(
+      (thing1, thing2) => {
+        switch (this.state.sortBy) {
+          case 'ByLength':
+            return thing1.length - thing2.length;
+
+          case 'Alphabetically':
+            return thing1.localeCompare(thing2);
+
+          default:
+            return 0;
+        }
+      },
+    );
 
     return (
       <>
@@ -39,6 +62,12 @@ export class GoodsList extends React.Component {
           onClick={sortAlphabetically}
         >
           Sort Alphabetically
+        </button>
+        <button
+          type="button"
+          onClick={sortByLength}
+        >
+          Sort By Length
         </button>
         <button
           type="button"
