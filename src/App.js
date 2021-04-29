75f71deada26a48d8aa4f76c9bdea2c6c428f007
Yevgeniy Podobreev <podobr_2018@ukr.net>
2021-04-30 01:35:32 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GoodsList from './GoodsList';
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +15,116 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    products: goodsFromServer,
+    isReversed: false,
+    sortBy: '',
+    isNotVisible: true,
+  }
+
+  visibility = () => {
+    this.setState({ isNotVisible: false });
+  }
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  }
+
+  sortAlphabetically = () => {
+    this.setState({ sortBy: 'name' });
+  }
+
+  sortLength = () => {
+    this.setState({ sortBy: 'length' });
+  }
+
+  reset = () => {
+    this.setState({
+      products: goodsFromServer,
+      isReversed: false,
+      sortBy: '',
+    });
+  }
+
+  render() {
+    const { products, isReversed, sortBy, isNotVisible } = this.state;
+
+    const productsCopy = [...products];
+
+    productsCopy.sort((productOne, productTwo) => {
+      switch (sortBy) {
+        case 'name':
+          return productOne.localeCompare(productTwo);
+        case 'length':
+          return productOne.length - productTwo.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      productsCopy.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {goodsFromServer.length}
+
+        <p>
+          <button
+            type="button"
+            onClick={this.visibility}
+            hidden={!isNotVisible}
+          >
+            Start
+          </button>
+        </p>
+
+        <GoodsList
+          products={productsCopy}
+          isNotVisible={isNotVisible}
+        />
+
+        <p>
+          <button
+            type="button"
+            onClick={this.reverse}
+            hidden={isNotVisible}
+          >
+            Reverse
+          </button>
+
+          <button
+            type="button"
+            onClick={this.sortAlphabetically}
+            hidden={isNotVisible}
+          >
+            Sort Alphabetically
+          </button>
+
+          <button
+            type="button"
+            onClick={this.sortLength}
+            hidden={isNotVisible}
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            onClick={this.reset}
+            hidden={isNotVisible}
+          >
+            Reset
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
