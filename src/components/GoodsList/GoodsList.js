9c4c4409a5a4ebce57471bcaf9e1514e2b874814
Anton Kostylov <antoniok003@gmail.com>
2021-05-23 15:09:49 +0300

    fix css

---
+++ src/components/GoodsList/GoodsList.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './GoodsList.css';
+
+export class GoodsList extends Component {
+  state ={
+    hidden: true,
+    reverse: false,
+    sortBy: '',
+    goodsSelector: 1,
+  }
+
+  hideButton = () => {
+    this.setState({
+      hidden: false,
+    });
+  }
+
+  reverseGoods = () => {
+    this.setState(state => ({
+      reverse: !state.reverse,
+    }));
+  }
+
+  sortGoods = () => {
+    this.setState(state => ({
+      sort: !state.sort,
+    }));
+  }
+
+  resetGoods = () => {
+    this.setState({
+      reverse: false,
+      sortBy: '',
+      goodsSelector: 1,
+    });
+  }
+
+  render() {
+    const { goods } = this.props;
+    const { hidden, reverse, sortBy, goodsSelector } = this.state;
+    const modifiedGoods = goods.filter(good => good.length >= goodsSelector);
+
+    modifiedGoods.sort((goodA, goodB) => {
+      switch (sortBy) {
+        case 'alphabet':
+          return goodA.localeCompare(goodB);
+
+        case 'length':
+          return goodA.length - goodB.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    if (reverse) {
+      modifiedGoods.reverse();
+    }
+
+    return (
+      <>
+        <ul
+          className={classNames('GoodsList', {
+            'GoodsList--hide': hidden,
+          })}
+        >
+          {modifiedGoods.map(good => (
+            <li key={good}>
+              {good}
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          className={classNames('GoodsList__toggleButton', {
+            'GoodsList__toggleButton--hide': !hidden,
+          })}
+          onClick={this.hideButton}
+        >
+          Start
+        </button>
+        <button
+          type="button"
+          className={classNames('GoodsList__modifieButton', {
+            'GoodsList__modifieButton--hide': hidden,
+          })}
+          onClick={this.reverseGoods}
+        >
+          Reverse
+        </button>
+        <button
+          type="button"
+          className={classNames('GoodsList__modifieButton', {
+            'GoodsList__modifieButton--hide': hidden,
+          })}
+          onClick={() => {
+            this.setState({
+              sortBy: 'alphabet',
+            });
+          }}
+        >
+          Sort alphabetically
+        </button>
+        <button
+          type="button"
+          className={classNames('GoodsList__modifieButton', {
+            'GoodsList__modifieButton--hide': hidden,
+          })}
+          onClick={() => {
+            this.setState({
+              sortBy: 'length',
+            });
+          }}
+        >
+          Sort by length
+        </button>
+        <button
+          type="button"
+          className={classNames('GoodsList__modifieButton', {
+            'GoodsList__modifieButton--hide': hidden,
+          })}
+          onClick={this.resetGoods}
+        >
+          Reset
+        </button>
+        <select
+          className={classNames('GoodsList__modifieButton', {
+            'GoodsList__modifieButton--hide': hidden,
+          })}
+          defaultValue={goodsSelector}
+          onChange={({ target }) => {
+            this.setState({
+              goodsSelector: target.value,
+            });
+          }}
+          value={goodsSelector}
+        >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+        </select>
+      </>
+    );
+  }
+}
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
