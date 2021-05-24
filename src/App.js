b6dce9b00f11f2af857e3b751636cecfee1f3690
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-05-24 20:42:53 +0300

    Solution v0.1 without advanced task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.css';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +16,120 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: goodsFromServer,
+    isVisible: false,
+    isReversed: false,
+    sortBy: '',
+  };
+
+  showList = () => {
+    this.setState(state => ({
+      isVisible: !state.isVisible,
+    }));
+  };
+
+  reverseList = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  }
+
+  reset = () => {
+    this.setState({
+      isReversed: false,
+      sortBy: '',
+    });
+  }
+
+  sortByAlphabet = () => {
+    this.setState({
+      sortBy: 'alphabet',
+    });
+  }
+
+  sortByLength = () => {
+    this.setState({
+      sortBy: 'length',
+    });
+  }
+
+  render() {
+    const { goods, isVisible, isReversed, sortBy } = this.state;
+    const sortedGoods = goods.filter(item => item);
+
+    sortedGoods.sort((a, b) => {
+      switch (sortBy) {
+        case 'alphabet':
+          return a.localeCompare(b);
+        case 'length':
+          return a.length - b.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      sortedGoods.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {isVisible === false
+          ? (
+            <button
+              type="button"
+              onClick={this.showList}
+            >
+              Start
+            </button>
+          )
+          : (
+            <>
+              <button
+                className={classNames(`btn`, {
+                  active: isReversed,
+                })}
+                type="button"
+                onClick={this.reverseList}
+              >
+                Reverse
+              </button>
+              <button
+                className={classNames(`btn`, {
+                  active: sortBy === 'alphabet',
+                })}
+                type="button"
+                onClick={this.sortByAlphabet}
+              >
+                Sort by alphabet
+              </button>
+              <button
+                className={classNames(`btn`, {
+                  active: sortBy === 'length',
+                })}
+                type="button"
+                onClick={this.sortByLength}
+              >
+                Sort by length
+              </button>
+              <button
+                className="btn"
+                type="button"
+                onClick={this.reset}
+              >
+                Reset
+              </button>
+            </>
+          )}
+
+        {isVisible && <GoodsList goods={sortedGoods} />}
+
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/GoodsList/GoodsList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoodsList = ({ goods }) => (
+  <ul>
+    {goods.map(item => (
+      <li key={item}>
+        {item}
+      </li>
+    ))}
+  </ul>
+);
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
