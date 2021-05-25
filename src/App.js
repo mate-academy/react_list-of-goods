b3a14de32b930a1d5c631641c9508110ffdb297e
Sergey Zub <vook1996@gmail.com>
2021-05-25 20:33:03 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GoodsList from './GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +15,85 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+    displayList: false,
+    arrayList: [...goodsFromServer],
+  };
+
+  reverseList = () => {
+    this.setState(state => ({
+      arrayList: [...state.goods].reverse(),
+    }));
+  };
+
+  sortList = () => {
+    this.setState(state => ({
+      arrayList: [...state.goods].sort(),
+    }));
+  };
+
+  sortByLengthList = () => {
+    this.setState(state => ({
+      arrayList: [...state.goods].sort((a, b) => a.length - b.length),
+    }));
+  };
+
+  render() {
+    const { goods, displayList, arrayList } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {displayList === false && (
+        <button
+          type="button"
+          onClick={() => {
+            this.setState({
+              displayList: true,
+            });
+          }}
+        >
+          Start
+        </button>
+        )}
+
+        {displayList === true && (
+        <>
+          <button type="button" onClick={this.reverseList}>Reverse</button>
+
+          <button
+            type="button"
+            onClick={this.sortList}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            onClick={this.sortByLengthList}
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({
+                arrayList: [...goods],
+              });
+            }}
+          >
+            Reset
+          </button>
+        </>
+        )}
+
+        {displayList && <GoodsList goods={arrayList} />}
+      </div>
+    );
+  }
+}
 
 export default App;
