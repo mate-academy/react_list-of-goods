d08ec0ee18b7445116ae4820ccd63847268f9c30
iryna-vys <vys.iryna@gmail.com>
2021-05-25 23:23:58 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GoodsList } from './components/GoodsList/GoodsList';
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +15,95 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: [...goodsFromServer],
+    isListShown: false,
+  };
+
+  showList= () => {
+    this.setState(state => ({
+      isListShown: !state.isListShown,
+    }));
+  }
+
+  reverse = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  }
+
+  sortAlphabetically = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.localeCompare(b)),
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  reset = () => {
+    this.setState(state => ({
+      goods: [...goodsFromServer],
+    }));
+  }
+
+  render() {
+    const { isListShown, goods } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          type="button"
+          onClick={this.showList}
+          style={(isListShown)
+            ? { visibility: 'hidden' }
+            : { visibility: 'visible' }
+          }
+        >
+          Start
+        </button>
+        {(!isListShown)
+          ? ''
+          : (
+            <>
+              <h1>
+                Goods:
+              </h1>
+              <GoodsList goods={goods} />
+              <button
+                type="button"
+                onClick={this.reverse}
+              >
+                Reverse
+              </button>
+              <button
+                type="button"
+                onClick={this.sortAlphabetically}
+              >
+                Sort alphabetically
+              </button>
+              <button
+                type="button"
+                onClick={this.sortByLength}
+              >
+                Sort by length
+              </button>
+              <button
+                type="button"
+                onClick={this.reset}
+              >
+                Reset
+              </button>
+            </>
+          )
+        }
+      </div>
+    );
+  }
+}
 
 export default App;
