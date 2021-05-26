771177035f764c9b36b60d7ef30afa4c09f336da
iryna-vys <vys.iryna@gmail.com>
2021-05-26 12:06:52 +0300

    Fixes after review

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ const goodsFromServer = [
 
 class App extends React.Component {
   state = {
-    goods: [...goodsFromServer],
     isListShown: false,
   };
 
@@ -27,79 +26,20 @@ class App extends React.Component {
     }));
   }
 
-  reverse = () => {
-    this.setState(state => ({
-      goods: [...state.goods].reverse(),
-    }));
-  }
-
-  sortAlphabetically = () => {
-    this.setState(state => ({
-      goods: [...state.goods].sort((a, b) => a.localeCompare(b)),
-    }));
-  }
-
-  sortByLength = () => {
-    this.setState(state => ({
-      goods: [...state.goods].sort((a, b) => a.length - b.length),
-    }));
-  }
-
-  reset = () => {
-    this.setState(state => ({
-      goods: [...goodsFromServer],
-    }));
-  }
-
   render() {
-    const { isListShown, goods } = this.state;
+    const { isListShown } = this.state;
 
     return (
       <div className="App">
-        <button
-          type="button"
-          onClick={this.showList}
-          style={(isListShown)
-            ? { visibility: 'hidden' }
-            : { visibility: 'visible' }
-          }
-        >
-          Start
-        </button>
-        {(!isListShown)
-          ? ''
-          : (
-            <>
-              <h1>
-                Goods:
-              </h1>
-              <GoodsList goods={goods} />
-              <button
-                type="button"
-                onClick={this.reverse}
-              >
-                Reverse
-              </button>
-              <button
-                type="button"
-                onClick={this.sortAlphabetically}
-              >
-                Sort alphabetically
-              </button>
-              <button
-                type="button"
-                onClick={this.sortByLength}
-              >
-                Sort by length
-              </button>
-              <button
-                type="button"
-                onClick={this.reset}
-              >
-                Reset
-              </button>
-            </>
-          )
+        {!isListShown && (
+          <button
+            type="button"
+            onClick={this.showList}
+          >
+            Start
+          </button>
+        )}
+        {isListShown && (<GoodsList goods={goodsFromServer} />)
         }
       </div>
     );
--- src/components/GoodsList/GoodsList.js
+++ src/components/GoodsList/GoodsList.js
@@ -1,17 +1,78 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const GoodsList = React.memo(
-  ({ goods }) => (
-    <ul>
-      {goods.map(good => (
-        <li key={good}>
-          {good}
-        </li>
-      ))}
-    </ul>
-  ),
-);
+export class GoodsList extends React.Component {
+  state = {
+    goods: [...this.props.goods],
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  }
+
+  sortAlphabetically = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.localeCompare(b)),
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.length - b.length),
+    }));
+  }
+
+  reset = () => {
+    this.setState(state => ({
+      goods: [...this.props.goods],
+    }));
+  }
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <>
+        <h1>
+          Goods:
+        </h1>
+        <ul>
+          {goods.map(good => (
+            <li key={good}>
+              {good}
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          onClick={this.reverse}
+        >
+          Reverse
+        </button>
+        <button
+          type="button"
+          onClick={this.sortAlphabetically}
+        >
+          Sort alphabetically
+        </button>
+        <button
+          type="button"
+          onClick={this.sortByLength}
+        >
+          Sort by length
+        </button>
+        <button
+          type="button"
+          onClick={this.reset}
+        >
+          Reset
+        </button>
+      </>
+    );
+  }
+}
 
 GoodsList.propTypes = {
   goods: PropTypes.arrayOf(
