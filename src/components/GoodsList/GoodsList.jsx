24ba8d66c10d79c98c1bc372d3b5b31c550336ac
Vitaliy Buchincik <v.buchinchik@gmail.com>
2021-05-26 17:21:16 +0300

    Solution

---
+++ src/components/GoodsList/GoodsList.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import './GoodsList.css';
+
+class GoodsList extends React.Component {
+  state = {
+    goods: [...this.props.goods],
+  }
+
+  reverseList = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  };
+
+  sortByABC = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort(),
+    }));
+  };
+
+  resetToDefault = () => {
+    this.setState(state => ({
+      goods: [...this.props.goods],
+    }))
+  }
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.length - b.length),
+    }));
+  };
+
+  render() {
+    return(
+      <div>
+        <ul>
+          {this.state.goods.map(good => (
+            <li className="good">
+              <span className="good-item">
+                {good}
+              </span>
+            </li>
+          ))}
+        </ul>
+
+        <button
+          className="button"
+          type="button"
+          onClick={this.reverseList}
+        >
+          Reverse
+        </button>
+
+        <button
+          className="button"
+          type="button"
+          onClick={this.sortByABC}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          className="button"
+          type="button"
+          onClick={this.resetToDefault}
+        >
+          Reset
+        </button>
+
+        <button
+          className="button"
+          type="button"
+          onClick={this.sortByLength}
+        >
+          Sort by length
+        </button>
+      </div>
+    )
+  }
+}
+
+export default GoodsList;
