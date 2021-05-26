369d7ddb03cc344585fbf04723b2830a940d1b21
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-05-26 13:25:49 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const goodsFromServer = [
@@ -14,11 +14,90 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends Component {
+  state = {
+    goods: goodsFromServer,
+    showList: false,
+  }
+
+toggleListHandler = () => {
+  this.setState(state => ({
+    showList: !state.showList,
+  }));
+};
+
+reverseList = () => {
+  this.setState(state => ({
+    goods: [...state.goods].reverse(),
+  }));
+};
+
+sortList = () => {
+  this.setState(state => ({
+    goods: [...state.goods].sort(),
+  }));
+}
+
+resetList = () => {
+  this.setState({
+    goods: goodsFromServer,
+  });
+};
+
+sortByLength = () => {
+  this.setState(state => ({
+    goods: [...state.goods].sort((a, b) => a.length - b.length),
+  }));
+};
+
+render() {
+  return (
+    <div className="app">
+      <h1>Goods</h1>
+      <button
+        className={this.state.showList && 'visibility'}
+        type="button"
+        onClick={this.toggleListHandler}
+      >
+        Start
+      </button>
+      <button
+        className={!this.state.showList && 'visibility'}
+        type="button"
+        onClick={this.reverseList}
+      >
+        Reverse
+      </button>
+      <button
+        className={!this.state.showList && 'visibility'}
+        type="button"
+        onClick={this.sortList}
+      >
+        Sort
+      </button>
+      <button
+        className={!this.state.showList && 'visibility'}
+        type="button"
+        onClick={this.resetList}
+      >
+        Reset
+      </button>
+      <button
+        className={!this.state.showList && 'visibility'}
+        type="button"
+        onClick={this.sortByLength}
+      >
+        Sort by length
+      </button>
+      <ul>
+        {this.state.showList && this.state.goods.map(product => (
+          <li key={product}>{product}</li>
+        ))
+        }
+      </ul>
+    </div>
+  );
+}
+}
 
 export default App;
