058b6673ce10db01ed8f6b20a5157e6ca9d9a6a0
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2021-07-03 13:32:38 +0300

    Reviwed as advised mentor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable max-len */
 import React from 'react';
 import './App.scss';
-import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
-// import { classnames } from classnames;
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@material-ui/core';
+import goodsImgsFromServer from '../../images';
 import { GoodsList } from '../GoodsList/GoodsList';
 
 const goodsFromServer = [
@@ -19,57 +24,6 @@ const goodsFromServer = [
   'Potato',
   'Pear',
   'Watermelon',
-];
-
-const goodsImgsFromServer = [
-  {
-    Potato:
-      'https://st3.depositphotos.com/1967477/13510/v/1600/depositphotos_135100584-stock-illustration-cartoon-potato-giving-thumbs-up.jpg',
-  },
-  {
-    Pear: 'https://st.depositphotos.com/1526816/1348/v/950/depositphotos_13485012-stock-illustration-pear.jpg',
-  },
-  {
-    Watermelon:
-      'https://st3.depositphotos.com/1967477/13510/v/1600/depositphotos_135100662-stock-illustration-cartoon-watermelon-giving-thumbs-up.jpg',
-  },
-  {
-    'Ice cream':
-      'https://st4.depositphotos.com/24038622/27073/v/1600/depositphotos_270733152-stock-illustration-ice-cream-mascot-cartoon-illustration.jpg',
-  },
-  {
-    Apple:
-      'https://static8.depositphotos.com/1497380/1035/v/950/depositphotos_10356035-stock-illustration-green-apple-cartoon-character.jpg',
-  },
-  {
-    Bread:
-      'https://st2.depositphotos.com/23395854/46307/v/1600/depositphotos_463070890-stock-illustration-cute-wheat-loaf-of-bread.jpg',
-  },
-  {
-    Carrot:
-      'https://st2.depositphotos.com/1007168/6107/v/950/depositphotos_61077827-stock-illustration-carrot-showing-muscle-arms.jpg',
-  },
-  {
-    Honey:
-      'https://st2.depositphotos.com/7857468/12441/v/950/depositphotos_124417528-stock-illustration-cartoon-bee-holding-bucket-with.jpg',
-  },
-  {
-    Jam: 'https://st3.depositphotos.com/9034578/34559/v/1600/depositphotos_345599216-stock-illustration-waving-friendly-strawberry-jam-mascot.jpg',
-  },
-  {
-    Garlic:
-      'https://st4.depositphotos.com/7850056/20472/v/1600/depositphotos_204721044-stock-illustration-cute-cartoon-garlic-character-vector.jpg',
-  },
-  {
-    Dumplings:
-      'https://st3.depositphotos.com/34407204/37619/v/1600/depositphotos_376192220-stock-illustration-happy-gyoza-cartoon-character-emoji.jpg',
-  },
-  {
-    Eggs: 'https://static8.depositphotos.com/1472772/978/i/950/depositphotos_9787339-stock-photo-funny-cartoon-egg.jpg',
-  },
-  {
-    Fish: 'https://st2.depositphotos.com/2255567/10039/v/950/depositphotos_100393514-stock-illustration-fish-chef-cartoon.jpg',
-  },
 ];
 
 export class App extends React.Component {
