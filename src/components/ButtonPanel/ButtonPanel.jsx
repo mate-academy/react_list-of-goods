1b9e3d89d326cb1e5b44d2000fb6fb9822e02952
VadymMakevytss <vadymlat@mail.ru>
2021-07-26 18:03:41 +0300

    solution

---
+++ src/components/ButtonPanel/ButtonPanel.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+export default class ButtonPanel extends PureComponent {
+  // state = {
+  //   number: 0,
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Button Panel Component</h1>
+      </div>
+    );
+  }
+}
