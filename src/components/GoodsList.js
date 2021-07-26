aaa07752c255d8a448e4b5d470907a0fd3df796f
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-07-26 20:30:42 +0300

    solution5

--- src/components/GoodsList.js
+++ src/components/GoodsList.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export const GoodsList = ({ goods }) => (
   <ul>
     {goods.map(good => (
-      <li key={Math.random()}>
+      <li key={good}>
         {good}
       </li>
     ))}
