b521f4a8782270a622aa1e5c79f54d5a277d6549
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-07-26 13:24:51 +0300

    solution

---
+++ src/components/GoodsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoodsList = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li key={Math.random()}>
+        {good}
+      </li>
+    ))}
+  </ul>
+);
+
+GoodsList.propTypes = {
+  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
