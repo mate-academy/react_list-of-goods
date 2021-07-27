0a394135ecd350aca359f6986a325b2cf0a13d54
Pavlo Starynskyi <starinskiy@gmail.com>
2021-07-27 17:35:52 +0300

    first try

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
+import { Button } from './components/Button';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +16,77 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    goods: goodsFromServer,
+    isListVisible: false,
+  }
+
+  showGoodsList = () => {
+    this.setState({
+      isListVisible: true,
+    });
+  }
+
+  reverseGoodsList = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  }
+
+  sortByName = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods]
+        .sort((good1, good2) => good1.localeCompare(good2)),
+    }));
+  }
+
+  sortByLength = () => {
+    this.setState(prevState => ({
+      goods: [...prevState.goods]
+        .sort((good1, good2) => good1.length - good2.length),
+    }));
+  }
+
+  resetGoodsList = () => {
+    this.setState({
+      goods: goodsFromServer,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {!this.state.isListVisible
+          && (
+          <Button
+            title="Start"
+            onClick={this.showGoodsList}
+          />
+          )
+        }
+        <div className={!this.state.isListVisible && 'invisibleList'}>
+          <GoodsList goods={this.state.goods} />
+          <Button
+            title="Reverse"
+            onClick={this.reverseGoodsList}
+          />
+          <Button
+            title="Sort by Name"
+            onClick={this.sortByName}
+          />
+          <Button
+            title="Sort by length"
+            onClick={this.sortByLength}
+          />
+          <Button
+            title="Reset"
+            onClick={this.resetGoodsList}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
