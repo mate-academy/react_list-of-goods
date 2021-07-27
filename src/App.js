b8cf376facf6558f946797ea156d3616924ef1e8
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-07-27 15:57:18 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import { ProductList } from './components/ProductList';
+import { Button } from './components/Button';
 
-const goodsFromServer = [
+const productFromServer = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -14,11 +16,82 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.PureComponent {
+  state = {
+    productList: [...productFromServer],
+    isStarted: false,
+  }
+
+  start = () => {
+    this.setState({ isStarted: true });
+  }
+
+  reverse = () => {
+    this.setState(
+      state => ({
+        productList: [...state.productList].reverse(),
+      }),
+    );
+  }
+
+  sortAlphabet = () => {
+    this.setState(
+      state => ({
+        productList: [...state.productList].sort(),
+      }),
+    );
+  }
+
+  reset = () => {
+    this.setState(
+      () => ({
+        productList: [...productFromServer],
+      }),
+    );
+  }
+
+  sortLength = () => {
+    this.setState(
+      state => ({
+        productList: [...state.productList].sort(
+          (a, b) => (a.length - b.length),
+        ),
+      }),
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isStarted ? (
+          <React.Fragment>
+            <ProductList productList={this.state.productList} />
+            <Button
+              text="Reverse"
+              onClick={this.reverse}
+            />
+            <Button
+              text="Sort alphabetically"
+              onClick={this.sortAlphabet}
+            />
+            <Button
+              text="Reset"
+              onClick={this.reset}
+            />
+            <Button
+              text="Sort by length"
+              onClick={this.sortLength}
+            />
+          </React.Fragment>
+        ) : (
+          <Button
+            text="Start"
+            onClick={this.start}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
