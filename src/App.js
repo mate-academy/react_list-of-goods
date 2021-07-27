1cba5d7f1920a58a6c3ecd40e5f2d77d5da36dd8
Leonid <manu31051996@gmail.com>
2021-07-27 17:29:44 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Buttons/Buttons';
+import GoodsList from './components/GoodsList/GoodsList';
 
 const goodsFromServer = [
   'Dumplings',
@@ -14,11 +16,84 @@ const goodsFromServer = [
   'Garlic',
 ];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.PureComponent {
+  state = {
+    goodsList: goodsFromServer,
+    openPage: false,
+  }
+
+  openPage = () => {
+    this.setState({ openPage: true });
+  }
+
+  reverseGoods = () => {
+    this.setState(
+      state => ({
+        goodsList: [...state.goodsList].reverse(),
+      }),
+    );
+  }
+
+  sortByAlphabet = () => {
+    this.setState(
+      state => ({
+        goodsList: [...state.goodsList].sort(
+          (prevGood, nextGood) => (prevGood.localeCompare(nextGood)),
+        ),
+      }),
+    );
+  }
+
+  resetGoods = () => {
+    this.setState({ goodsList: goodsFromServer });
+  }
+
+  sortByLength = () => {
+    this.setState(
+      state => ({
+        goodsList: [...state.goodsList].sort(
+          (prevGood, nextGood) => (prevGood.length - nextGood.length),
+        ),
+      }),
+    );
+  }
+
+  render() {
+    const { goodsList, openPage } = this.state;
+
+    return (
+      <div>
+        {openPage ? (
+          <>
+            <GoodsList goodsList={goodsList} />
+            <div>
+              <Button
+                text="Reverse"
+                onClick={this.reverseGoods}
+              />
+              <Button
+                text="Sort alphabetically"
+                onClick={this.sortByAlphabet}
+              />
+              <Button
+                text="Reset"
+                onClick={this.resetGoods}
+              />
+              <Button
+                text="Sort by length"
+                onClick={this.sortByLength}
+              />
+            </div>
+          </>
+        ) : (
+          <Button
+            text="Start"
+            onClick={this.openPage}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
