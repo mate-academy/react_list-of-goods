b92c20f7d92fd1488ab1ce73313b6b991cb4bfe4
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-08-31 19:57:03 +0300

    Resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RenderList from './RenderList';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,107 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  list: string[]
+  sortBy: string
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    list: [],
+    sortBy: '',
+  };
+
+  showList = () => {
+    this.setState({ list: [...goodsFromServer] });
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      list: state.list.reverse(),
+      sortBy: '',
+    }));
+  };
+
+  sortByAZ = () => {
+    this.setState({ sortBy: 'AZ' });
+  };
+
+  sortByLength = () => {
+    this.setState({ sortBy: 'length' });
+  };
+
+  reset = () => {
+    this.setState({ list: [...goodsFromServer], sortBy: '' });
+  };
+
+  render() {
+    const { list = [''], sortBy } = this.state;
+
+    list.sort((g1, g2) => {
+      switch (sortBy) {
+        case 'AZ':
+          return g1.localeCompare(g2);
+
+        case 'length':
+          return g1.length - g2.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    return (
+      <div className="App">
+        <button
+          type="button"
+          className="button"
+          onClick={this.showList}
+          style={{ display: (list.length > 0) ? 'none' : 'visible' }}
+        >
+          Start
+        </button>
+
+        <RenderList list={this.state.list} />
+
+        <button
+          type="button"
+          className="button"
+          onClick={this.reverse}
+          style={{ visibility: (list.length > 0) ? 'visible' : 'hidden' }}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          className="button"
+          onClick={this.sortByAZ}
+          style={{ visibility: (list.length > 0) ? 'visible' : 'hidden' }}
+        >
+          Sort by A-Z
+        </button>
+
+        <button
+          type="button"
+          className="button"
+          onClick={this.sortByLength}
+          style={{ visibility: (list.length > 0) ? 'visible' : 'hidden' }}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className="button"
+          onClick={this.reset}
+          style={{ visibility: (list.length > 0) ? 'visible' : 'hidden' }}
+        >
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
