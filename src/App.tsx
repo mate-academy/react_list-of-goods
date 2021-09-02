6c4e7a1cd868b49993ae3b63b46abe6ce6ff2a2c
Anna-Tkachenko <tkachenko.anna835@gmail.com>
2021-09-02 18:40:28 +0300

    Created list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,39 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isGoodsListVisible: boolean;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isGoodsListVisible: false,
+  };
+
+  showGoodsList = () => {
+    this.setState({ isGoodsListVisible: true });
+  };
+
+  render() {
+    const { isGoodsListVisible } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">Goods</h1>
+        {isGoodsListVisible
+          ? <GoodsList goods={goodsFromServer} />
+          : (
+            <button
+              type="button"
+              className="btn btn-outline-success"
+              onClick={this.showGoodsList}
+            >
+              Start
+            </button>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
