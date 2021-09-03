ca8cc188084daddafef4cdfa53a9eb17d716a798
VeronikaLushchik <loaaary212@gmail.com>
2021-09-03 12:46:37 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GoodsList } from './components/GoodsList';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +15,39 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isVisible: boolean;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isVisible: false,
+  };
+
+  showGoodsList = () => {
+    this.setState({ isVisible: true });
+  };
+
+  render() {
+    const { isVisible } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Goods List</h1>
+        {isVisible
+          ? <GoodsList goods={goodsFromServer} />
+          : (
+            <button
+              type="button"
+              className="button__start"
+              onClick={this.showGoodsList}
+            >
+              Start
+            </button>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
