c4c62f7a1b01a18ebd5005903c31d212e4f7a273
lera-yaskevych <redrum.back@gmail.com>
2021-09-03 10:25:14 +0300

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from './Button';
+import { GoodsList } from './GoodsList';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +16,102 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component<{}, State> {
+  state: State = {
+    defaultGoods: goodsFromServer,
+    isShown: false,
+    isReversed: false,
+    sortBy: 'index',
+  };
+
+  showList = () => {
+    this.setState(state => ({
+      isShown: !state.isShown,
+    }));
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortAlphabetically = () => {
+    this.setState({ sortBy: 'alphabet' });
+  };
+
+  sortByLength = () => {
+    this.setState({ sortBy: 'length' });
+  };
+
+  reset = () => {
+    this.setState({
+      isReversed: false,
+      sortBy: 'index',
+    });
+  };
+
+  render() {
+    const {
+      defaultGoods,
+      isShown,
+      isReversed,
+      sortBy,
+    } = this.state;
+
+    const visibleGoods = [...defaultGoods];
+
+    visibleGoods.sort((good1, good2) => {
+      switch (sortBy) {
+        case 'alphabet':
+          return good1.localeCompare(good2);
+        case 'length':
+          return good1.length - good2.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      visibleGoods.reverse();
+    }
+
+    return (
+      <div className="App">
+        {isShown ? (
+          <>
+            <GoodsList goods={visibleGoods} />
+            <Button
+              action={this.reverse}
+              stylingClass="App__button--reverse"
+              text="Reverse"
+            />
+            <Button
+              action={this.sortAlphabetically}
+              stylingClass="App__button--sort-abc"
+              text="Sort alphabetically"
+            />
+            <Button
+              action={this.sortByLength}
+              stylingClass="App__button--sort-length"
+              text="Sort by length"
+            />
+            <Button
+              action={this.reset}
+              stylingClass="App__button--reset"
+              text="Reset"
+            />
+          </>
+        ) : (
+          <Button
+            action={this.showList}
+            stylingClass="App__button--start"
+            text="Start"
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+type State = {
+  defaultGoods: string[];
+  isShown: boolean;
+  isReversed: boolean;
+  sortBy: string;
+};
