0736ebbf7a598762b9d6f32b66acde3e1eed66ec
Taras Holiuk <taras.holiuk@gmail.com"dsveagj 3rm>
2021-09-07 18:18:06 +0300

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-// import { GoodsList } from './components/GoodsList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -17,72 +18,24 @@ const goodsFromServer: string[] = [
 
 interface State {
   isStarted: boolean;
-  goodsList: string[];
-  isReversed: boolean;
-  sortBy: 'alphabet' | 'length',
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
     isStarted: false,
-    goodsList: goodsFromServer,
-    isReversed: false,
-    sortBy: 'alphabet',
   };
 
   start = () => {
     this.setState((currentState) => ({
-      isStarted: currentState.isStarted,
+      isStarted: !currentState.isStarted,
     }));
-  };
-
-  reverseGoods = () => {
-    this.setState((currentState) => ({
-      isReversed: !currentState.isReversed,
-    }));
-  };
-
-  alphabetSort = () => {
-    this.setState({
-      sortBy: 'alphabet',
-    });
-  };
-
-  lengthSort = () => {
-    this.setState({
-      sortBy: 'length',
-    });
   };
 
   render() {
-    const {
-      isStarted,
-      goodsList,
-      isReversed,
-      sortBy,
-    } = this.state;
-
-    const visibleGoods = [...goodsList];
-
-    visibleGoods.sort((goodA, goodB) => {
-      switch (sortBy) {
-        case 'alphabet':
-          return goodA.localeCompare(goodB);
-
-        case 'length':
-          return goodA.length - goodB.length;
-
-        default:
-          return 0;
-      }
-    });
-
-    if (isReversed) {
-      visibleGoods.reverse();
-    }
+    const { isStarted } = this.state;
 
     return (
-      <div className="App">
+      <div className="App d-flex flex-column align-items-center bg-light vh-100">
         <h1 className="display-1 text-center">
           Goods
         </h1>
@@ -90,49 +43,14 @@ class App extends React.Component<{}, State> {
         {!isStarted && (
           <button
             type="button"
-            className="btn btn-success"
+            className="btn btn-success w-50"
             onClick={this.start}
           >
             Start
           </button>
         )}
 
-        {isStarted && (
-          <div>
-            <ul>
-              {visibleGoods.map((good) => (
-                <li>
-                  {good}
-                </li>
-              ))}
-            </ul>
-
-            <button
-              type="button"
-              className="btn btn-warning"
-              onClick={this.reverseGoods}
-            >
-              Reverse
-            </button>
-
-            Sort by:
-            <button
-              type="button"
-              className="btn btn-warning"
-              onClick={this.alphabetSort}
-            >
-              Alphabet
-            </button>
-
-            <button
-              type="button"
-              className="btn btn-warning"
-              onClick={this.lengthSort}
-            >
-              length
-            </button>
-          </div>
-        )}
+        {isStarted && <GoodsList goods={goodsFromServer} />}
       </div>
     );
   }
---
+++ src/components/GoodsList/GoodsList.tsx
@@ -0,0 +1,131 @@
+import { PureComponent } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface State {
+  goodsList: string[];
+  isReversed: boolean;
+  sortBy: 'alphabet' | 'length' | null;
+  isDefault: boolean;
+
+}
+
+interface Props {
+  goods: string[];
+}
+
+export class GoodsList extends PureComponent<Props, State> {
+  state: State = {
+    goodsList: this.props.goods,
+    isReversed: false,
+    sortBy: null,
+    isDefault: true,
+  };
+
+  reverseGoods = () => {
+    this.setState((currentState) => ({
+      isReversed: !currentState.isReversed,
+      isDefault: false,
+    }));
+  };
+
+  alphabetSort = () => {
+    this.setState({
+      sortBy: 'alphabet',
+      isDefault: false,
+    });
+  };
+
+  lengthSort = () => {
+    this.setState({
+      sortBy: 'length',
+      isDefault: false,
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      isDefault: true,
+    });
+  };
+
+  render() {
+    const {
+      goodsList,
+      isReversed,
+      sortBy,
+      isDefault,
+    } = this.state;
+
+    let visibleGoods = [...goodsList];
+
+    visibleGoods.sort((goodA, goodB) => {
+      switch (sortBy) {
+        case 'alphabet':
+          return goodA.localeCompare(goodB);
+
+        case 'length':
+          return goodA.length - goodB.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      visibleGoods.reverse();
+    }
+
+    if (isDefault) {
+      visibleGoods = [...goodsList];
+    }
+
+    return (
+      <div className="d-flex w-50 justify-content-center">
+        <ul className="p-4">
+          {visibleGoods.map((good) => (
+            <li key={good}>
+              {good}
+            </li>
+          ))}
+        </ul>
+
+        <div className="d-flex flex-column p-4">
+          <div className="d-flex flex-column border border-success rounded p-2 mb-4">
+            Sort by:
+            <button
+              type="button"
+              className="btn btn-warning my-2"
+              onClick={this.alphabetSort}
+            >
+              Alphabet
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-warning"
+              onClick={this.lengthSort}
+            >
+              length
+            </button>
+          </div>
+
+          <button
+            type="button"
+            className="btn btn-warning mb-2"
+            onClick={this.reverseGoods}
+          >
+            Reverse
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={this.reset}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
