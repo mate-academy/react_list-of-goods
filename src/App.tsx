0b7d3f9d22be18356d3a2da9c560709427c7301f
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-09-08 17:12:53 +0300

    fixes was added

--- src/App.tsx
+++ src/App.tsx
@@ -17,103 +17,43 @@ const goodsFromServer: string[] = [
 ];
 
 interface State {
-  isVisible: boolean;
-  sortCheck: boolean;
+  isVisibleStart: boolean;
+  isVisibleList: boolean,
   goodsArray: string[];
 }
 
 export class App extends React.Component<{}, State> {
   state = {
-    isVisible: true,
-    sortCheck: true,
+    isVisibleList: false,
+    isVisibleStart: true,
     goodsArray: [...goodsFromServer],
   };
 
-  getShow = () => {
+  getStart = () => {
     this.setState((currentState) => ({
-      isVisible: !currentState.isVisible,
-    }));
-  };
-
-  getRevers = () => {
-    this.setState((currentState) => ({
-      goodsArray: [...currentState.goodsArray].reverse(),
-    }));
-  };
-
-  getAlphabetSort = () => {
-    this.setState((currentState) => ({
-      sortCheck: !currentState.sortCheck,
-      goodsArray: [...currentState.goodsArray]
-        .sort((a, b) => (
-          currentState.sortCheck
-            ? a.localeCompare(b)
-            : b.localeCompare(a)
-        )),
-    }));
-  };
-
-  getReset = () => {
-    this.setState({
-      goodsArray: [...goodsFromServer],
-    });
-  };
-
-  getLengthSort = () => {
-    this.setState((currentState) => ({
-      sortCheck: !currentState.sortCheck,
-      goodsArray: [...currentState.goodsArray]
-        .sort((a, b) => (
-          currentState.sortCheck
-            ? a.length - b.length
-            : b.length - a.length
-        )),
+      isVisibleStart: !currentState.isVisibleStart,
+      isVisibleList: !currentState.isVisibleList,
     }));
   };
 
   render() {
-    const { goodsArray } = this.state;
+    const { goodsArray, isVisibleStart } = this.state;
 
     return (
       <div className="App">
-        <div className="App__buttonsList">
-          <button
-            className="App__button"
-            type="button"
-            onClick={this.getShow}
-          >
-            Start
-          </button>
-          <button
-            className="App__button"
-            type="button"
-            onClick={this.getRevers}
-          >
-            Revers
-          </button>
-          <button
-            className="App__button"
-            type="button"
-            onClick={this.getAlphabetSort}
-          >
-            Sort alphabetically
-          </button>
-          <button
-            className="App__button"
-            type="button"
-            onClick={this.getReset}
-          >
-            Reset
-          </button>
-          <button
-            className="App__button"
-            type="button"
-            onClick={this.getLengthSort}
-          >
-            Sort by length
-          </button>
-        </div>
-        {this.state.isVisible && <GoodsList goodsList={goodsArray} />}
+        {isVisibleStart && (
+          <div className="start">
+            <h1>List of goods</h1>
+            <button
+              className="App__button"
+              type="button"
+              onClick={this.getStart}
+            >
+              Start
+            </button>
+          </div>
+        )}
+        {this.state.isVisibleList && <GoodsList goods={goodsArray} />}
       </div>
     );
   }
--- src/Components/GoodsList/GoodsList.tsx
+++ src/Components/GoodsList/GoodsList.tsx
@@ -3,22 +3,101 @@ import React from 'react';
 import './GoodsList.scss';
 
 type Props = {
+  goods: string[],
+};
+
+type State = {
+  sortCheck: boolean;
   goodsList: string[],
 };
 
-export class GoodsList extends React.PureComponent<Props> {
+export class GoodsList extends React.PureComponent<Props, State> {
+  state = {
+    sortCheck: true,
+    goodsList: [...this.props.goods],
+  };
+
+  getRevers = () => {
+    this.setState((currentState) => ({
+      goodsList: [...currentState.goodsList].reverse(),
+    }));
+  };
+
+  getAlphabetSort = () => {
+    this.setState((currentState) => ({
+      sortCheck: !currentState.sortCheck,
+      goodsList: [...currentState.goodsList]
+        .sort((a, b) => (
+          currentState.sortCheck
+            ? a.localeCompare(b)
+            : b.localeCompare(a)
+        )),
+    }));
+  };
+
+  getReset = () => {
+    this.setState({
+      goodsList: [...this.props.goods],
+    });
+  };
+
+  getLengthSort = () => {
+    this.setState((currentState) => ({
+      sortCheck: !currentState.sortCheck,
+      goodsList: [...currentState.goodsList]
+        .sort((a, b) => (
+          currentState.sortCheck
+            ? a.length - b.length
+            : b.length - a.length
+        )),
+    }));
+  };
+
   render() {
     return (
-      <ul className="goodsList">
-        {this.props.goodsList.map(goods => (
-          <li
-            className="goodsList__item"
-            key={goods}
+      <>
+        <div className="App__buttonsList">
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.getRevers}
           >
-            {goods}
-          </li>
-        ))}
-      </ul>
+            Revers
+          </button>
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.getAlphabetSort}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.getReset}
+          >
+            Reset
+          </button>
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.getLengthSort}
+          >
+            Sort by length
+          </button>
+        </div>
+
+        <ul className="goodsList">
+          {this.state.goodsList.map(goods => (
+            <li
+              className="goodsList__item"
+              key={goods}
+            >
+              {goods}
+            </li>
+          ))}
+        </ul>
+      </>
     );
   }
 }
