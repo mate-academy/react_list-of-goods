10dfef523fe291e868600cfd0ff3f6bfdd320cfc
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-09-08 18:08:31 +0300

    initial solution for peer-to-peer review

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { GoodsList } from './GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +17,141 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  goods: string[];
+  isListVisible: boolean;
+  isReversed: boolean;
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [...goodsFromServer],
+    isListVisible: false,
+    isReversed: false,
+  };
+
+  showList = () => {
+    this.setState({ isListVisible: true });
+  };
+
+  reverseList = () => {
+    const { isListVisible } = this.state;
+
+    if (isListVisible) {
+      this.setState(state => ({
+        isReversed: !state.isReversed,
+      }));
+    }
+  };
+
+  reset = () => {
+    this.setState(state => ({
+      goods: [...goodsFromServer],
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortByAlphabet = () => {
+    this.setState(state => ({
+      goods: state.goods.sort((a, b) => b.localeCompare(a)),
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: state.goods.sort((a, b) => b.length - a.length),
+    }));
+  };
+
+  render() {
+    const {
+      goods,
+      isListVisible,
+      isReversed,
+    } = this.state;
+
+    let modifiedGoods = goods;
+
+    if (isReversed) {
+      modifiedGoods = [...modifiedGoods].reverse();
+    }
+
+    const buttonClass = 'btn-box bg-primary border-0 rounded text-light w-25 py-2 m-1';
+    const flexLayout = 'd-flex justify-content-center align-items-center';
+
+    return (
+      <div className={`h-100 w-100 ${flexLayout}`}>
+        {isListVisible
+          ? (
+            <div className="w-100">
+
+              <div className={`${flexLayout} flex-column`}>
+                <h1>Goods</h1>
+                <GoodsList goods={modifiedGoods} />
+              </div>
+
+              <div className={`${flexLayout}`}>
+                <button
+                  type="button"
+                  onClick={() => this.reverseList()}
+                  className={`${buttonClass}`}
+                >
+                  Reverse List
+                </button>
+
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (!isListVisible) {
+                      return;
+                    }
+
+                    this.sortByAlphabet();
+                  }}
+                  className={`${buttonClass}`}
+                >
+                  Sort by Alphabet
+                </button>
+
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (!isListVisible) {
+                      return;
+                    }
+
+                    this.sortByLength();
+                  }}
+                  className={`${buttonClass}`}
+                >
+                  Sort by Length
+                </button>
+
+                <button
+                  type="button"
+                  onClick={() => this.reset()}
+                  className={`${buttonClass}`}
+                >
+                  Reset
+                </button>
+              </div>
+
+            </div>
+
+          )
+
+          : (
+            <button
+              type="button"
+              onClick={() => this.showList()}
+              className={`${buttonClass}`}
+            >
+              Start
+            </button>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/GoodsList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface Props {
+  goods: string[]
+}
+
+export const GoodsList: React.FC<Props> = (props) => {
+  const { goods } = props;
+
+  return (
+    <ul className="w-25">
+      {goods.map(good => (
+        <li
+          key={good}
+          className="border border-success rounded p-1 m-1"
+        >
+          {good}
+        </li>
+      ))}
+    </ul>
+  );
+};
