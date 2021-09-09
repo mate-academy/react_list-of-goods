87f402ea6daf0944be0aef7517fd5816000b172e
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-09-09 12:53:35 +0300

    mentor and student suggestions have been implemented

--- src/App.tsx
+++ src/App.tsx
@@ -18,63 +18,22 @@ const goodsFromServer: string[] = [
 ];
 
 interface State {
-  goods: string[];
   isListVisible: boolean;
-  isReversed: boolean;
 }
 
 class App extends React.Component<{}, State> {
   state = {
-    goods: [...goodsFromServer],
     isListVisible: false,
-    isReversed: false,
   };
 
   showList = () => {
     this.setState({ isListVisible: true });
   };
 
-  reverseList = () => {
-    const { isListVisible } = this.state;
-
-    if (isListVisible) {
-      this.setState(state => ({
-        isReversed: !state.isReversed,
-      }));
-    }
-  };
-
-  reset = () => {
-    this.setState(state => ({
-      goods: [...goodsFromServer],
-      isReversed: !state.isReversed,
-    }));
-  };
-
-  sortByAlphabet = () => {
-    this.setState(state => ({
-      goods: [...state.goods.sort((a, b) => b.localeCompare(a))],
-    }));
-  };
-
-  sortByLength = () => {
-    this.setState(state => ({
-      goods: [...state.goods.sort((a, b) => b.length - a.length)],
-    }));
-  };
-
   render() {
     const {
-      goods,
       isListVisible,
-      isReversed,
     } = this.state;
-
-    let modifiedGoods = goods;
-
-    if (isReversed) {
-      modifiedGoods = [...modifiedGoods].reverse();
-    }
 
     const buttonClass = 'btn-box bg-primary border-0 rounded text-light w-25 py-2 m-1';
     const flexLayout = 'd-flex justify-content-center align-items-center';
@@ -83,63 +42,8 @@ class App extends React.Component<{}, State> {
       <div className={`h-100 w-100 ${flexLayout}`}>
         {isListVisible
           ? (
-            <div className="w-100">
-
-              <div className={`${flexLayout} flex-column`}>
-                <h1>Goods</h1>
-                <GoodsList goods={modifiedGoods} />
-              </div>
-
-              <div className={`${flexLayout}`}>
-                <button
-                  type="button"
-                  onClick={() => this.reverseList()}
-                  className={`${buttonClass}`}
-                >
-                  Reverse List
-                </button>
-
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (!isListVisible) {
-                      return;
-                    }
-
-                    this.sortByAlphabet();
-                  }}
-                  className={`${buttonClass}`}
-                >
-                  Sort by Alphabet
-                </button>
-
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (!isListVisible) {
-                      return;
-                    }
-
-                    this.sortByLength();
-                  }}
-                  className={`${buttonClass}`}
-                >
-                  Sort by Length
-                </button>
-
-                <button
-                  type="button"
-                  onClick={() => this.reset()}
-                  className={`${buttonClass}`}
-                >
-                  Reset
-                </button>
-              </div>
-
-            </div>
-
+            <GoodsList goodsFromServer={goodsFromServer} />
           )
-
           : (
             <button
               type="button"
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -1,23 +1,126 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-interface Props {
-  goods: string[]
+enum SortType {
+  length = 'length',
+  alphabet = 'alphabet',
 }
 
-export const GoodsList: React.FC<Props> = (props) => {
-  const { goods } = props;
+interface Props {
+  goodsFromServer: string[]
+}
 
-  return (
-    <ul className="w-25">
-      {goods.map(good => (
-        <li
-          key={good}
-          className="border border-success rounded p-1 m-1 text-center"
-        >
-          {good}
-        </li>
-      ))}
-    </ul>
-  );
-};
+interface State {
+  goods: string[];
+  isReversed: boolean;
+}
+
+export class GoodsList extends React.Component<Props, State> {
+  state = {
+    goods: [...this.props.goodsFromServer],
+    isReversed: false,
+  };
+
+  reverseList = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      goods: [...this.props.goodsFromServer],
+      isReversed: false,
+    });
+  };
+
+  sortByParam = (sortBy: SortType) => {
+    switch (sortBy) {
+      case SortType.alphabet:
+        return this.setState(state => ({
+          goods: [...state.goods.sort((a, b) => a.localeCompare(b))],
+        }));
+
+      case SortType.length:
+        return this.setState(state => ({
+          goods: [...state.goods.sort((a, b) => b.length - a.length)],
+        }));
+
+      default:
+        return 0;
+    }
+  };
+
+  getVisible = () => {
+    const { isReversed, goods } = this.state;
+    let modifiedGoods = goods;
+
+    if (isReversed) {
+      modifiedGoods = [...modifiedGoods].reverse();
+    }
+
+    return modifiedGoods;
+  };
+
+  render() {
+    const buttonClass = 'btn-box bg-primary border-0 rounded text-light w-25 py-2 m-1';
+    const flexLayout = 'd-flex justify-content-center align-items-center';
+
+    const modifiedGoods = this.getVisible();
+
+    return (
+      <div className="w-100">
+
+        <div className={`${flexLayout} flex-column`}>
+          <h1>Goods</h1>
+          <ul className="w-25">
+            {modifiedGoods.map(good => (
+              <li
+                key={good}
+                className="border border-success rounded p-1 m-1 text-center"
+              >
+                {good}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={`${flexLayout}`}>
+          <button
+            type="button"
+            onClick={() => this.reverseList()}
+            className={`${buttonClass}`}
+          >
+            Reverse List
+          </button>
+
+          <button
+            type="button"
+            onClick={() => this.sortByParam(SortType.alphabet)}
+            className={`${buttonClass}`}
+          >
+            Sort by Alphabet
+          </button>
+
+          <button
+            type="button"
+            onClick={() => this.sortByParam(SortType.length)}
+            className={`${buttonClass}`}
+          >
+            Sort by Length
+          </button>
+
+          <button
+            type="button"
+            onClick={() => this.reset()}
+            className={`${buttonClass}`}
+          >
+            Reset
+          </button>
+        </div>
+
+      </div>
+
+    );
+  }
+}
