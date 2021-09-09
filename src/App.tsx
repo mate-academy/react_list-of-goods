eb05efd547cd6571b28c14995f8c88676f1576e5
kristof22122 <kristf44@gmail.com>
2021-09-09 15:20:27 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './Components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,140 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isVisible: boolean,
+  isReversed: boolean,
+  sortBy: string,
+  goodsList: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    isVisible: true,
+    isReversed: false,
+    sortBy: '',
+    goodsList: [...goodsFromServer],
+  };
+
+  showGoods = () => {
+    this.setState({ isVisible: false });
+  };
+
+  reverse = () => {
+    this.setState((currentState) => ({
+      isReversed: !currentState.isReversed,
+    }));
+  };
+
+  getVisibleGoods = () => {
+    const {
+      isReversed,
+      goodsList,
+      sortBy,
+    } = this.state;
+
+    let visibleGoodsList = goodsList;
+
+    if (sortBy) {
+      visibleGoodsList = [...visibleGoodsList]
+        .sort((goodFirst, goodSecond) => {
+          switch (sortBy) {
+            case 'alphabet':
+              return goodFirst.localeCompare(goodSecond);
+            case 'length':
+              return goodFirst.length - goodSecond.length;
+
+            default:
+              return 0;
+          }
+        });
+    }
+
+    if (isReversed) {
+      visibleGoodsList = [...visibleGoodsList].reverse();
+    }
+
+    return visibleGoodsList;
+  };
+
+  sortByAlphabetically = () => {
+    this.setState({
+      sortBy: 'alphabet',
+    });
+  };
+
+  sortByLength = () => {
+    this.setState({
+      sortBy: 'length',
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      isReversed: false,
+      sortBy: '',
+      goodsList: [...goodsFromServer],
+    });
+  };
+
+  render() {
+    const { isVisible } = this.state;
+
+    const visibleGoods = this.getVisibleGoods();
+
+    return (
+      <div className="wrapper">
+        <div className="App">
+          {(isVisible) && (
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.showGoods}
+            >
+              Start
+            </button>
+          )}
+          {(!isVisible) && (
+            <div>
+              <div className="d-grid gap-2 col-6 mx-auto">
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  onClick={this.reverse}
+                >
+                  Reverse
+                </button>
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  onClick={this.sortByAlphabetically}
+                >
+                  Sort alphabetically
+                </button>
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  onClick={this.reset}
+                >
+                  Reset
+                </button>
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  onClick={this.sortByLength}
+                >
+                  Sort by length
+                </button>
+              </div>
+              {visibleGoods.length !== 0 && (
+                <GoodsList goods={visibleGoods} />
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
