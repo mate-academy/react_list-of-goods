d85b0a591559082d987ec98c438601b11bccb498
Kazkoviy <illyaboiko1807@gmail.com>
2021-09-13 23:09:21 +0300

    all was fixed

--- src/App.tsx
+++ src/App.tsx
@@ -17,73 +17,16 @@ const goodsFromServer: string[] = [
 ];
 
 type State = {
-  goods: string[];
   start: boolean;
-  reversed: boolean;
-  sortBy: string;
 };
-
-enum SortBy {
-  Length = 'length',
-  Name = 'name',
-}
 
 export class App extends React.Component<{}, State> {
   state = {
-    goods: goodsFromServer,
     start: false,
-    reversed: false,
-    sortBy: '',
   };
 
   start = () => {
     this.setState({ start: true });
-  };
-
-  reverse = () => {
-    this.setState(state => ({ reversed: !state.reversed }));
-  };
-
-  reset = () => {
-    this.setState({
-      reversed: false,
-      sortBy: '',
-    });
-  };
-
-  sortByMethod = (sortArgument: SortBy): void => {
-    if (sortArgument === SortBy.Length) {
-      this.setState({ sortBy: SortBy.Length });
-    } else {
-      this.setState({ sortBy: SortBy.Name });
-    }
-  };
-
-  visibleGoods = () => {
-    const {
-      goods,
-      reversed,
-      sortBy,
-    } = this.state;
-
-    const copyOfGoods = [...goods];
-
-    copyOfGoods.sort((a, b) => {
-      switch (sortBy) {
-        case 'name':
-          return a.localeCompare(b);
-        case 'length':
-          return a.length - b.length;
-        default:
-          return 0;
-      }
-    });
-
-    if (reversed) {
-      copyOfGoods.reverse();
-    }
-
-    return copyOfGoods;
   };
 
   render() {
@@ -96,14 +39,7 @@ export class App extends React.Component<{}, State> {
         </h1>
 
         {start ? (
-          <>
-            <List goods={this.visibleGoods()} />
-
-            <Button callback={this.reverse} name="Reverse" />
-            <Button callback={this.reset} name="Reset" />
-            <Button callback={() => this.sortByMethod(SortBy.Name)} name="Sort by name" />
-            <Button callback={() => this.sortByMethod(SortBy.Length)} name="Sort by length" />
-          </>
+          <List goods={goodsFromServer} />
         ) : (
           <Button callback={this.start} name="Start" />
         )}
