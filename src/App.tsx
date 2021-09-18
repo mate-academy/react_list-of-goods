46dfb98787c6fab173d481764b260947451fe9ef
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-09-18 22:28:34 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,125 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goodsArr: string[],
+  showList: boolean,
+  isReversed: boolean,
+  sortBy: string,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goodsArr: goodsFromServer,
+    showList: false,
+    isReversed: false,
+    sortBy: '',
+  };
+
+  showListed = () => {
+    this.setState((prevState) => (
+      { showList: !prevState.showList }
+    ));
+  };
+
+  sortAbc = () => {
+    this.setState({ sortBy: 'abc' });
+  };
+
+  sortLength = () => {
+    this.setState({ sortBy: 'length' });
+  };
+
+  revers = () => {
+    this.setState(prevState => ({
+      isReversed: !prevState.isReversed,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      isReversed: false,
+      sortBy: '',
+    });
+  };
+
+  render() {
+    const {
+      goodsArr,
+      showList,
+      isReversed,
+      sortBy,
+    } = this.state;
+    const visibleGoods = [...goodsArr];
+
+    visibleGoods.sort((a, b) => {
+      switch (sortBy) {
+        case 'abc':
+          return a.localeCompare(b);
+        case 'length':
+          return a.length - b.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      visibleGoods.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {!showList && (
+          <button
+            type="button"
+            className="button"
+            onClick={this.showListed}
+          >
+            Start
+          </button>
+        )}
+        {showList && (
+          <>
+            <ul className="list">
+              {visibleGoods.map((item) => (
+                <li key={item} className="list-item">{item}</li>
+              ))}
+            </ul>
+
+            <button
+              type="button"
+              className="button"
+              onClick={this.revers}
+            >
+              Reverse
+            </button>
+            <button
+              type="button"
+              className="button"
+              onClick={this.sortAbc}
+            >
+              Sort alphabetically
+            </button>
+            <button
+              type="button"
+              className="button"
+              onClick={this.sortLength}
+            >
+              Sort by length
+            </button>
+            <button
+              type="button"
+              className="button"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
