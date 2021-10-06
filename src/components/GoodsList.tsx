536b5b03a681aa9706a545529c6ab071ab30380e
Bohdan Hnatuk <hnatuk996@gmail.com>
2021-10-06 21:45:07 +0300

    solution

---
+++ src/components/GoodsList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type Props = {
+  goods: string[],
+};
+
+const GoodsList: React.FC<Props> = ({ goods }) => {
+  return (
+    <ul>
+      {goods.map(good => (
+        <li
+          key={good}
+        >
+          {good}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default GoodsList;
