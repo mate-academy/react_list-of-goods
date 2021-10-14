b7c6f60891584cf6c29fe3f8a128b40a92e604c6
magithub@yandex.ru <magithub@yandex.ru>
2021-10-14 22:52:39 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,110 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  originGoods: string[],
+  goods: string[],
+  isVisible: boolean,
+  isReverse: boolean,
+  isSortByAlphabet: boolean,
+  isSortByLength: boolean,
+}
 
-export default App;
+export default class App extends React.Component<{}, State> {
+  state:State = {
+    originGoods: [...goodsFromServer],
+    goods: goodsFromServer,
+    isVisible: false,
+    isReverse: false,
+    isSortByAlphabet: false,
+    isSortByLength: false,
+  };
+
+  toggleVisible = () => this.setState(({ isVisible }) => ({ isVisible: !isVisible }));
+
+  toggleReverse = () => this.setState(({ isReverse }) => ({
+    isReverse: !isReverse,
+    goods: goodsFromServer.reverse(),
+  }));
+
+  sortByAlphabet = () => {
+    this.setState(({ isSortByAlphabet }) => ({
+      isSortByAlphabet: !isSortByAlphabet,
+      goods: goodsFromServer.sort((f:string, s:string) => f.localeCompare(s)),
+    }));
+  };
+
+  sortByLength = () => this.setState(({ isSortByLength }) => ({
+    isSortByLength: !isSortByLength,
+    goods: goodsFromServer.sort((a:string, b:string) => a.length - b.length),
+  }));
+
+  reset = () => this.setState(({ originGoods }) => ({
+    goods: originGoods,
+    isVisible: true,
+    isReverse: false,
+    isSortByAlphabet: false,
+    isSortByLength: false,
+  }));
+
+  filter = (event:any) => {
+    this.setState({
+      goods: goodsFromServer.filter(good => good.length >= event.target.value),
+    });
+  };
+
+  render() {
+    const { goods, isVisible } = this.state;
+
+    return (
+      <>
+        {isVisible ? (
+          <>
+            <button type="button" className="button button__reverse" onClick={this.toggleReverse}>
+              toggleReverse
+            </button>
+
+            <button type="button" className="button button__sort-by-alphabet" onClick={this.sortByAlphabet}>
+              sortByAlphabet
+            </button>
+
+            <button type="button" className="button button__sort-by-length" onClick={this.sortByLength}>
+              sortByLength
+            </button>
+
+            <button type="button" className="button button__reset" onClick={this.reset}>
+              reset
+            </button>
+
+            <select name="select-values" id="" onChange={this.filter}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+
+            <ul>
+              {goods.map(good => (
+                <li key={good}>
+                  {good}
+                </li>
+              ))}
+            </ul>
+          </>
+        )
+          : (
+            <button type="button" className="button button__reverse" onClick={this.toggleVisible}>
+              toggleVisible
+            </button>
+          )}
+      </>
+
+    );
+  }
+}
