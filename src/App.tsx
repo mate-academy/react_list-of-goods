51ec26ee59dd1840f61663bcb8a3c203c352b334
Igor Igor <you@example.com>
2021-10-18 20:00:23 +0300

    first part solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,123 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[],
+  isReverse: boolean,
+  sortBy: string,
+  isStart: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: goodsFromServer,
+    isReverse: false,
+    sortBy: '',
+    isStart: false,
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReverse: !state.isReverse,
+    }));
+  };
+
+  sortByAlphabetically = () => {
+    this.setState(({ sortBy: 'alphabetically' }));
+  };
+
+  sortByLength = () => {
+    this.setState(({ sortBy: 'length' }));
+  };
+
+  toStart = () => {
+    this.setState(({ isStart: true }));
+  };
+
+  reset = () => {
+    this.setState(({ sortBy: '', isReverse: false }));
+  };
+
+  render() {
+    const {
+      goods,
+      isReverse,
+      sortBy,
+      isStart,
+    } = this.state;
+
+    const visibleFriends = [...goods];
+
+    visibleFriends.sort((good1: string, good2: string) => {
+      switch (sortBy) {
+        case 'alphabetically':
+          return good1.localeCompare(good2);
+          break;
+        case 'length':
+          return good1.length - good2.length;
+          break;
+        default:
+          return 0;
+          break;
+      }
+    });
+
+    if (isReverse) {
+      visibleFriends.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1>
+          Goods
+        </h1>
+        {!isStart && (
+          <button
+            type="button"
+            onClick={this.toStart}
+          >
+            Start
+          </button>
+        )}
+
+        {isStart && (
+          <div>
+            <button
+              type="button"
+              onClick={this.reverse}
+            >
+              Reverse
+            </button>
+            <button
+              type="button"
+              onClick={this.sortByAlphabetically}
+            >
+              Sort alphabetically
+            </button>
+            <button
+              type="button"
+              onClick={this.sortByLength}
+            >
+              Sort by length
+            </button>
+            <button
+              type="button"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+            <ul>
+              {visibleFriends.map(good => (
+                <li key={good}>
+                  {good}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
