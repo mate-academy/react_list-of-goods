9c3c68ea3d527e9aa2ce06738adcc75ca0746ff3
Vladyslav Banul <vladyslav.banul@gmail.com>
2021-10-18 19:45:46 +0300

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,67 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    renderStatus: false,
+    goodsToShow: goodsFromServer,
+    defaultOrder: goodsFromServer
+  }
+  SwitchStatus = () => {
+    this.setState({renderStatus: !this.state.renderStatus});
+  }
+  reverseOrder = () => {
+    this.setState({goodsToShow: [...this.state.goodsToShow].reverse()});
+  }
+  alphabeticalOrder = () => {
+    this.setState({goodsToShow: [...this.state.goodsToShow].sort((g1,g2) => g1.localeCompare(g2))});
+  }
+  resetOrder = () => {
+    this.setState({goodsToShow: [...this.state.defaultOrder]});
+  }
+  lengthOrder = () => {
+    this.setState({goodsToShow: [...this.state.goodsToShow].sort((g1,g2) => g1.length - g2.length)});
+  }
+
+  render() {
+    let {renderStatus, goodsToShow} = {...this.state}
+    return(
+      <div className="App">
+        <button
+          type="button"
+          className={`show--${!renderStatus}`}
+          onClick={this.SwitchStatus}>
+          Start
+        </button>
+        
+        <div className={`show--${renderStatus}`}>
+        <button
+          type="button"
+          onClick={this.reverseOrder}>
+          Reverse
+        </button>
+        <button
+          type="button"
+          onClick={this.alphabeticalOrder}>
+          Sort alphabetically
+        </button>
+        <button
+          type="button"
+          onClick={this.resetOrder}>
+          Reset
+        </button>
+        <button
+          type="button"
+          onClick={this.lengthOrder}>
+          Sort by length
+        </button>
+          <ul>
+            {goodsToShow.map((good) => <li>{good}</li>)}
+          </ul>
+        </div>
+        
+      </div>
+  );}
+}
 
 export default App;
