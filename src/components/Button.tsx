ae8c1b0ea4bd6649d3a552de44d640c649b85879
Maksym Vikolash <vikolashmaxim@gmail.com>
2021-10-18 21:04:14 +0300

    solution

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+
+type Props = {
+  name: string;
+  clickHandler(): void;
+};
+
+export const Button: FC<Props> = ({ name, clickHandler }) => (
+  <button
+    type="button"
+    onClick={() => {
+      clickHandler();
+    }}
+  >
+    {name}
+  </button>
+);
