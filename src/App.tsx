f6197b6ff2fff4029bf020534d549eaad170942b
Maksym Vikolash <vikolashmaxim@gmail.com>
2021-10-18 21:07:06 +0300

    state typization added

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ type State = {
 };
 
 class App extends Component<{}, State> {
-  state = {
+  state: State = {
     isStarted: false,
   };
 
--- src/components/GoodsList.tsx
+++ src/components/GoodsList.tsx
@@ -11,7 +11,7 @@ type State = {
 };
 
 export class GoodsList extends Component<Props, State> {
-  state = {
+  state: State = {
     isReversed: false,
     sortBy: '',
   };
