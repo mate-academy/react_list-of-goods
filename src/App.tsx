ed23eacc34634d3679ac2f84c68f301648603441
Dima Narozhnyi <dmitriy.narozhnyi@gmail.com>
2021-11-12 00:57:12 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,165 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[],
+  isShown: boolean,
+  isReversed: boolean,
+  sortBy: string,
+  length: number,
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    goods: goodsFromServer,
+    isShown: false,
+    isReversed: false,
+    sortBy: '',
+    length: 1,
+  };
+
+  showGoods = () => {
+    this.setState((state) => ({
+      isShown: !state.isShown,
+    }));
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortAlphabetically = () => {
+    this.setState({ sortBy: 'alphabet' });
+  };
+
+  sortByLength = () => {
+    this.setState({ sortBy: 'length' });
+  };
+
+  reset = () => {
+    this.setState({
+      goods: [...goodsFromServer],
+      isReversed: false,
+      sortBy: '',
+      length: 1,
+    });
+  };
+
+  filterByLength = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+
+    this.setState({
+      length: +value,
+    });
+  };
+
+  render() {
+    const {
+      goods,
+      isShown,
+      isReversed,
+      sortBy,
+      length,
+    } = this.state;
+
+    const products = [...goods].filter(good => good.length >= length);
+
+    if (sortBy) {
+      products.sort((product1, product2) => {
+        switch (sortBy) {
+          case 'alphabet':
+            return product1.localeCompare(product2);
+          case 'length':
+            return product1.length - product2.length;
+          default:
+            return 0;
+        }
+      });
+    }
+
+    if (isReversed) {
+      products.reverse();
+    }
+
+    return (
+      <div className="App">
+        {!isShown && (
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.showGoods}
+          >
+            Start
+          </button>
+        )}
+
+        {isShown && (
+          <div className="App__container">
+            <ul className="goods-list">
+              {products.map(item => (
+                <li
+                  className="goods-list__item"
+                  key={item}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+            <div className="goods-list__button-wrapper">
+              <button
+                className="goods-list__button"
+                type="button"
+                onClick={this.reverse}
+              >
+                Reverse
+              </button>
+
+              <button
+                className="goods-list__button"
+                type="button"
+                onClick={this.sortAlphabetically}
+              >
+                Sort alphabetically
+              </button>
+
+              <button
+                className="goods-list__button"
+                type="button"
+                onClick={this.sortByLength}
+              >
+                Sort by length
+              </button>
+
+              <button
+                className="goods-list__button"
+                type="button"
+                onClick={this.reset}
+              >
+                Reset
+              </button>
+              <select
+                className="goods-list__select"
+                name="length"
+                value={this.state.length}
+                onChange={this.filterByLength}
+              >
+                <option className="goods-list__option" value="1" selected>1</option>
+                <option className="goods-list__option" value="2">2</option>
+                <option className="goods-list__option" value="3">3</option>
+                <option className="goods-list__option" value="4">4</option>
+                <option className="goods-list__option" value="5">5</option>
+                <option className="goods-list__option" value="6">6</option>
+                <option className="goods-list__option" value="7">7</option>
+                <option className="goods-list__option" value="8">8</option>
+                <option className="goods-list__option" value="9">9</option>
+                <option className="goods-list__option" value="10">10</option>
+              </select>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
