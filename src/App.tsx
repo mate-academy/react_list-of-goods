95b8f4985f51275f1e385248a72339878b0867e0
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-11-15 22:29:04 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -27,58 +28,58 @@ export class App extends React.Component<{}, State> {
     isShown: false,
     isReversed: false,
     sortBy: '',
-  }
+  };
 
   showItems = () => {
     this.setState(state => ({
       isShown: !state.isShown,
-    }))
-  }
+    }));
+  };
 
   sortByAlphabet = () => {
-    this.setState({ 
+    this.setState({
       sortBy: 'alphabet',
       isReversed: false,
-    })
-  }
+    });
+  };
 
   sortByLength = () => {
-    this.setState({ 
+    this.setState({
       sortBy: 'length',
       isReversed: false,
-    })
-  }
+    });
+  };
 
   reverse = () => {
     this.setState(state => ({
       isReversed: !state.isReversed,
-    }))
-  }
+    }));
+  };
 
   reset = () => {
     this.setState({
       isReversed: false,
       sortBy: '',
-    })
-  }
+    });
+  };
 
   listToRender = () => {
     const { goods, isReversed, sortBy } = this.state;
 
-    if(isReversed) {
+    if (isReversed) {
       return [...goods].reverse();
     }
 
-    if(sortBy === 'alphabet') {
+    if (sortBy === 'alphabet') {
       return [...goods].sort();
     }
 
-    if(sortBy === 'length') {
-      return [...goods].sort((a, b) => a.length - b.length)
+    if (sortBy === 'length') {
+      return [...goods].sort((a, b) => a.length - b.length);
     }
 
     return goods;
-  }
+  };
 
   render() {
     const { isShown } = this.state;
@@ -86,65 +87,60 @@ export class App extends React.Component<{}, State> {
 
     return (
       <div className="App">
-        <ul>
-          {!isShown &&
-          <button
-            onClick={this.showItems}
-            className="button"
-            type="button"
-          >
-            start
-          </button>}
+        <div>
+          {!isShown
+          && (
+            <button
+              onClick={this.showItems}
+              className="button"
+              type="button"
+            >
+              start
+            </button>
+          )}
           {isShown && (
             <>
-            <div className="buttons">
-              <button
-                onClick={this.reverse}
-                className="button"
-                type="button"
-              >
-                Reverse
-              </button>
+              <div className="buttons">
+                <button
+                  onClick={this.reverse}
+                  className="button"
+                  type="button"
+                >
+                  Reverse
+                </button>
 
-              <button
-                onClick={this.sortByAlphabet}
-                className="button"
-                type="button"
-              >
-                Sort alphabetically
-              </button>
+                <button
+                  onClick={this.sortByAlphabet}
+                  className="button"
+                  type="button"
+                >
+                  Sort alphabetically
+                </button>
 
-              <button
-                onClick={this.reset}
-                className="button"
-                type="button"
-              >
-                Reset
-              </button>
+                <button
+                  onClick={this.reset}
+                  className="button"
+                  type="button"
+                >
+                  Reset
+                </button>
 
-              <button
-                onClick={this.sortByLength}
-                className="button"
-                type="button"
-              >
-                Sort by length
-              </button>
-            </div>
+                <button
+                  onClick={this.sortByLength}
+                  className="button"
+                  type="button"
+                >
+                  Sort by length
+                </button>
+              </div>
 
-            {items.map(item => (
-            <li
-              key={item}
-              className="item"
-            >
-              {item}
-            </li>
-            ))}
+              <GoodsList items={items}/>
             </>
           )}
-        </ul>
+        </div>
       </div>
     );
   }
-} 
+}
 
 export default App;
