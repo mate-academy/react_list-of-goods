ea8ef91a69044c18ac29f1bfa2f9b37b23462539
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-16 11:29:46 +0200

    Moved buttons in parent component

--- src/App.tsx
+++ src/App.tsx
@@ -19,14 +19,24 @@ type Props = {};
 
 interface State {
   goods: string[];
+  isReversed: boolean,
+  isSortAlphabetically: boolean,
+  isSortByLength: boolean,
+  isReset: boolean,
   isVisible: boolean;
 }
 
 class App extends React.PureComponent<Props, State> {
   state = {
     goods: goodsFromServer,
+    isReversed: false,
+    isSortAlphabetically: false,
+    isSortByLength: false,
+    isReset: false,
     isVisible: false,
   };
+
+  initialState = { ...this.state };
 
   activeList = (): void => {
     this.setState(prevState => ({
@@ -34,18 +44,99 @@ class App extends React.PureComponent<Props, State> {
     }));
   };
 
+  sortAlphabetically = () => {
+    this.setState(prevState => ({
+      ...this.initialState,
+      isSortAlphabetically: !prevState.isSortAlphabetically,
+      isVisible: prevState.isVisible,
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState(prevState => ({
+      ...this.initialState,
+      isSortByLength: !prevState.isSortByLength,
+      isVisible: prevState.isVisible,
+    }));
+  };
+
+  reverse = () => {
+    this.setState(prevState => ({
+      ...this.initialState,
+      isReversed: !prevState.isReversed,
+      isVisible: prevState.isVisible,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      ...this.initialState,
+    });
+  };
+
   render() {
+    let preparedGoods = this.state.goods;
+
+    if (this.state.isReset) {
+      this.state = this.initialState;
+    }
+
+    if (this.state.isSortAlphabetically) {
+      preparedGoods = [...preparedGoods].sort((g1, g2) => g1.localeCompare(g2));
+    }
+
+    if (this.state.isSortByLength) {
+      preparedGoods = [...preparedGoods].sort((g1, g2) => g1.length - g2.length);
+    }
+
+    if (this.state.isReversed) {
+      preparedGoods = [...preparedGoods].reverse();
+    }
+
     return (
       <div className="App">
-        {this.state.isVisible && <GoodsList goods={[...this.state.goods]} />}
+        {this.state.isVisible && <GoodsList goods={[...preparedGoods]} />}
 
-        <button
-          className="App__button"
-          type="button"
-          onClick={this.activeList}
-        >
-          Start
-        </button>
+        {this.state.isVisible ? (
+          <div className="goods__config">
+            <button
+              className="goods__button"
+              type="button"
+              onClick={this.reverse}
+            >
+              Reverse
+            </button>
+            <button
+              className="goods__button"
+              type="button"
+              onClick={this.sortAlphabetically}
+            >
+              Sort alphabetically
+            </button>
+            <button
+              className="goods__button"
+              type="button"
+              onClick={this.sortByLength}
+            >
+              Sort by length
+            </button>
+            <button
+              className="goods__button"
+              type="button"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+          </div>
+        ) : (
+          <button
+            className="App__button"
+            type="button"
+            onClick={this.activeList}
+          >
+            Start
+          </button>
+        )}
       </div>
     );
   }
--- src/components/GoodsList.tsx
+++ src/components/GoodsList.tsx
@@ -6,109 +6,14 @@ type Props = {
   goods: string[];
 };
 
-interface State {
-  isReversed: boolean,
-  isSortAlphabetically: boolean,
-  isSortByLength: boolean,
-}
-
-export class GoodsList extends React.Component<Props, State> {
-  state = {
-    isReversed: false,
-    isSortAlphabetically: false,
-    isSortByLength: false,
-    isReset: false,
-  };
-
-  initialState = { ...this.state };
-
-  sortAlphabetically = () => {
-    this.setState(prevState => ({
-      ...this.initialState,
-      isSortAlphabetically: !prevState.isSortAlphabetically,
-    }));
-  };
-
-  sortByLength = () => {
-    this.setState(prevState => ({
-      ...this.initialState,
-      isSortByLength: !prevState.isSortByLength,
-    }));
-  };
-
-  reverse = () => {
-    this.setState(prevState => ({
-      ...this.initialState,
-      isReversed: !prevState.isReversed,
-    }));
-  };
-
-  reset = () => {
-    this.setState({
-      ...this.initialState,
-    });
-  };
-
-  render() {
-    let { goods } = this.props;
-
-    if (this.state.isReset) {
-      goods = this.props.goods;
-    }
-
-    if (this.state.isSortAlphabetically) {
-      goods = [...goods].sort((g1, g2) => g1.localeCompare(g2));
-    }
-
-    if (this.state.isSortByLength) {
-      goods = [...goods].sort((g1, g2) => g1.length - g2.length);
-    }
-
-    if (this.state.isReversed) {
-      goods = [...goods].reverse();
-    }
-
-    return (
-      <div className="goods">
-        <ul className="goods__list">
-          {goods.map(goodsItem => (
-            <li className="goods__item" key={uuidv4()}>
-              {goodsItem}
-            </li>
-          ))}
-        </ul>
-
-        <div className="goods__config">
-          <button
-            className="goods__button"
-            type="button"
-            onClick={this.reverse}
-          >
-            Reverse
-          </button>
-          <button
-            className="goods__button"
-            type="button"
-            onClick={this.sortAlphabetically}
-          >
-            Sort alphabetically
-          </button>
-          <button
-            className="goods__button"
-            type="button"
-            onClick={this.sortByLength}
-          >
-            Sort by length
-          </button>
-          <button
-            className="goods__button"
-            type="button"
-            onClick={this.reset}
-          >
-            Reset
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <div className="goods">
+    <ul className="goods__list">
+      {goods.map(goodsItem => (
+        <li className="goods__item" key={uuidv4()}>
+          {goodsItem}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
