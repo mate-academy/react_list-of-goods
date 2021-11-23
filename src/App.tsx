6fe62d64c747fb93317f6fa4881c1ff9fc4ca651
Vladyslav Sak <Vladyslav.Sak@tx.local>
2021-11-23 11:11:51 +0200

    Solved a main task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GoodsList from './GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,101 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  goods: string[],
+  isShowList: boolean;
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    isShowList: false,
+  };
+
+  showList = () => {
+    this.setState({ isShowList: true });
+  };
+
+  reverseList = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  };
+
+  sortListByAlphabetically = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort(),
+    }));
+  };
+
+  sortListByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods]
+        .sort((prevGood, nextGood) => prevGood.length - nextGood.length),
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      goods: [...goodsFromServer],
+    });
+  };
+
+  render() {
+    const { goods, isShowList } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+
+        {isShowList
+          ? (
+            <GoodsList goods={goods} />
+          )
+          : (
+            <button
+              onClick={this.showList}
+              type="button"
+            >
+              Start
+            </button>
+          )}
+
+        {isShowList
+          && (
+            <div>
+              <button
+                onClick={this.reverseList}
+                type="button"
+              >
+                Reverse list
+              </button>
+
+              <button
+                onClick={this.sortListByAlphabetically}
+                type="button"
+              >
+                Sort alphabetically
+              </button>
+
+              <button
+                onClick={this.sortListByLength}
+                type="button"
+              >
+                Sort by length
+              </button>
+
+              <button
+                onClick={this.reset}
+                type="button"
+              >
+                Reset
+              </button>
+            </div>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
