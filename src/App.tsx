c6faab734cf29b951f81de3b62187ec08649068a
Taras Pyda <tapac1991@icloud.com>
2021-11-28 12:21:52 +0200

    Solution2

--- src/App.tsx
+++ src/App.tsx
@@ -138,16 +138,16 @@ export class App extends React.Component<{}, State> {
                 value={minLength}
                 onChange={this.handleChange}
               >
-                {goods.map((good, index) => {
-                  return (
-                    <option
-                      key={good}
-                      value={index + 1}
-                    >
-                      {index + 1}
-                    </option>
-                  );
-                })}
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
               </select>
             </form>
           </>
