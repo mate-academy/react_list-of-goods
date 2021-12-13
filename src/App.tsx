4d55d9c64c710a0287d002b11bfcaa83fe71d1cd
Lera Litfullina <litfullina@icloud.com>
2021-12-13 13:44:55 +0300

    list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodList } from './components/GoodList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,94 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[];
+  isShown: boolean;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    isShown: false,
+  };
+
+  sortAlphabetically = () => {
+    this.setState((state) => ({
+      goods: [...state.goods].sort((goodA, goodB) => goodA.localeCompare(goodB)),
+    }));
+  };
+
+  SortByLength = () => {
+    this.setState((state) => ({
+      goods: [...state.goods].sort((goodA, goodB) => goodA.length - goodB.length),
+    }));
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  };
+
+  reset = () => {
+    this.setState({ goods: goodsFromServer });
+  };
+
+  render() {
+    const { goods, isShown } = this.state;
+
+    return (
+      <div className="App">
+        <div className="goods">
+          {isShown
+          && (
+            <>
+              <GoodList goods={goods} />
+              <div className="btn-wrap">
+                <button
+                  type="button"
+                  className="btn-reverse btn"
+                  onClick={this.reverse}
+                >
+                  Reverse
+                </button>
+                <button
+                  type="button"
+                  className="btn-sortAlpha btn"
+                  onClick={this.sortAlphabetically}
+                >
+                  Sort alphabetically
+                </button>
+                <button
+                  type="button"
+                  className="btn-reset btn"
+                  onClick={this.reset}
+                >
+                  Reset
+                </button>
+                <button
+                  type="button"
+                  className="btn-sortByLen btn"
+                  onClick={this.SortByLength}
+                >
+                  Sort by length
+                </button>
+              </div>
+            </>
+          )}
+          {!isShown && (
+            <button
+              type="button"
+              className="btn-start btn"
+              onClick={() => this.setState({ isShown: true })}
+            >
+              Start your groccery journey
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
