77b4f079f9429c7c5d135fffe321487fff52a41e
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-12-14 18:33:15 +0200

    wrong

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 
+type State = {
+  goods: string[]
+};
+
 const goodsFromServer: string[] = [
   'Dumplings',
   'Carrot',
@@ -14,11 +18,44 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.PureComponent<{}, State> {
+  state = {
+    goods: [...goodsFromServer],
+  };
+
+  reverseList = () => {
+    this.setState(state => ({
+      goods: state.goods.reverse(),
+    }));
+    // eslint-disable-next-line no-console
+    console.log('reversed');
+  };
+
+  render() {
+    const { goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="App__title">Goods</h1>
+        <button
+          type="button"
+          onClick={this.reverseList}
+        >
+          Reverse
+        </button>
+        <ul className="goodList">
+          {goods.map((good) => (
+            <li
+              key={good}
+              className="goodList__item"
+            >
+              <span>{good}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
