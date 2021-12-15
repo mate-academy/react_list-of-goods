f5e2830564902827a13d802a155e5affcfd98376
nadiaH <nadiahrb79@gmailcom>
2021-12-15 12:04:55 +0200

    state

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,19 @@ const goodsFromServer: string[] = [
 ];
 
 interface State {
-  list: string[];
   goodsList: string[];
-  isClicked: boolean;
+  isStarted: boolean;
 }
 
 class App extends Component<{}, State> {
   state: State = {
-    list: [...goodsFromServer],
     goodsList: [...goodsFromServer],
-    isClicked: false,
+    isStarted: false,
   };
 
   displayList = () => {
     this.setState(state => ({
-      isClicked: !state.isClicked,
+      isStarted: !state.isStarted,
     }));
   };
 
@@ -52,54 +50,54 @@ class App extends Component<{}, State> {
   );
 
   resetHandler = () => (
-    this.setState(state => ({
-      goodsList: [...state.list],
-    }))
+    this.setState({
+      goodsList: [...goodsFromServer],
+    })
   );
 
   render() {
-    const { isClicked, goodsList } = this.state;
+    const { isStarted, goodsList } = this.state;
 
     return (
       <div className="App">
         <h1 className="App__title">Goods</h1>
         <button
           type="button"
-          className={`App__btn ${isClicked && 'App__hiden'}`}
+          className={`App__btn ${isStarted && 'App__hiden'}`}
           onClick={this.displayList}
         >
           Start
         </button>
         <button
           type="button"
-          className={`App__btn ${!isClicked && 'App__hiden'}`}
+          className={`App__btn ${!isStarted && 'App__hiden'}`}
           onClick={this.reverseHandler}
         >
           Reverse
         </button>
         <button
           type="button"
-          className={`App__btn ${!isClicked && 'App__hiden'}`}
+          className={`App__btn ${!isStarted && 'App__hiden'}`}
           onClick={this.sortByAlpha}
         >
           Sort alphabetically
         </button>
         <button
           type="button"
-          className={`App__btn ${!isClicked && 'App__hiden'}`}
+          className={`App__btn ${!isStarted && 'App__hiden'}`}
           onClick={this.resetHandler}
         >
           Reset
         </button>
         <button
           type="button"
-          className={`App__btn ${!isClicked && 'App__hiden'}`}
+          className={`App__btn ${!isStarted && 'App__hiden'}`}
           onClick={this.sortByLength}
         >
           Sort by length
         </button>
         <ul className="App__list">
-          {isClicked && goodsList.map(good => (
+          {isStarted && goodsList.map(good => (
             <li
               key={good}
               className="App__item"
