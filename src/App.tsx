d5a1d9110eae757a8d4106e0aa6752d9c31d153d
Andrii Koptiev <koptev.andrey87@gmail.com>
2022-01-23 00:53:59 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import classNames from 'classnames';
+import { GoodsList } from './GoodsList';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +21,164 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[];
+  isStarted: boolean;
+  isReversed: boolean;
+  goodLength: number;
+  sortBy: string;
+};
 
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    isStarted: false,
+    isReversed: false,
+    goodLength: 1,
+    sortBy: '',
+  };
+
+  startApp = () => {
+    this.setState({ isStarted: true });
+  };
+
+  reverseList = () => {
+    this.setState((state) => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortAlphabetical = () => {
+    this.setState({ sortBy: 'alphabet' });
+  };
+
+  sortByLength = () => {
+    this.setState({ sortBy: 'length' });
+  };
+
+  resetList = () => {
+    this.setState({ goods: goodsFromServer });
+    this.setState({ sortBy: '' });
+    this.setState({ goodLength: 1 });
+  };
+
+  // eslint-disable-next-line
+  handleChange = (event: any) => {
+    this.setState({ goodLength: event.target.value });
+  };
+
+  render() {
+    const {
+      goods,
+      isStarted,
+      isReversed,
+      goodLength,
+      sortBy,
+    } = this.state;
+
+    const visibleGoods = goods.filter(
+      good => good.length > goodLength,
+    );
+
+    visibleGoods.sort((good1, good2) => {
+      switch (sortBy) {
+        case 'alphabet':
+          return good1.localeCompare(good2);
+
+        case 'length':
+          return good1.length - good2.length;
+
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      visibleGoods.reverse();
+    }
+
+    return (
+      <div className="App">
+        {!isStarted && (
+          <Button
+            type="button"
+            className="App__start-button"
+            variant="contained"
+            color="success"
+            onClick={this.startApp}
+          >
+            Start
+          </Button>
+        )}
+        <div className={classNames(
+          'container',
+          { visible: isStarted },
+        )}
+        >
+          {isStarted && <GoodsList goods={visibleGoods} />}
+          <div className={classNames(
+            'goods-list__button-container',
+            { visible: isStarted },
+          )}
+          >
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Filter by length</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={this.state.goodLength}
+                label="Filter by length"
+                onChange={this.handleChange}
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={6}>6</MenuItem>
+                <MenuItem value={7}>7</MenuItem>
+                <MenuItem value={8}>8</MenuItem>
+                <MenuItem value={9}>9</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+
+              </Select>
+            </FormControl>
+            <Button
+              type="button"
+              variant="contained"
+              color="success"
+              onClick={this.sortAlphabetical}
+            >
+              Sort A-Z
+            </Button>
+            <Button
+              type="button"
+              variant="contained"
+              color="success"
+              onClick={this.sortByLength}
+            >
+              Sort by length
+            </Button>
+            <Button
+              type="button"
+              variant="contained"
+              color="success"
+              onClick={this.reverseList}
+            >
+              Reverse
+            </Button>
+            <Button
+              type="button"
+              variant="outlined"
+              color="error"
+              onClick={this.resetList}
+            >
+              Reset
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/no-named-as-default
 import App from './App';
 
 ReactDOM.render(
