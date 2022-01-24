bf0264c042290b0b9404291f743f453fd2a7df50
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2022-01-24 22:36:15 +0200

    updated on FC

--- src/components/GoodsList/GoodsList.tsx
+++ src/components/GoodsList/GoodsList.tsx
@@ -4,18 +4,10 @@ type Props = {
   goods: string[];
 };
 
-export class GoodsList extends React.Component<Props> {
-  a = 2;
-
-  render() {
-    const { goods } = this.props;
-
-    return (
-      <ul>
-        {goods.map(good => (
-          <li key={good}>{good}</li>
-        ))}
-      </ul>
-    );
-  }
-}
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li key={good}>{good}</li>
+    ))}
+  </ul>
+);
