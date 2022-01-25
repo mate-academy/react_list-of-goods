0afb014d69413fc5fa81c235c1f6a6bd56708d7d
Iryna Zolko <koko_irina@ukr.net>
2022-01-25 17:16:15 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import classNames from 'classnames';
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,114 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isListVisible: boolean;
+  listOfGoods: string[];
+};
+
+class App extends React.PureComponent<{}, State> {
+  state: State = {
+    isListVisible: false,
+    listOfGoods: goodsFromServer,
+  };
+
+  reverseList = () => {
+    this.setState(state => ({
+      listOfGoods: [...state.listOfGoods].reverse(),
+    }));
+  };
+
+  sortAlphabetically = () => {
+    this.setState(state => ({
+      listOfGoods: [...state.listOfGoods].sort((a, b) => a.localeCompare(b)),
+    }));
+  };
+
+  sortbyLength = () => {
+    this.setState(state => ({
+      listOfGoods: [...state.listOfGoods].sort((a, b) => a.length - b.length),
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      listOfGoods: [...goodsFromServer],
+    });
+  };
+
+  render() {
+    const { isListVisible, listOfGoods } = this.state;
+
+    return (
+      <div className="App">
+        <ul
+          className={
+            classNames(
+              'list',
+              { 'list-visible': isListVisible === true },
+            )
+          }
+        >
+          {listOfGoods.map(good => (
+            <li key={good}>
+              {good}
+            </li>
+          ))}
+        </ul>
+
+        <div className="buttons-block">
+          <button
+            type="button"
+            className={
+              classNames(
+                'button',
+                { 'button-hidden': isListVisible === true },
+              )
+            }
+            onClick={() => {
+              this.setState({
+                isListVisible: true,
+              });
+            }}
+          >
+            Start
+          </button>
+
+          <button
+            type="button"
+            className="button"
+            onClick={this.reverseList}
+          >
+            Reverse
+          </button>
+
+          <button
+            type="button"
+            className="button button-alphabet"
+            onClick={this.sortAlphabetically}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            className="button button-by-length"
+            onClick={this.sortbyLength}
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            className="button"
+            onClick={this.reset}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
