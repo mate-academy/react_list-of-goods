57c55144c6896cdad66cd3fe7ae9686c19a7c113
Maksym Shymanskij <makskampod@gmail.com>
2022-01-25 16:27:59 +0200

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,128 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component<{}, State> {
+  state = {
+    goods: goodsFromServer,
+    isListVisible: false,
+    sortBy: '',
+    isReversed: false,
+  };
+
+  reverse = () => {
+    this.setState((state) => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortByAlphabet = () => {
+    this.setState({
+      sortBy: 'alphabetically',
+    });
+  };
+
+  sortByLength = () => {
+    this.setState({
+      sortBy: 'length',
+    });
+  };
+
+  reset = () => {
+    this.setState(
+      {
+        isReversed: false,
+        sortBy: '',
+      },
+    );
+  };
+
+  prepareGoods = () => {
+    const { goods, sortBy, isReversed } = this.state;
+    const goodsCopy = [...goods];
+
+    goodsCopy.sort((g1, g2) => {
+      switch (sortBy) {
+        case 'alphabetically':
+          return g1.localeCompare(g2);
+        case 'length':
+          return g1.length - g2.length;
+        default:
+          return 0;
+      }
+    });
+    if (isReversed) {
+      goodsCopy.reverse();
+    }
+
+    return goodsCopy;
+  };
+
+  render() {
+    const { isListVisible } = this.state;
+    const preparedGoods = this.prepareGoods();
+
+    return (
+      <div className="App">
+        {!isListVisible && (
+          <div className="start">
+            <button
+              type="button"
+              className="btn btn-dark"
+              onClick={() => {
+                this.setState(({ isListVisible: true }));
+              }}
+            >
+              Start
+            </button>
+          </div>
+        )}
+
+        {isListVisible && (
+          <div className="goods">
+            <h1>Goods</h1>
+            <ul className="list-group">
+              {preparedGoods.map(good => (
+                <li key={good} className="list-group-item">
+                  {good}
+                </li>
+              ))}
+            </ul>
+
+            <div className="buttons">
+              <button
+                type="button"
+                onClick={this.sortByLength}
+                className="btn btn-dark sort-btn"
+              >
+                Sort by length
+              </button>
+              <button
+                type="button"
+                onClick={this.sortByAlphabet}
+                className="btn btn-dark sort-btn"
+              >
+                Sort by name
+              </button>
+              <button
+                type="button"
+                onClick={this.reverse}
+                className="btn btn-dark sort-btn"
+              >
+                Revers
+              </button>
+              <button
+                type="button"
+                onClick={this.reset}
+                className="btn btn-dark"
+              >
+                Reset
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
