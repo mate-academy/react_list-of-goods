a26931cfd53f39df4cd3109eeff3b7ada0a2c360
Taras Zherebetskiy <tarasj76@gmail.com>
2022-01-25 15:44:45 +0200

    fix and rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -22,64 +22,77 @@ enum SortBy {
 }
 
 interface State {
+  initialGoods: string[];
   goods: string[];
   isStart: boolean;
   isReverse: boolean;
   isAlphabetically: boolean;
   isLength: boolean;
-  sortedBy: SortBy;
   minLength: number;
 }
 
 class App extends React.Component<{}, State> {
   state: State = {
+    initialGoods: goodsFromServer,
     goods: goodsFromServer,
     isStart: true,
     isReverse: false,
     isAlphabetically: false,
     isLength: false,
-    sortedBy: SortBy.None,
     minLength: 1,
   };
 
   selected = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({
-      minLength: Number(event.currentTarget.value),
-    });
+    const length = Number(event.currentTarget.value);
+
+    if (length) {
+      this.setState((state) => ({
+        minLength: length,
+        goods: [...state.initialGoods.filter(item => (item.length >= length))],
+      }));
+    }
   };
 
   sortList = (sortBy: SortBy) => {
     switch (sortBy) {
       case SortBy.Alphabetically:
-        this.setState({
+        this.setState((state) => ({
           isAlphabetically: true,
           isLength: false,
-        });
+          goods: [...state.goods.sort((item1, item2) => (item1.localeCompare(item2)))],
+        }));
         break;
       case SortBy.Length:
-        this.setState({
+        this.setState((state) => ({
           isAlphabetically: false,
           isLength: true,
-        });
+          goods: [...state.goods.sort((item1, item2) => (item1.length - item2.length))],
+        }));
 
         break;
       default:
     }
 
     this.setState({
-      sortedBy: sortBy,
       isReverse: false,
     });
   };
 
   reset = () => {
-    this.setState({
+    this.setState((state) => ({
       isReverse: false,
       isAlphabetically: false,
       isLength: false,
-      sortedBy: SortBy.None,
       minLength: 1,
-    });
+      goods: [...state.initialGoods],
+    }));
+  };
+
+  reverse = () => {
+    this.setState((state) => ({
+      isReverse: !state.isReverse,
+      goods: [...state.goods.reverse()],
+    }));
   };
 
   render() {
@@ -89,26 +102,8 @@ class App extends React.Component<{}, State> {
       isReverse,
       isAlphabetically,
       isLength,
-      sortedBy,
       minLength,
     } = this.state;
-
-    const visibleGoods = goods.filter(item => item.length >= minLength);
-
-    visibleGoods.sort((item1, item2) => {
-      switch (sortedBy) {
-        case SortBy.Alphabetically:
-          return item1.localeCompare(item2);
-        case SortBy.Length:
-          return item1.length - item2.length;
-        default:
-          return 0;
-      }
-    });
-
-    if (isReverse) {
-      visibleGoods.reverse();
-    }
 
     return (
       <div className="App">
@@ -137,7 +132,7 @@ class App extends React.Component<{}, State> {
             <div className="functional">
               <ul className="list">
                 {
-                  visibleGoods.map((item) => (
+                  goods.map((item) => (
                     <li key={item} className="list__item">
                       {item}
                     </li>
@@ -151,11 +146,7 @@ class App extends React.Component<{}, State> {
                   'list__button',
                   { 'list__button--active': isReverse },
                 )}
-                onClick={() => (
-                  this.setState((state) => ({
-                    isReverse: !state.isReverse,
-                  }))
-                )}
+                onClick={this.reverse}
               >
                 Reverse
               </button>
@@ -182,18 +173,10 @@ class App extends React.Component<{}, State> {
                 Sort by length
               </button>
 
-              <button
-                type="button"
-                className="list__button-reset"
-                onClick={this.reset}
-              >
-                Reset
-              </button>
-
               <select
                 name="select"
                 className="select"
-                defaultValue="1"
+                value={minLength}
                 onChange={(event) => (this.selected(event))}
               >
                 <option value="1">1</option>
@@ -207,6 +190,14 @@ class App extends React.Component<{}, State> {
                 <option value="9">9</option>
                 <option value="10">10</option>
               </select>
+
+              <button
+                type="button"
+                className="list__button-reset"
+                onClick={this.reset}
+              >
+                Reset
+              </button>
             </div>
           )
         }
