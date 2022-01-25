c09b01c7ac80b03726bc15ddebab40bae8bbc2d8
Sesile26 <37846431+Sesile26@users.noreply.github.com>
2022-01-25 17:56:02 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.css';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,116 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[],
+  isVisible: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    isVisible: false,
+  };
+
+  start = () => {
+    this.setState(prev => ({
+      isVisible: !prev.isVisible,
+    }));
+  };
+
+  reverse = () => {
+    this.setState(prev => ({
+      goods: [...prev.goods].reverse(),
+    }));
+  };
+
+  sortByAlphabet = () => {
+    this.setState(prev => ({
+      goods: [...prev.goods].sort((a, b) => a.localeCompare(b)),
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState(prev => ({
+      goods: [...prev.goods].sort((a, b) => a.length - b.length),
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      goods: [...goodsFromServer],
+    });
+  };
+
+  render() {
+    const { isVisible, goods } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          type="button"
+          onClick={this.start}
+          className={
+            classNames(
+              'button',
+              { 'button-hidden': isVisible === true },
+            )
+          }
+        >
+          Start
+        </button>
+
+        <button
+          type="button"
+          onClick={this.reverse}
+          className={
+            classNames(
+              'button',
+            )
+          }
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          onClick={this.sortByAlphabet}
+          className={
+            classNames(
+              'button',
+            )
+          }
+        >
+          sortByAlphabet
+        </button>
+
+        <button
+          type="button"
+          onClick={this.sortByLength}
+          className={
+            classNames(
+              'button',
+            )
+          }
+        >
+          sortByLength
+        </button>
+
+        <button
+          type="button"
+          onClick={this.reset}
+          className={
+            classNames(
+              'button',
+            )
+          }
+        >
+          reset
+        </button>
+        {isVisible && <GoodsList goods={goods} />}
+      </div>
+    );
+  }
+}
 
 export default App;
