f6119f97f7b875020fb8b1a127d36d998a52ca68
Nikita Polishchuk <theetempleovpsychicyouth@gmail.com>
2022-01-25 20:52:55 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +14,106 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    isListDisplayed: false,
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  };
+
+  sortAplphabetically = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((goodA, goodB) => (
+        goodA.localeCompare(goodB)
+      )),
+    }));
+  };
+
+  reset = () => {
+    this.setState(({
+      goods: [...goodsFromServer],
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((goodA, goodB) => (
+        goodA.length - goodB.length
+      )),
+    }));
+  };
+
+  render() {
+    const { goods, isListDisplayed } = this.state;
+
+    return (
+      <div className="App">
+        {!isListDisplayed && (
+          <button
+            className="button is-primary"
+            type="button"
+            onClick={() => {
+              this.setState({
+                isListDisplayed: true,
+              });
+            }}
+          >
+            Start
+          </button>
+        )}
+
+        {isListDisplayed && (
+          <>
+            <ul className="menu-list">
+              {goods.map((product) => (
+                <li className="box" key={product}>
+                  {product}
+                </li>
+              ))}
+            </ul>
+
+            <button
+              className="button is-info"
+              type="button"
+              onClick={this.reverse}
+            >
+              Reverse
+            </button>
+
+            <button
+              className="button is-success"
+              type="button"
+              onClick={this.sortAplphabetically}
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              className="button is-warning"
+              type="button"
+              onClick={this.sortByLength}
+            >
+              Sort by length
+            </button>
+
+            <button
+              className="button is-danger"
+              type="button"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+          </>
+        )}
+
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bulma/css/bulma.css';
 
 ReactDOM.render(
   <App />,
