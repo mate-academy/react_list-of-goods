7e3944ac512f423156246df5283fac6248550f53
Inna <inna_temis@ukr.net>
2022-02-18 15:43:28 +0200

    List-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import './App.css';
+import GoodsList from './components/GoodsList';
 
-const goodsFromServer: string[] = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+interface State {
+  goods: string[],
+  isGoodsVisible: boolean,
+  display: string,
+}
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [
+      'Dumplings',
+      'Carrot',
+      'Eggs',
+      'Ice cream',
+      'Apple',
+      'Bread',
+      'Fish',
+      'Honey',
+      'Jam',
+      'Garlic',
+    ],
+    isGoodsVisible: false,
+    display: 'block',
+  };
+
+  getHide = () => {
+    this.setState({
+      isGoodsVisible: true,
+      display: 'none',
+    });
+  };
+
+  render() {
+    const { goods, isGoodsVisible, display } = this.state;
+
+    return (
+      <div>
+        <h1>Goods</h1>
+        <button
+          type="submit"
+          onClick={this.getHide}
+          style={{ display: `${display}` }}
+        >
+          Show
+        </button>
+        {isGoodsVisible && <GoodsList goods={goods} />}
+      </div>
+    );
+  }
+}
 
 export default App;
