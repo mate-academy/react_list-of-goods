7ce10fba85f35847fc2e61a9b4aee00ea9003eaa
Juliya <jursylian@gmail.com>
2022-02-19 14:50:39 +0200

    first solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodList } from './components/GoodList/GoodList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,131 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isReverse: boolean,
+  sortBy: string;
+  isVisible: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isReverse: false,
+    sortBy: '',
+    isVisible: false,
+  };
+
+  start = () => {
+    this.setState({ isVisible: true });
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReverse: !state.isReverse,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      isReverse: false,
+      sortBy: '',
+    });
+  };
+
+  sortByAlpha = () => {
+    this.setState({
+      sortBy: 'alphabet',
+    });
+  };
+
+  sortByLength = () => {
+    this.setState({
+      sortBy: 'length',
+    });
+  };
+
+  render() {
+    const {
+      isReverse,
+      sortBy,
+      isVisible,
+    } = this.state;
+    const goods = [...goodsFromServer];
+    const buttonClass = 'button is-primary';
+
+    switch (sortBy) {
+      case 'alphabet':
+        goods.sort((g1, g2) => g1.localeCompare(g2));
+        break;
+
+      case 'length':
+        goods.sort((g1, g2) => g1.length - g2.length);
+        break;
+
+      default:
+        break;
+    }
+
+    if (isReverse) {
+      goods.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1 className="title">Goods</h1>
+
+        <div className="buttons__container">
+          <button
+            type="button"
+            className={this.state.isReverse
+              ? `${buttonClass} is-active`
+              : `${buttonClass}`}
+            onClick={this.reverse}
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            className="button is-primary"
+            onClick={this.reset}
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            className={this.state.sortBy
+              ? `${buttonClass} is-active`
+              : `${buttonClass}`}
+            onClick={this.sortByAlpha}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            className={this.state.sortBy
+              ? `${buttonClass} is-active`
+              : `${buttonClass}`}
+            onClick={this.sortByLength}
+          >
+            Sort by length
+          </button>
+        </div>
+
+        {isVisible ? (
+          <GoodList goods={goods} />
+        ) : (
+          <div className="button__container">
+            <button
+              type="button"
+              className="button-start"
+              onClick={this.start}
+            >
+              Start
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
