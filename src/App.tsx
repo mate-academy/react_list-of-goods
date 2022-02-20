c9f0b630c47847f03f9909d76132373d5d8b91b6
Alex <asultan4ik@gmail.com>
2022-02-20 21:46:30 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,87 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  start: boolean
+  goods: string[]
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    start: false,
+    goods: [...goodsFromServer],
+  };
+
+  getStart = () => {
+    this.setState(state => (
+      { start: !state.start }
+    ));
+  };
+
+  reverse = () => {
+    this.setState(state => (
+      { goods: [...state.goods].reverse() }
+    ));
+  };
+
+  sortAlphabetically = () => {
+    this.setState(state => (
+      { goods: [...state.goods].sort((a, b) => a.localeCompare(b)) }
+    ));
+  };
+
+  Reset = () => {
+    this.setState((
+      { goods: [...goodsFromServer] }
+    ));
+  };
+
+  sortByLength = () => {
+    this.setState(state => (
+      { goods: [...state.goods].sort((a, b) => a.length - b.length) }
+    ));
+  };
+
+  render(): React.ReactNode {
+    const { start, goods } = this.state;
+
+    return (
+      <div className="App">
+        <button type="button" onClick={this.getStart} hidden={start}>Start</button>
+        <div hidden={!start}>
+          <ul>
+            {goods.map(good => (
+              <li>{good}</li>
+            ))}
+          </ul>
+          <button
+            type="button"
+            onClick={this.reverse}
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            onClick={this.sortAlphabetically}
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            onClick={this.Reset}
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            onClick={this.sortByLength}
+          >
+            Sort by length
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
