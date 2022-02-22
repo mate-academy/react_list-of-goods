d58f7e65dc15f2d4e1fa15399dd1959e0a5d0dc9
Alex <asultan4ik@gmail.com>
2022-02-22 06:50:33 +0200

    React.Component was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { List } from './List';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -15,20 +16,20 @@ const goodsFromServer: string[] = [
 ];
 
 type State = {
-  start: boolean
+  isVisible: boolean
   goods: string[]
 };
 
 class App extends React.Component<{}, State> {
   state: State = {
-    start: false,
+    isVisible: false,
     goods: [...goodsFromServer],
   };
 
-  getStart = () => {
-    this.setState(state => (
-      { start: !state.start }
-    ));
+  getIsVisible = () => {
+    this.setState(state => ({
+      isVisible: !state.isVisible,
+    }));
   };
 
   reverse = () => {
@@ -43,7 +44,7 @@ class App extends React.Component<{}, State> {
     ));
   };
 
-  Reset = () => {
+  reset = () => {
     this.setState((
       { goods: [...goodsFromServer] }
     ));
@@ -56,42 +57,41 @@ class App extends React.Component<{}, State> {
   };
 
   render(): React.ReactNode {
-    const { start, goods } = this.state;
+    const { isVisible, goods } = this.state;
 
     return (
       <div className="App">
-        <button type="button" onClick={this.getStart} hidden={start}>Start</button>
-        <div hidden={!start}>
-          <ul>
-            {goods.map(good => (
-              <li>{good}</li>
-            ))}
-          </ul>
-          <button
-            type="button"
-            onClick={this.reverse}
-          >
-            Reverse
-          </button>
-          <button
-            type="button"
-            onClick={this.sortAlphabetically}
-          >
-            Sort alphabetically
-          </button>
-          <button
-            type="button"
-            onClick={this.Reset}
-          >
-            Reset
-          </button>
-          <button
-            type="button"
-            onClick={this.sortByLength}
-          >
-            Sort by length
-          </button>
-        </div>
+        <button type="button" onClick={this.getIsVisible} hidden={isVisible}>Start</button>
+        {isVisible
+          && (
+            <div>
+              <List goods={goods} />
+              <button
+                type="button"
+                onClick={this.reverse}
+              >
+                Reverse
+              </button>
+              <button
+                type="button"
+                onClick={this.sortAlphabetically}
+              >
+                Sort alphabetically
+              </button>
+              <button
+                type="button"
+                onClick={this.reset}
+              >
+                Reset
+              </button>
+              <button
+                type="button"
+                onClick={this.sortByLength}
+              >
+                Sort by length
+              </button>
+            </div>
+          )}
       </div>
     );
   }
