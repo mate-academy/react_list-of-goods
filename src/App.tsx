fc6ced389c2b302af14ec94a1287f49177532d1e
Anna Serdechna <anserdechna@gmail.com>
2022-04-01 17:37:32 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
-const goodsFromServer: string[] = [
+interface Good {
+  name: string;
+  id:string;
+}
+
+const goodsFromServer: Good[] = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -12,13 +18,116 @@ const goodsFromServer: string[] = [
   'Honey',
   'Jam',
   'Garlic',
-];
+].map(good => ({
+  name: good,
+  id: uuidv4(),
+}));
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: Good[];
+  isListVisible: boolean;
+  isReversed: boolean;
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: [...goodsFromServer],
+    isListVisible: false,
+    isReversed: false,
+  };
+
+  sortedByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.name.length - b.name.length),
+    }));
+  };
+
+  sortedByAlphbet = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort((a, b) => a.name.localeCompare(b.name)),
+    }));
+  };
+
+  showList = () => {
+    this.setState(state => ({
+      isListVisible: !state.isListVisible,
+    }));
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      goods: [...goodsFromServer],
+    });
+  };
+
+  render() {
+    const { goods, isListVisible } = this.state;
+
+    const copyGood = [...goods];
+
+    if (this.state.isReversed) {
+      copyGood.reverse();
+    }
+
+    return (
+      <div className="App">
+        <button
+          type="button"
+          onClick={this.showList}
+        >
+          Start
+        </button>
+
+        {isListVisible
+       && (
+         <>
+           <button
+             type="button"
+             onClick={this.reverse}
+           >
+             Reverse
+           </button>
+
+           <button
+             type="button"
+             onClick={this.sortedByAlphbet}
+           >
+             Sort By Alphabet
+           </button>
+
+           <button
+             type="button"
+             onClick={this.sortedByLength}
+           >
+             Sort By Length
+           </button>
+
+           <button
+             type="button"
+             onClick={this.reset}
+           >
+             reset
+           </button>
+
+           {isListVisible
+          && (
+            <ul>
+              {copyGood.map(good => (
+                <li key={good.id}>{good.name}</li>
+              ))}
+            </ul>
+          )}
+         </>
+       )}
+      </div>
+    );
+  }
+}
 
 export default App;
