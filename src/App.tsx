eb83875f393130cd07627c747f73c7e8adb32719
Dina Samoilova <s13.dina@gmail.com>
2022-04-09 20:21:27 +0300

    solution react list of goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import classNames from 'classnames';
 import './App.css';
+import { GoodList } from './component/GoodsList';
+
+type Options = {
+  value: number,
+  label: string,
+};
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +21,194 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const options: Options[] = [
+  {
+    value: 1,
+    label: '1',
+  },
+  {
+    value: 2,
+    label: '2',
+  },
+  {
+    value: 3,
+    label: '3',
+  },
+  {
+    value: 4,
+    label: '4',
+  },
+  {
+    value: 5,
+    label: '5',
+  },
+  {
+    value: 6,
+    label: '6',
+  },
+  {
+    value: 7,
+    label: '7',
+  },
+  {
+    value: 8,
+    label: '8',
+  },
+  {
+    value: 9,
+    label: '9',
+  },
+  {
+    value: 10,
+    label: '10',
+  },
+];
+
+type State = {
+  goodList: string[] | [];
+  isReversed: boolean;
+  sort: boolean;
+  sortByLength: boolean;
+  choosedLength: number;
+  hidden: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goodList: [],
+    isReversed: false,
+    sort: false,
+    sortByLength: false,
+    choosedLength: 1,
+    hidden: true,
+  };
+
+  changeLength = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ choosedLength: +event.target.value });
+  };
+
+  startHandle = () => {
+    this.setState(state => ({
+      goodList: [...state.goodList, ...goodsFromServer],
+    }));
+
+    this.setState({ hidden: false });
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sort = () => {
+    this.setState({ sort: true });
+    this.setState({ sortByLength: false });
+  };
+
+  sortByLength = () => {
+    this.setState({ sortByLength: true });
+    this.setState({ sort: false });
+  };
+
+  reset = () => {
+    this.setState({ sort: false });
+    this.setState({ sortByLength: false });
+    this.setState({ choosedLength: 1 });
+  };
+
+  render() {
+    const {
+      goodList,
+      isReversed,
+      sort,
+      sortByLength,
+      choosedLength,
+      hidden,
+    } = this.state;
+
+    return (
+      <div className="App">
+        {goodList.length === 0 && (
+          <button
+            type="button"
+            className={classNames('button', 'start-button')}
+            onClick={this.startHandle}
+          >
+            START
+          </button>
+        )}
+
+        {!hidden && (
+          <>
+            <div
+              className="actions-buttons-block"
+            >
+              <select
+                className="select"
+                value={choosedLength}
+                onChange={this.changeLength}
+              >
+                {options.map(option => (
+                  <option
+                    value={option.value}
+                    key={option.label}
+                  >
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+
+              <button
+                type="button"
+                className={classNames('button', 'reverse')}
+                onClick={this.reverse}
+              >
+                Reverse
+              </button>
+
+              <button
+                type="button"
+                className={classNames('button', 'reset')}
+                onClick={this.reset}
+              >
+                Reset all sort
+              </button>
+            </div>
+
+            <div
+              className="sort-buttons"
+            >
+              <button
+                type="button"
+                className={classNames('button', 'albSort')}
+                onClick={this.sort}
+              >
+                Sort alphabetically
+              </button>
+
+              <button
+                type="button"
+                className={classNames('button', 'lengthSort')}
+                onClick={this.sortByLength}
+              >
+                Sort by length
+              </button>
+            </div>
+          </>
+
+        )}
+
+        <GoodList
+          goods={goodList}
+          isReversed={isReversed}
+          sorted={sort}
+          lengthSort={sortByLength}
+          choosedLength={choosedLength}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/component/GoodsList.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './GoodsList.css';
+
+type Props = {
+  goods: string[] | [],
+  isReversed: boolean,
+  sorted: boolean,
+  lengthSort: boolean,
+  choosedLength: number,
+};
+
+export const GoodList: React.FC<Props> = ({
+  goods,
+  isReversed,
+  sorted,
+  lengthSort,
+  choosedLength,
+}) => {
+  const visibleGoods = goods.filter(good => good.length >= choosedLength);
+
+  if (sorted) {
+    visibleGoods.sort();
+  }
+
+  if (lengthSort) {
+    visibleGoods.sort((good1, good2) => good1.length - good2.length);
+  }
+
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
+
+  return (
+    <>
+      <h1
+        className="title"
+      >
+        {visibleGoods.length}
+        {' '}
+        from
+        {' '}
+        {goods.length}
+        {' '}
+        Goods
+      </h1>
+
+      <ul
+        className="goods-list"
+      >
+        {visibleGoods.map(good => (
+          <li
+            key={good}
+            className="goods-list__item"
+          >
+            {good}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
