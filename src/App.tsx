0bc2dbb95e6ae7259c4b30c83de1c3307aced3c8
Roman Kovalchuk <roma220702@gmail.com>
2022-04-15 11:59:19 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// import { type } from 'os';
 import React from 'react';
 import './App.css';
 
@@ -14,11 +15,136 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[],
+  listVisible: boolean,
+  isReversed: boolean,
+  sortGoods: string,
+  sortLength: boolean,
+  reset: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: goodsFromServer,
+    listVisible: false,
+    isReversed: false,
+    sortGoods: '',
+    sortLength: false,
+    reset: false,
+  };
+
+  reverse = () => {
+    this.setState((state) => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortAlpha = () => {
+    this.setState({ sortGoods: 'Alpha' });
+  };
+
+  reset = () => {
+    this.setState({ reset: true });
+  };
+
+  sortLength = () => {
+    this.setState({ sortGoods: 'Length' });
+    this.setState((state) => ({
+      sortLength: !state.sortLength,
+    }));
+  };
+
+  render() {
+    const {
+      goods, listVisible, isReversed, sortGoods, sortLength, reset,
+    } = this.state;
+    const newGoods = goods.filter(good => good);
+
+    newGoods.sort((g1, g2) => {
+      switch (sortGoods) {
+        case 'Alpha':
+          return g1.localeCompare(g2);
+        case 'Length':
+          if (sortLength) {
+            return g1.length - g2.length;
+          }
+
+          return g2.length - g1.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (reset === true) {
+      newGoods.splice(0, goods.length, ...goods);
+    }
+
+    if (isReversed) {
+      newGoods.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {listVisible === false
+        && (
+          <button
+            type="button"
+            onClick={() => {
+              this.setState({ listVisible: true });
+            }}
+          >
+            Start
+          </button>
+        )}
+        {listVisible === true
+        && (
+          <>
+            <ul>
+              {newGoods.map(good => (
+                <li key={good}>
+                  {good}
+                </li>
+              ))}
+            </ul>
+            <button
+              type="button"
+              onClick={() => {
+                this.reverse();
+              }}
+            >
+              Reverse
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                this.sortAlpha();
+              }}
+            >
+              Sort alphabetically
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                this.sortLength();
+              }}
+            >
+              Sort by length
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                this.reset();
+              }}
+            >
+              Reset
+            </button>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
