2d00326736d806edfd1b8ad7477e3d3b20bc3638
murat-yesimbietov <murat.y@mate.academy>
2022-04-20 20:42:48 +0300

    Cypress test have been added

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 - Use [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript)
 
 ## Tasks
-1. At first show a button `Start` on the page
-1. After clicking the button show a `GoodsList` (ul > li) with the given goods and hide the button
-1. Add `Reverse` button to show the goods in reversed order
-1. Add `Sort alphabetically` button to show the goods in alphabetical order
-1. Add `Reset` button to show the goods in the initial order
-1. Add `Sort by length` button to show the goods ordered by name length
+1. At first show a button with text `Start` on the page
+1. After clicking the button shows a `GoodsList` (ul > li) with the given goods and hide the `Start` button
+1. Add button with text `Reverse` to show the goods in reversed order
+1. Add button with text `Sort alphabetically` to show the goods in alphabetical order
+1. Add button with text `Reset` to show the goods in the initial order
+1. Add button with text `Sort by length` to show the goods ordered by name length
 
 ## (*) Advanced tasks (require understanding of Forms in React)
 1. Add `<select>` with numbers from 1 to 10. (1 is default). All the previous buttons
--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,7 +1,114 @@
-// WRITE TESTS HERE
+const buttons = {
+  start: 'Start',
+  reverse: 'Reverse',
+  sortAlph: 'Sort alphabetically',
+  reset: 'Reset',
+  sortByLength: 'Sort by length',
+}
+
+const goodsFromServer = [
+  'Dumplings',
+  'Carrot',
+  'Eggs',
+  'Ice cream',
+  'Apple',
+  'Bread',
+  'Fish',
+  'Honey',
+  'Jam',
+  'Garlic'
+];
+
+Cypress.Commands.add('getButton', (text) => {
+  cy.contains('button', text);
+});
+
+Cypress.Commands.add('checkGoodsList', (items) => {
+  cy.get('li').each((li, index) => {
+    cy.wrap(li)
+      .should('contain', items[index]);
+  });
+});
 
 describe('Page', () => {
-  it('should be visitable', () => {
+  beforeEach(() => {
     cy.visit('/');
+
+    cy.getButton(buttons.start).click();
+  });
+  
+  it(`should hide 'Start' button after start`, () => {
+    cy.getButton(buttons.start).should('not.exist');
+  });
+
+  it(`should show the goods in the initial order after clicking on 'Start'`, () => {
+    cy.get('li').should('have.length', goodsFromServer.length);
+  });
+
+  it(`should show reversed goods after clicking on 'Reverse'`, () => {
+    const reverseListOfGoods = [
+      'Garlic',
+      'Jam',
+      'Honey',
+      'Fish',
+      'Bread',
+      'Apple',
+      'Ice cream',
+      'Eggs',
+      'Carrot',
+      'Dumplings'
+    ];
+
+    cy.getButton(buttons.reverse).click();
+
+    cy.checkGoodsList(reverseListOfGoods);
+  });
+
+  it(`should show the goods sorted alphabetically after clicking on 'Sort alphabetically'`, () => {
+    const sortedAlphabeticallyGoods = [
+      'Apple',
+      'Bread',
+      'Carrot',
+      'Dumplings',
+      'Eggs',
+      'Fish',
+      'Garlic',
+      'Honey',
+      'Ice cream',
+      'Jam'
+    ];
+
+    cy.getButton(buttons.sortAlph).click();
+
+    cy.checkGoodsList(sortedAlphabeticallyGoods);
+  });
+
+  it(`should show the goods sorted by length after clicking on 'Sort by length'`, () => {
+    const sortedByLengthGoods = [
+      'Jam',
+      'Eggs',
+      'Fish',
+      'Apple',
+      'Bread',
+      'Honey',
+      'Carrot',
+      'Garlic',
+      'Dumplings',
+      'Ice cream'
+    ];
+
+    cy.getButton(buttons.sortByLength).click();
+
+    cy.checkGoodsList(sortedByLengthGoods);
+  });
+
+  it(`should show the goods in the initial order after clicking on 'Reset'`, () => {
+    cy.getButton(buttons.sortByLength).click();
+
+    cy.getButton(buttons.sortAlph).click();
+
+    cy.getButton(buttons.reset).click();
+
+    cy.checkGoodsList(goodsFromServer);
   });
 });
