9c8faca0c84ab87d964e4e137000f6065b0ba6e6
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-04-23 14:06:59 +0300

    The solution was implemented

---
+++ src/components/GoodsList/GoodsList.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './GoodsList.scss';
+
+type ArrayOfGoods = {
+  goods: string[],
+  lengthForDisplay: number,
+};
+
+const GoodsList: React.FC<ArrayOfGoods> = ({ goods, lengthForDisplay }) => (
+  <>
+    {goods.map(good => {
+      return (
+        <li
+          className="GoodsList-item"
+          hidden={good.length >= lengthForDisplay}
+          key={good}
+        >
+          {good}
+        </li>
+      );
+    })}
+  </>
+);
+
+export default GoodsList;
