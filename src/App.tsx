c9c75787ee0f08a39dbc34b0de6d1fe256c9cae8
Olga <arnab@ukr.net>
2022-04-26 16:03:40 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +14,154 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+  type State = {
+    goods: string [];
+    visibilityHidden: boolean,
+    minLength: number,
+  };
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    goods: goodsFromServer,
+    visibilityHidden: true,
+    minLength: 1,
+  };
+
+  showList = () => {
+    this.setState((state) => ({
+      visibilityHidden: !state.visibilityHidden,
+    }));
+  };
+
+  sortByAbc = () => {
+    const { goods } = this.state;
+
+    this.setState({
+      goods: [...goods].sort((a, b) => a.localeCompare(b)),
+    });
+  };
+
+  sortByLength = () => {
+    const { goods } = this.state;
+
+    this.setState({
+      goods: [...goods].sort((a, b) => a.length - b.length),
+    });
+  };
+
+  reverse = () => {
+    const { goods } = this.state;
+
+    this.setState({
+      goods: [...goods].reverse(),
+    });
+  };
+
+  setLength = (length: string) => {
+    const { goods, minLength } = this.state;
+
+    this.setState({
+      minLength: +length,
+    });
+
+    return [...goods].filter(g => g.length >= minLength);
+  };
+
+  reset = () => {
+    this.setState({
+      goods: goodsFromServer,
+      minLength: 1,
+    });
+  };
+
+  render() {
+    const { goods, visibilityHidden, minLength } = this.state;
+    const sortedList = goods.filter(g => g.length >= minLength);
+
+    return (
+      <div className="App">
+        <h1 className="App__title">
+          Goods:
+          {' '}
+          {goods.length}
+        </h1>
+        {visibilityHidden
+          ? (
+            <button
+              type="button"
+              className="App__button"
+              onClick={this.showList}
+            >
+              Start
+            </button>
+          )
+          : (
+            <>
+              <div className="App__buttons">
+
+                <button
+                  type="button"
+                  className="App__button"
+                  onClick={this.reverse}
+                >
+                  Reverse
+                </button>
+                <button
+                  type="button"
+                  className="App__button"
+                  onClick={this.sortByAbc}
+                >
+                  Sort alphabetically
+                </button>
+                <button
+                  type="button"
+                  className="App__button"
+                  onClick={this.sortByLength}
+                >
+                  Sort by length
+                </button>
+                <button
+                  type="button"
+                  className="App__button"
+                  onClick={this.reset}
+                >
+                  Reset
+                </button>
+              </div>
+
+              <span className="App__button-description">
+                Set length:
+              </span>
+              <select
+                name="setLength"
+                id="setLength"
+                value={minLength}
+                onChange={({ target }) => {
+                  this.setLength(target.value);
+                }}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+
+              <ul className="App__list">
+                {sortedList.map(item => (
+                  <li key={item} className="App__item">
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+      </div>
+    );
+  }
+}
