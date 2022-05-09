b8d4c0f49e4921109e72ca025d7e7a19107e66f2
MariaBondar <marybondar777@gmail.com>
2022-05-09 15:51:50 +0200

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -18,88 +18,88 @@ const goodsFromServer: string[] = [
 type State = {
   visible: boolean;
   goodsAll: string[];
-}
+};
 
-class App extends React.Component<{},State> {
+class App extends React.Component<{}, State> {
   state = {
     goodsAll: goodsFromServer,
     visible: true,
-  }
+  };
 
   notVisible = () => {
     this.setState({
       visible: false,
-    })
-  }
+    });
+  };
 
   reverseGood = () => {
-    this.setState( state => ({
+    this.setState(state => ({
       goodsAll: [...state.goodsAll].reverse(),
-    }))
+    }));
   };
 
   sortedByAbc = () => {
-    this.setState( state => ({
+    this.setState(state => ({
       goodsAll: [...state.goodsAll].sort((a, b) => a.localeCompare(b)),
-    }))
+    }));
   };
 
   sortedByNumber = () => {
-    this.setState( state => ({
+    this.setState(state => ({
       goodsAll: [...state.goodsAll].sort((a, b) => a.length - b.length),
-    }))
+    }));
   };
 
   reset = () => {
     this.setState({
       goodsAll: goodsFromServer,
-    })
-  }
+    });
+  };
 
   removeNumber = () => {
-    this.setState( state => ({
-      goodsAll: state.goodsAll.slice()
-    }))
-  }
+    this.setState(state => ({
+      goodsAll: state.goodsAll.slice(),
+    }));
+  };
 
   render() {
     const { visible } = this.state;
 
     return (
       <div className="App">
-        <h1 className='App__title'>Goods</h1>
+        <h1 className="App__title">Goods</h1>
         {visible && (
           <button
-            className='App__button'
-            type='button'
+            className="App__button"
+            type="button"
             onClick={this.notVisible}
           >
-          Start
-        </button>
+            Start
+          </button>
         )}
-        <div>
+        <div className='App__block'>
           {
             !visible && (
               <>
-                <button onClick={this.reverseGood}>
+                <button onClick={this.reverseGood} type="button">
                   Reverse
                 </button>
-                <button onClick={this.sortedByAbc}>
+                <button onClick={this.sortedByAbc} type="button">
                   Sort alphabetically
                 </button>
-                <button onClick={this.sortedByNumber}>
+                <button onClick={this.sortedByNumber} type="button">
                   Sort by length
                 </button>
-                <button onClick={this.reset}>
+                <button onClick={this.reset} type="button">
                   Reset
                 </button>
-                <Goods goods={this.state.goodsAll}/>
+                <Goods goods={this.state.goodsAll} />
               </>
             )
           }
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/Goods.tsx
+++ src/Goods.tsx
@@ -6,16 +6,16 @@ type Props = {
 
 export const Goods: React.FC<Props> = ({ goods }) => {
   return (
-    <ul>
+    <ul className='goods__list'>
       {
         goods.map(good => (
-          <li key={good}>
+          <li key={good} className='goods__item'>
             {good}
           </li>
         ))
       }
     </ul>
-  )
-}
+  );
+};
 
 export default React.memo(Goods);
