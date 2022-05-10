8b0ccb0d84557dff6ce63312d82f34e85e915563
Misha Kravtsov <aniahucm@gmail.com>
2022-05-11 00:31:50 +0300

    list with sort and filter implemented

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,172 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  goods: Array<string>;
+  showList: boolean;
+  lettersLimit: number;
+  isReversed: boolean;
+  sortBy: string;
+}
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    showList: false,
+    lettersLimit: 1,
+    isReversed: false,
+    sortBy: 'none',
+  };
+
+  show = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      showList: !prevState.showList,
+    }));
+  };
+
+  reverse = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      isReversed: !prevState.isReversed,
+    }));
+  };
+
+  filterByLength = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newValue = e.currentTarget.value;
+
+    this.setState((prevState) => ({
+      ...prevState,
+      lettersLimit: Number(newValue),
+    }));
+  };
+
+  sortAlphabetically = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      sortBy: 'alph',
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      sortBy: 'leng',
+    }));
+  };
+
+  reset = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      sortBy: 'none',
+      isReversed: false,
+      lettersLimit: 1,
+    }));
+  };
+
+  render() {
+    const {
+      goods,
+      showList,
+      lettersLimit,
+      isReversed,
+      sortBy,
+    } = this.state;
+
+    console.log(lettersLimit)
+
+    const listToShow = goods.filter(good => good.length >= lettersLimit);
+
+    switch (sortBy) {
+      case 'alph':
+        listToShow.sort((g1, g2) => (g1.localeCompare(g2)));
+        break;
+
+      case 'leng':
+        listToShow.sort((g1, g2) => (g1.length - g2.length));
+        break;
+
+      default:
+        break;
+    }
+
+    if (isReversed) {
+      listToShow.reverse();
+    }
+
+    return (
+      <div className="app">
+        {!showList && (
+          <button
+            className="app__btn"
+            type="button"
+            onClick={this.show}
+          >
+            show list
+          </button>
+        )}
+
+        {showList
+        && (
+          <>
+            <div className="app__list-container">
+              <ul className="app__list-goods">
+                {listToShow.map(good => (
+                  <li key={good}>
+                    {good}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="app__sort-btn-container">
+              <button
+                className="app__btn"
+                type="button"
+                onClick={this.reverse}
+              >
+                reverse
+              </button>
+              <button
+                className="app__btn"
+                type="button"
+                onClick={this.sortAlphabetically}
+              >
+                Sort alphabetically
+              </button>
+              <button
+                className="app__btn"
+                type="button"
+                onClick={this.reset}
+              >
+                reset
+              </button>
+              <button
+                className="app__btn"
+                type="button"
+                onClick={this.sortByLength}
+              >
+                sort by length
+              </button>
+              <select
+                name="length"
+                className="app__lettersCountFilter"
+                value={lettersLimit}
+                onChange={this.filterByLength}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+          </>)}
+      </div>
+    );
+  }
+}
