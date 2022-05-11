94b0329c49a25d436ca1ad7850043c8231e4942d
Nikita Prepelytsia <prep.nikita@gmail.com>
2022-05-11 15:38:08 +0300

    listOfGoods

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import { GoodsList } from './components/GoodsList/GoodsList';
+import './App.scss';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,126 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[],
+  isReverse: boolean,
+  sortBy: string,
+  start: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: goodsFromServer,
+    isReverse: false,
+    sortBy: '',
+    start: false,
+  };
+
+  reverse = () => {
+    this.setState((prevState) => {
+      return {
+        isReverse: !prevState.isReverse,
+      };
+    });
+  };
+
+  sortByName = () => {
+    this.setState({
+      sortBy: 'name',
+      isReverse: false,
+    });
+  };
+
+  sortByLength = () => {
+    this.setState({
+      sortBy: 'length',
+      isReverse: false,
+    });
+  };
+
+  default = () => {
+    this.setState({
+      sortBy: '',
+      isReverse: false,
+    });
+  };
+
+  render() {
+    const {
+      goods,
+      isReverse,
+      sortBy,
+      start,
+    } = this.state;
+    const visibleGoods = [...goods];
+
+    visibleGoods.sort((goodOne, goodTwo) => {
+      switch (sortBy) {
+        case ('name'):
+          return goodOne.localeCompare(goodTwo);
+        case ('length'):
+          return goodOne.length - goodTwo.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (isReverse) {
+      visibleGoods.reverse();
+    }
+
+    return start
+      ? (
+        <div className="app">
+          <h1 className="app__title">Goods:</h1>
+          <GoodsList
+            goods={visibleGoods}
+          />
+          <div className="buttons">
+            <button
+              className="app__button"
+              type="button"
+              onClick={this.reverse}
+            >
+              Reverse
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={this.sortByName}
+            >
+              Sort by name
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={this.sortByLength}
+            >
+              Sort by length
+            </button>
+
+            <button
+              className="app__button"
+              type="button"
+              onClick={this.default}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      )
+      : (
+        <button
+          className="start"
+          type="button"
+          onClick={() => this.setState({ start: true })}
+        >
+          start
+        </button>
+      );
+  }
+}
 
 export default App;
