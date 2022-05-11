9883431cfeb71eb2e88e5de4f4a06bf5e3bc4238
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-05-11 22:16:07 +0200

    react_list-of-goods

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,82 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[],
+  isStartOnClick: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    goods: goodsFromServer,
+    isStartOnClick: false,
+  };
+
+  render() {
+    const { goods, isStartOnClick } = this.state;
+
+    return isStartOnClick ? (
+      <div>
+        <h1>Goods</h1>
+        <button
+          type="button"
+          onClick={() => (
+            this.setState({ goods: [...goods].reverse() })
+          )}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          onClick={() => (
+            this.setState({ goods: [...goods].sort() })
+          )}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          onClick={() => (
+            this.setState({ goods: goodsFromServer })
+          )}
+        >
+          Reset
+        </button>
+
+        <button
+          type="button"
+          onClick={() => (
+            this.setState({
+              goods: [...goods].sort((a, b) => (
+                a.length - b.length
+              )),
+            })
+          )}
+        >
+          Sort by length
+        </button>
+
+        <ol>
+          {goods.map(good => (
+            <li key={good}>
+              {good}
+            </li>
+          ))}
+        </ol>
+      </div>
+    ) : (
+      <button
+        type="button"
+        onClick={() => (
+          this.setState({ isStartOnClick: true })
+        )}
+      >
+        Start
+      </button>
+    );
+  }
+}
 
 export default App;
