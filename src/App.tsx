bdfd9495e568d089fe35d5f7b64b34b8fea6fae1
yurii837 <gorogotskiy@gmail.com>
2022-05-12 06:19:13 +0300

    with problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Goods from './Components/Goods/Goods';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,95 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  visibleGoods: string[],
+  isStart: boolean,
+  sortBy: string,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    visibleGoods: goodsFromServer,
+    isStart: false,
+    sortBy: '',
+  };
+
+  startButton = () => {
+    this.setState(pState => ({
+      isStart: !pState.isStart,
+    }));
+  };
+
+  reverseButton = () => {
+    this.setState(pState => ({
+      visibleGoods: pState.visibleGoods.reverse(),
+    }));
+  };
+
+  resetButton = () => {
+    this.setState({
+      visibleGoods: goodsFromServer,
+    });
+  };
+
+  sortButton(sortStr: string) {
+    this.setState(() => ({
+      sortBy: sortStr,
+    }));
+  }
+
+  render() {
+    const {
+      visibleGoods,
+      isStart,
+      sortBy,
+    } = this.state;
+
+    if (sortBy !== '') {
+      visibleGoods.sort((good1, good2) => {
+        switch (sortBy) {
+          case 'alphab': return good1.localeCompare(good2);
+          case 'length': return good1.length - good2.length;
+          default: return 0;
+        }
+      });
+    }
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        { !isStart
+          ? (<button type="button" onClick={this.startButton}>Start</button>)
+          : (
+            <>
+              <button type="button" onClick={this.reverseButton}>
+                Reverse
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  this.sortButton('alphab');
+                }}
+              >
+                Sort alphabetically
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  this.sortButton('length');
+                }}
+              >
+                Sort by length
+              </button>
+              <button type="button" onClick={this.startButton}>
+                resetButton
+              </button>
+              <Goods goods={visibleGoods} />
+            </>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Components/Goods/Goods.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type Props = {
+  goods: string[],
+};
+
+const Goods: React.FC<Props> = ({ goods }) => {
+  return (
+    <ul className="goods__list">
+      {
+        goods.map(good => (
+          <li key={good} className="goods__item">
+            {good}
+          </li>
+        ))
+      }
+    </ul>
+  );
+};
+
+export default Goods;
