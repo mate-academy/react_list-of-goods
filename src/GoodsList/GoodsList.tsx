613acc26e718808a4b2f6e86ab1fd3999cbf3963
Helen Petrovska <paznachka@gmail.com>
2022-05-12 13:23:06 +0300

    conditional to GoodsList was added

--- src/GoodsList/GoodsList.tsx
+++ src/GoodsList/GoodsList.tsx
@@ -9,16 +9,19 @@ interface Props {
 export const GoodsList: React.FC<Props> = ({ goodsList }) => (
   <>
     <h1 className="goods__title">Goods</h1>
-    <ul className="goods__list">
-      {goodsList.map(good => (
-        <li
-          className="goods__item"
-          key={good}
-        >
-          {good}
-        </li>
-      ))}
-    </ul>
+    {goodsList.length > 0
+      && (
+        <ul className="goods__list">
+          {goodsList.map(good => (
+            <li
+              className="goods__item"
+              key={good}
+            >
+              {good}
+            </li>
+          ))}
+        </ul>
+      )}
   </>
 
 );
