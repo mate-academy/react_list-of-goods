33ddc1d84f6bc8dced1a7ad315b8a2b90e507145
Oleksandr <pogrebnyak19999@gmail.com>
2022-05-27 11:43:59 +0200

    Solution

---
+++ src/components/Goods/Goods.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+type Props = {
+  goods: string[],
+  sortBy: string,
+  isReversed: boolean,
+  filterLength: number,
+};
+
+class Goods extends React.Component<Props, {}> {
+  render() {
+    const {
+      goods,
+      sortBy,
+      isReversed,
+      filterLength,
+    } = this.props;
+
+    let goodsSorted = [...goods].filter(item => item.length > filterLength);
+
+    switch (sortBy) {
+      case 'alphabet':
+        goodsSorted.sort((w1, w2) => {
+          return w1.localeCompare(w2);
+        });
+        break;
+      case 'length':
+        goodsSorted.sort((w1, w2) => {
+          return w1.length - w2.length;
+        });
+        break;
+      case 'initial':
+        goodsSorted = [...goods];
+        break;
+      default:
+        break;
+    }
+
+    if (isReversed) {
+      goodsSorted.reverse();
+    }
+
+    return (
+      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <nav aria-label="secondary mailbox folders">
+          <List>
+            {goodsSorted.map(item => (
+              <ListItem disablePadding>
+                <ListItemText
+                  key={item}
+                  primary={item}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </nav>
+      </Box>
+    );
+  }
+}
+
+export default Goods;
