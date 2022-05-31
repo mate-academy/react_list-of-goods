9a2a5b552e447e676387e84145e750bd62ea19d5
Andriy Fesych <andriyhimself@gmail.com>
2022-05-31 16:51:50 +0300

    Solution_0.1_no_extra

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,102 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type Props = {};
+
+type State = {
+  goodsList : string[],
+  visibility : boolean,
+  // lengthLimit : number,
+};
+
+class App extends React.Component <Props, State> {
+  state = {
+    goodsList: [...goodsFromServer],
+    visibility: false,
+    // lengthLimit: 1,
+  };
+
+  visibilitySwitch = () => {
+    this.setState((prevState) => ({
+      visibility: !prevState.visibility,
+    }));
+  };
+
+  buttonGenerator = (text:string, method:() => void) => (
+    <button type="button" onClick={method}>
+      {text}
+    </button>
+  );
+
+  reverseList = () => {
+    this.setState((prevState) => ({
+      goodsList: [...prevState.goodsList].reverse(),
+    }));
+  };
+
+  sortABC = () => {
+    this.setState((prevState) => ({
+      goodsList: [...prevState.goodsList].sort(
+        (good1, good2) => good1.localeCompare(good2),
+      ),
+    }));
+  };
+
+  initialOrder = () => {
+    this.setState(() => ({
+      goodsList: [...goodsFromServer],
+    }));
+  };
+
+  byLength = () => {
+    this.setState((prevState) => ({
+      goodsList: [...prevState.goodsList].sort(
+        (good1, good2) => good1.length - good2.length,
+      ),
+    }));
+  };
+
+  /*
+  changeLimit = (event) => {
+    this.setState(() => ({
+      lengthLimit: event.target.value,
+    }));
+  };
+
+  selectGenerator = () => (
+    <select
+      value={this.state.lengthLimit}
+      onChange={this.changeLimit}
+    >
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      { {options.map(({ value, label }, index) => <option value={value} >{label}</option>)} }
+    </select>
+  );
+  */
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {!this.state.visibility
+          && this.buttonGenerator('Start', this.visibilitySwitch)}
+        {this.state.visibility
+        && (
+          <>
+            {this.buttonGenerator('Reverse', this.reverseList)}
+            {this.buttonGenerator('Sort alphabetically', this.sortABC)}
+            {this.buttonGenerator('Reset', this.initialOrder)}
+            {this.buttonGenerator('Sort by length', this.byLength)}
+            {/* {this.selectGenerator()} */}
+            <GoodsList goods={this.state.goodsList} />
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
