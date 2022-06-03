9b03d75810acb859aca538a5ae085a77d7fe7d11
Anastasiia Sopotnytska <nastyasopotnytska14@gmail.com>
2022-06-03 15:32:22 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,118 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[];
+  clickStart: boolean;
+  visibleGoods: string[];
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    clickStart: false,
+    visibleGoods: [...goodsFromServer],
+  };
+
+  getReset() {
+    this.setState(prevState => ({ visibleGoods: [...prevState.goods] }));
+  }
+
+  getReverse() {
+    this.setState((prevState) => (
+      { visibleGoods: prevState.visibleGoods.reverse() }
+    ));
+  }
+
+  SortBylength() {
+    this.setState((prevState) => (
+      {
+        visibleGoods:
+        prevState.visibleGoods.sort((a, b) => a.length - b.length),
+      }
+    ));
+  }
+
+  sortAlphabetically() {
+    this.setState((prevState) => (
+      {
+        visibleGoods:
+        [...prevState.visibleGoods.sort((a, b) => a.localeCompare(b))],
+      }
+    ));
+  }
+
+  startFinishButton() {
+    this.setState((prevState) => ({ clickStart: !prevState.clickStart }));
+  }
+
+  render() {
+    const { clickStart } = this.state;
+    const { visibleGoods } = this.state;
+
+    // eslint-disable-next-line no-console
+    console.log(goodsFromServer);
+
+    return (
+      <div className="m-5 level-item has-text-centered">
+        <button
+          type="button"
+          className="button is-warning is-outlined"
+          onClick={() => this.startFinishButton()}
+        >
+          {!clickStart
+            ? 'Start'
+            : 'Finish'}
+        </button>
+        {clickStart
+          && (
+            <div>
+              <ul>
+                {visibleGoods.map(good => (
+                  <div key={good} className="has-text-white">
+                    <li>
+                      {good}
+                    </li>
+                  </div>
+                ))}
+              </ul>
+
+              <button
+                type="button"
+                className="button is-warning is-small mr-1"
+                onClick={() => this.getReverse()}
+              >
+                Reverse
+              </button>
+
+              <button
+                type="button"
+                className="button is-warning is-small mr-1"
+                onClick={() => this.sortAlphabetically()}
+              >
+                Sort alphabetically
+              </button>
+
+              <button
+                type="button"
+                className="button is-warning is-small mr-1"
+                onClick={() => this.SortBylength()}
+              >
+                Sort by length
+              </button>
+
+              <button
+                type="button"
+                className="button is-warning is-small mr-1"
+                onClick={() => this.getReset()}
+              >
+                Reset
+              </button>
+            </div>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
