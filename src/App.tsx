5ffa585fcd4963ada83ef34bba4e99d2eb1bd2d1
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-06-03 22:33:26 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,107 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isVisible: boolean,
+  goods: string[],
+  initialGoods: string[],
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    isVisible: false,
+    goods: [...goodsFromServer],
+    initialGoods: [...goodsFromServer],
+  };
+
+  render() {
+    const { isVisible, goods } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">Goods</h1>
+        {!isVisible
+          ? (
+            <button
+              className="start-button btn btn-warning"
+              type="button"
+              onClick={() => {
+                this.setState({ isVisible: true });
+              }}
+            >
+              Start
+            </button>
+          ) : (
+            <div>
+              <div
+                className="buttons btn-group"
+                role="group"
+                aria-label="Basic example"
+              >
+                <button
+                  className="btn btn-warning"
+                  type="button"
+                  onClick={() => {
+                    this.setState((state) => ({
+                      goods: [...state.goods].reverse(),
+                    }));
+                  }}
+                >
+                  Reverse
+                </button>
+
+                <button
+                  className="btn btn-warning"
+                  type="button"
+                  onClick={() => {
+                    this.setState((state) => ({
+                      goods: [...state.goods]
+                        .sort((goodA, goodB) => (goodA.localeCompare(goodB))),
+                    }));
+                  }}
+                >
+                  Sort alphabetically
+                </button>
+
+                <button
+                  className="btn btn-warning"
+                  type="button"
+                  onClick={() => {
+                    this.setState((state) => ({
+                      goods: state.initialGoods,
+                    }));
+                  }}
+                >
+                  Reset
+                </button>
+                <button
+                  className="btn btn-warning"
+                  type="button"
+                  onClick={() => {
+                    this.setState((state) => ({
+                      goods: [...state.goods]
+                        .sort((goodA, goodB) => goodA.length - goodB.length),
+                    }));
+                  }}
+                >
+                  Sort by length
+                </button>
+              </div>
+
+              <ul className="goods-list">
+                {goods.map(good => (
+                  <li key={good}>
+                    {good}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+          )}
+
+      </div>
+    );
+  }
+}
 
 export default App;
