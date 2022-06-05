94401563daa25d3557022f4e233ef9060b5c30ab
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2022-06-05 21:43:39 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,126 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  isVisible: boolean,
+  isReversed: boolean,
+  sortBy: string,
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    isVisible: false,
+    isReversed: false,
+    sortBy: '',
+  };
+
+  showList = () => {
+    this.setState((state) => ({
+      isVisible: !state.isVisible,
+    }));
+  };
+
+  reverse = () => {
+    this.setState((state) => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortByName = () => {
+    this.setState({
+      sortBy: 'name',
+    });
+  };
+
+  sortByLength = () => {
+    this.setState({
+      sortBy: 'length',
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      isReversed: false,
+      sortBy: '',
+    });
+  };
+
+  render() {
+    const { isVisible, isReversed, sortBy } = this.state;
+    const goodsCopy = goodsFromServer.slice();
+
+    goodsCopy.sort((a, b) => {
+      switch (sortBy) {
+        case 'name':
+          return a.localeCompare(b);
+        case 'length':
+          return a.length - b.length;
+        default:
+          return 0;
+      }
+    });
+
+    if (isReversed) {
+      goodsCopy.reverse();
+    }
+
+    return (
+      <div className="container text-center">
+
+        {!isVisible && (
+          <button
+            className="btn btn-success btn-lg"
+            type="button"
+            onClick={this.showList}
+          >
+            Start
+          </button>
+        )}
+
+        {isVisible && (
+          <>
+            <h1>Goods</h1>
+
+            <GoodsList products={goodsCopy} />
+
+            <div className="buttons d-flex justify-content-evenly">
+              <button
+                className="btn btn-warning"
+                type="button"
+                onClick={this.reverse}
+              >
+                Reverse
+              </button>
+
+              <button
+                className="btn btn-success"
+                type="button"
+                onClick={this.sortByName}
+              >
+                Sort alphabetically
+              </button>
+
+              <button
+                className="btn btn-success"
+                type="button"
+                onClick={this.sortByLength}
+              >
+                Sort by length
+              </button>
+
+              <button
+                className="btn btn-danger"
+                type="button"
+                onClick={this.reset}
+              >
+                Reset
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
