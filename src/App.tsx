bcc9125c940a1bb39e07d65401b9274085b9d36f
Alexandra Protyanova <protyanova22@gmail.com>
2022-06-05 16:56:29 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,42 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  buttonHidden: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    buttonHidden: false,
+  };
+
+  buttonHidden = () => {
+    this.setState(state => ({
+      buttonHidden: !state.buttonHidden,
+    }));
+  };
+
+  render() {
+    const { buttonHidden } = this.state;
+
+    return (
+      <div className="App">
+        {buttonHidden
+          ? (
+            <GoodsList goods={goodsFromServer} />
+          )
+          : (
+            <button
+              className="button is-success"
+              type="button"
+              onClick={this.buttonHidden}
+            >
+              Start
+            </button>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
