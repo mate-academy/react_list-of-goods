3cffeee32246ca4370d08db5bca0372cb9072892
Dmytro Semenenko <dmitriy.semenenko@gmail.com>
2022-06-07 17:04:49 +0300

    Solution React List Of Goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import { GoodsList } from './components/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,45 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+class App extends React.Component {
+  state = {
+    showButton: true,
+    showComponent: false,
+  };
+
+  showHide = () => {
+    this.setState({
+      showButton: false,
+      showComponent: true,
+    });
+  };
+
+  render() {
+    const { showButton, showComponent } = this.state;
+
+    return (
+      <div
+        className="
+          App
+          container
+          m-1
+          p-3
+        "
+      >
+        {showButton && (
+          <button
+            type="button"
+            className="button is-primary is-large"
+            onClick={this.showHide}
+          >
+            Start
+          </button>
+        )}
+
+        {showComponent && <GoodsList goods={goodsFromServer} />}
+      </div>
+    );
+  }
+}
 
 export default App;
