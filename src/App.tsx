8803299d52d884b339c11b237fa9764712d86af1
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-06-07 09:27:01 +0300

    Task Done v2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +14,122 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[];
+  isVisibleList: boolean;
+};
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state: State = {
+    goods: goodsFromServer,
+    isVisibleList: false,
+  };
+
+  showList = () => {
+    this.setState((state) => ({
+      isVisibleList: !state.isVisibleList,
+    }));
+  };
+
+  getReverseGoodsList = () => {
+    this.setState((state) => ({
+      goods: [...state.goods].reverse(),
+    }));
+  };
+
+  getSortAlphabetGoodsList = () => {
+    this.setState((state) => ({
+      goods: [...state.goods].sort((a, b) => a.localeCompare(b)),
+    }));
+  };
+
+  getSortLengthGoodsList = () => {
+    this.setState((state) => ({
+      goods: [...state.goods].sort((a, b) => a.length - b.length),
+    }));
+  };
+
+  getResetGoodsList = () => {
+    this.setState({
+      goods: goodsFromServer,
+    });
+  };
+
+  render() {
+    const { goods, isVisibleList } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="mainTitle title">
+          Goods
+        </h1>
+
+        {!isVisibleList
+          ? (
+            <button
+              type="button"
+              className="
+                button
+                is-success
+                btn
+                btn--center
+              "
+              onClick={this.showList}
+            >
+              Start
+            </button>
+          ) : (
+            <ul className="list list__wrapper">
+              {goods.map(good => (
+                <li className="list__item" key={good}>
+                  {good}
+                </li>
+              ))}
+
+              <div className="buttonWrapper">
+                <button
+                  type="button"
+                  className="button is-info btn"
+                  onClick={this.getReverseGoodsList}
+                >
+                  Reverse
+                </button>
+
+                <button
+                  type="button"
+                  className="button is-info btn"
+                  onClick={this.getSortAlphabetGoodsList}
+                >
+                  Sort alphabetically
+                </button>
+
+                <button
+                  type="button"
+                  className="button is-info btn"
+                  onClick={this.getSortLengthGoodsList}
+                >
+                  Sort by length
+                </button>
+
+                <button
+                  type="button"
+                  className="button is-warning btn"
+                  onClick={this.getResetGoodsList}
+                >
+                  Reset
+                </button>
+              </div>
+
+              <button
+                type="button"
+                className="button is-danger btn btn--center"
+                onClick={this.showList}
+              >
+                Hide List
+              </button>
+            </ul>
+          )}
+      </div>
+    );
+  }
+}
