895f25816656b6a43de827df3c4e2c15c062f8ca
Lebogang Mogane <mogane420@gmail.com>
2022-06-12 23:26:18 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { GoodsList } from './components/goodsList/goodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,103 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isListVisible: boolean;
+  isReversed: boolean;
+  sortBy: 'alphabetically' | 'byLength' | null;
+};
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    isListVisible: false,
+    isReversed: false,
+    sortBy: null,
+  };
+
+  showList = () => this.setState({ isListVisible: true });
+
+  reverseList = () => this.setState({ isReversed: true });
+
+  sortList = (sortOption: 'alphabetically' | 'byLength') => (
+    this.setState({ sortBy: sortOption })
+  );
+
+  resetList = () => this.setState({
+    isReversed: false,
+    sortBy: null,
+  });
+
+  render() {
+    const goodsCopy = [...goodsFromServer];
+    const {
+      isListVisible,
+      isReversed,
+      sortBy,
+    } = this.state;
+
+    switch (sortBy) {
+      case 'alphabetically':
+        goodsCopy.sort();
+        break;
+      case 'byLength':
+        goodsCopy.sort((itemA, itemB) => itemA.length - itemB.length);
+        break;
+      default:
+        break;
+    }
+
+    if (isReversed) {
+      goodsCopy.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {goodsFromServer.length}
+
+        <div className="buttons">
+          <button
+            type="button"
+            onClick={() => this.reverseList()}
+          >
+            Reverse
+          </button>
+
+          <button
+            type="button"
+            onClick={() => this.sortList('alphabetically')}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            onClick={() => this.sortList('byLength')}
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            onClick={() => this.resetList()}
+          >
+            Reset
+          </button>
+        </div>
+
+        {isListVisible
+          ? <GoodsList goods={goodsCopy} />
+          : (
+            <button
+              type="button"
+              onClick={() => this.showList()}
+            >
+              Start
+            </button>
+          )}
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/goodsList/goodsList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {
+  goods: string[],
+}
+
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <ul>
+    {goods.map(good => (
+      <li key={good}>{good}</li>
+    ))}
+  </ul>
+);
