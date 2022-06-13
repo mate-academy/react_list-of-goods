c081aa151eb8f7899e90676574181f5cff9ea117
MabuzaM <mabuzamfano@gmail.com>
2022-06-13 04:04:51 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bulma/css/bulma.min.css';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,142 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+interface State {
+  isClicked: boolean;
+  goodsList: string[];
+}
+
+class App extends React.Component<{}, State> {
+  state = {
+    isClicked: false,
+    goodsList: [...goodsFromServer],
+  };
+
+  renderList = (goods: string[]) => {
+    return goods.map(goodItem => (
+      <li key={goodItem}>
+        {
+          goodItem
+        }
+      </li>
+    ));
+  };
+
+  showList = (goods: string[]) => {
+    if (this.state.isClicked) {
+      return this.renderList(goods);
+    }
+
+    return null;
+  };
+
+  reverse = (goods: string[]) => {
+    this.setState({
+      goodsList: [...goods].reverse(),
+    });
+
+    return this.renderList(this.state.goodsList);
+  };
+
+  sortAlphabetically = (goods: string[]) => {
+    this.setState({
+      goodsList: [...goods].sort((goodItem1: string, goodItem2: string) => (
+        goodItem1.localeCompare(goodItem2)
+      )),
+    });
+
+    return this.renderList(this.state.goodsList);
+  };
+
+  reset = () => {
+    this.setState({
+      goodsList: goodsFromServer,
+    });
+
+    return this.renderList(this.state.goodsList);
+  };
+
+  sortByLength = (goods: string[]) => {
+    const sortedGoods = [...goods].sort((goodItem1, goodItem2) => (
+      goodItem1.length - goodItem2.length
+    ));
+
+    this.setState({
+      goodsList: sortedGoods,
+    });
+
+    return this.renderList(sortedGoods);
+  };
+
+  render() {
+    const showOrHideButton = this.state.isClicked
+      ? null
+      : (
+        <button
+          type="button"
+          onClick={
+            () => {
+              this.setState({
+                isClicked: true,
+              });
+            }
+          }
+        >
+          Start
+        </button>
+      );
+
+    return (
+      <div className="App">
+        <h1>Goods</h1>
+        {showOrHideButton}
+        <ul>
+          {
+            this.showList(this.state.goodsList)
+          }
+        </ul>
+        <hr />
+        <article className="button button-group">
+          <button
+            className="button is-link"
+            type="button"
+            onClick={() => {
+              this.reverse(this.state.goodsList);
+            }}
+          >
+            Reverse
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              this.sortAlphabetically(this.state.goodsList);
+            }}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              this.reset();
+            }}
+          >
+            Reset
+          </button>
+
+          <button
+            type="button"
+            onClick={() => {
+              this.sortByLength(this.state.goodsList);
+            }}
+          >
+            Sort by length
+          </button>
+        </article>
+      </div>
+    );
+  }
+}
 
 export default App;
