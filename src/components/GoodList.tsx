974de1dbe01a681e32440d533c8dfc58c5eac99c
Lilia Mazur <liliamazur2508@gmail.com>
2022-06-15 20:19:44 +0300

    solution

---
+++ src/components/GoodList.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+
+type Props = {
+  goodsList: string[],
+};
+
+type State = {
+  sortBy: string,
+  isReversed: boolean,
+};
+
+export class GoodList extends React.Component<Props, State> {
+  state = {
+    sortBy: '',
+    isReversed: false,
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sort = (value: string) => {
+    this.setState({ sortBy: value });
+  };
+
+  reset = () => {
+    this.setState({
+      sortBy: '',
+      isReversed: false,
+    });
+  };
+
+  getGoodList = () => {
+    const { sortBy, isReversed } = this.state;
+    let visibleList = this.props.goodsList;
+
+    if (sortBy) {
+      visibleList = [...visibleList].sort((a, b) => {
+        switch (sortBy) {
+          case 'alphabet':
+            return a.localeCompare(b);
+          case 'length':
+            return (a.length - b.length);
+          default:
+            return 0;
+        }
+      });
+    }
+
+    if (isReversed) {
+      visibleList = [...visibleList].reverse();
+    }
+
+    return visibleList;
+  };
+
+  render() {
+    const goodList = this.getGoodList();
+
+    return (
+      <>
+        <ul className="content is-medium">
+          {goodList.map(good => (
+            <li key={good}>
+              {good}
+            </li>
+          ))}
+        </ul>
+        <div className="buttons">
+          <button
+            type="button"
+            onClick={this.reverse}
+            className="button is-danger is-light is-rounded"
+          >
+            Reverse
+          </button>
+          <button
+            type="button"
+            onClick={() => this.sort('alphabet')}
+            className="button is-info is-light is-rounded"
+          >
+            Sort alphabetically
+          </button>
+          <button
+            type="button"
+            onClick={() => this.sort('length')}
+            className="button is-link is-light is-rounded"
+          >
+            Sort by length
+          </button>
+          <button
+            type="button"
+            onClick={this.reset}
+            className="button is-primary is-light is-rounded"
+          >
+            Reset
+          </button>
+        </div>
+      </>
+    );
+  }
+}
