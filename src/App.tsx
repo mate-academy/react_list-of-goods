2a009cf95fa9a3e1f4cf7fc347ed0d11771d691d
Olya Yevsieienko <olya.evsnko@gmail.com>
2022-07-13 18:39:28 +0300

    Created Goods List

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
 const goodsFromServer: string[] = [
@@ -14,11 +14,128 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  goods: string[],
+  isVisibleList: boolean,
+};
+
+class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    goods: goodsFromServer,
+    isVisibleList: false,
+  };
+
+  getStart = () => {
+    this.setState(state => ({
+      isVisibleList: !state.isVisibleList,
+    }));
+  };
+
+  reverseList = () => {
+    this.setState(state => ({
+      goods: [...state.goods].reverse(),
+    }));
+  };
+
+  sortByName = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort(
+        (good1, good2) => good1.localeCompare(good2),
+      ),
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState(state => ({
+      goods: [...state.goods].sort(
+        (good1, good2) => good1.length - good2.length,
+      ),
+    }));
+  };
+
+  resetChanges = () => {
+    this.setState({
+      goods: [...goodsFromServer],
+    });
+  };
+
+  render() {
+    const { goods, isVisibleList } = this.state;
+
+    return (
+      <div className="App block">
+        <h1
+          className="
+            App__title
+            title
+            is-2
+          "
+        >
+          List of Goods
+        </h1>
+
+        {!isVisibleList && (
+          <button
+            className="
+              button
+              is-success
+              is-medium
+            "
+            type="button"
+            onClick={this.getStart}
+          >
+            Start
+          </button>
+        )}
+
+        {isVisibleList && (
+          <>
+            <ul className="list">
+              {goods.map(good => (
+                <li className="item media" key={good}>
+                  <p>{good}</p>
+                </li>
+              ))}
+            </ul>
+
+            <div className="buttons">
+              <button
+                className="button is-warning"
+                type="button"
+                onClick={this.reverseList}
+              >
+                Reverse
+              </button>
+
+              <button
+                className="button is-link"
+                type="button"
+                onClick={this.sortByName}
+              >
+                Sort alphabetically
+              </button>
+
+              <button
+                className="button is-info"
+                type="button"
+                onClick={this.sortByLength}
+              >
+                Sort by length
+              </button>
+
+              <button
+                className="button is-danger"
+                type="button"
+                onClick={this.resetChanges}
+              >
+                Reset
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
