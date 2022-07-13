bedbfc7406972cfffa220676f52b24093d2309ad
mira1zu <mira1zu733@gmail.com>
2022-07-13 19:53:48 +0200

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -18,155 +18,39 @@ const goodsFromServer: string[] = [
 ];
 
 interface State {
-  initialGoods: string[];
   isStarted: boolean;
-  isReversed: boolean;
-  sortBy: string;
-  minLength: number;
 }
 
 class App extends React.PureComponent<{}, State> {
   state: State = {
-    initialGoods: goodsFromServer,
-    isStarted: true,
-    isReversed: false,
-    sortBy: 'reset',
-    minLength: 1,
+    isStarted: false,
   };
 
-  onStartButtonClick = () => {
+  handleStartButtonClick = () => {
     this.setState({
-      isStarted: false,
-    });
-  };
-
-  onReverseButtonClick = () => {
-    this.setState(state => ({
-      isReversed: !state.isReversed,
-    }));
-  };
-
-  onSortByNameButtonClick = () => {
-    this.setState({
-      isReversed: false,
-      sortBy: 'name',
-    });
-  };
-
-  onSortByLengthButtonClick = () => {
-    this.setState({
-      isReversed: false,
-      sortBy: 'length',
-    });
-  };
-
-  onResetButtonClick = () => {
-    this.setState({
-      minLength: 1,
-      isReversed: false,
-      sortBy: 'reset',
+      isStarted: true,
     });
   };
 
   render() {
-    const {
-      initialGoods,
-      isStarted,
-      isReversed,
-      sortBy,
-      minLength,
-    } = this.state;
-
-    const currentGoods = [...initialGoods]
-      .filter((product) => product.length >= minLength);
-
-    currentGoods.sort((firstProduct, secondProduct) => {
-      switch (sortBy) {
-        case 'name':
-          return firstProduct.localeCompare(secondProduct);
-
-        case 'length':
-          return firstProduct.length - secondProduct.length;
-
-        default:
-          return 0;
-      }
-    });
-
-    if (isReversed) {
-      currentGoods.reverse();
-    }
-
     return (
       <div className="App">
         <div className="box container is-max-desktop">
           <h1 className="title">Goods</h1>
 
-          {isStarted && (
-            <button
-              type="button"
-              className="button is-primary is-fullwidth"
-              onClick={this.onStartButtonClick}
-            >
-              Start
-            </button>
-          )}
-
-          {!isStarted && (
-            <div className="columns">
-              <div className="column">
-                <GoodsList goods={currentGoods} />
-              </div>
-
-              <div className="column">
-                <div className="interaction-container">
-                  <button
-                    type="button"
-                    className="button is-primary is-fullwidth"
-                    onClick={this.onReverseButtonClick}
-                  >
-                    Reverse
-                  </button>
-                  <button
-                    type="button"
-                    className="button is-primary is-fullwidth"
-                    onClick={this.onSortByNameButtonClick}
-                  >
-                    Sort alphabetically
-                  </button>
-                  <button
-                    type="button"
-                    className="button is-primary is-fullwidth"
-                    onClick={this.onResetButtonClick}
-                  >
-                    Reset
-                  </button>
-                  <button
-                    type="button"
-                    className="button is-primary is-fullwidth"
-                    onClick={this.onSortByLengthButtonClick}
-                  >
-                    Sort by length
-                  </button>
-                  <div className="select is-primary is-fullwidth">
-                    <select
-                      name="minLength"
-                      onChange={({ target }) => {
-                        this.setState({
-                          minLength: +target.value,
-                        });
-                      }}
-                    >
-                      {[...new Array(10)].map((_, i) => (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <option key={i} value={i + 1}>{i + 1}</option>
-                      ))}
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
+          {this.state.isStarted
+            ? (
+              <GoodsList goods={goodsFromServer} />
+            )
+            : (
+              <button
+                type="button"
+                className="button is-primary is-fullwidth"
+                onClick={this.handleStartButtonClick}
+              >
+                Start
+              </button>
+            )}
         </div>
       </div>
     );
--- src/components/GoodsList.tsx
+++ src/components/GoodsList.tsx
@@ -4,12 +4,162 @@ interface Props {
   goods: string[];
 }
 
-const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
-    {goods.map((product) => (
-      <li key={product}>{product}</li>
-    ))}
-  </ul>
-);
+interface State {
+  sortBy: string;
+  isToReverse: boolean;
+  isToReset: boolean;
+  minLength: number;
+}
+
+class GoodsList extends React.PureComponent<Props, State> {
+  state: State = {
+    sortBy: 'reset',
+    isToReverse: false,
+    isToReset: false,
+    minLength: 1,
+  };
+
+  handleReverseButtonClick = () => {
+    this.setState(state => ({
+      isToReverse: !state.isToReverse,
+    }));
+  };
+
+  handleSortByNameButtonClick = () => {
+    this.setState({
+      sortBy: 'name',
+    });
+  };
+
+  handleSortByLengthButtonClick = () => {
+    this.setState({
+      sortBy: 'length',
+    });
+  };
+
+  handleResetButtonClick = () => {
+    this.setState({
+      isToReverse: false,
+      sortBy: 'reset',
+      minLength: 1,
+    });
+  };
+
+  handleLengthSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({
+      minLength: Number(event.target.value),
+    });
+  };
+
+  prepareGoods = () => {
+    const {
+      sortBy,
+      isToReverse,
+      isToReset,
+      minLength,
+    } = this.state;
+
+    if (isToReset) {
+      return this.props.goods;
+    }
+
+    const currentGoods = this.props.goods.filter(product => (
+      product.length >= minLength
+    ));
+
+    switch (sortBy) {
+      case 'name': {
+        currentGoods.sort((firstProduct, secondProduct) => (
+          firstProduct.localeCompare(secondProduct)
+        ));
+        break;
+      }
+
+      case 'length': {
+        currentGoods.sort((firstProduct, secondProduct) => (
+          firstProduct.length - secondProduct.length
+        ));
+        break;
+      }
+
+      default:
+        break;
+    }
+
+    if (isToReverse) {
+      currentGoods.reverse();
+    }
+
+    return currentGoods;
+  };
+
+  render() {
+    return (
+      <div className="columns">
+        <div className="column">
+          <ul>
+            {this.prepareGoods().map(product => (
+              <li key={product}>{product}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="column">
+          <div className="interaction-container">
+            <button
+              type="button"
+              className="button is-primary is-fullwidth"
+              onClick={this.handleReverseButtonClick}
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              className="button is-primary is-fullwidth"
+              onClick={this.handleSortByNameButtonClick}
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              className="button is-primary is-fullwidth"
+              onClick={this.handleResetButtonClick}
+            >
+              Reset
+            </button>
+
+            <button
+              type="button"
+              className="button is-primary is-fullwidth"
+              onClick={this.handleSortByLengthButtonClick}
+            >
+              Sort by length
+            </button>
+
+            <div className="select is-primary is-fullwidth">
+              <select
+                name="minLength"
+                onChange={this.handleLengthSelectChange}
+              >
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+                <option value={6}>6</option>
+                <option value={7}>7</option>
+                <option value={8}>8</option>
+                <option value={9}>9</option>
+                <option value={10}>10</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default React.memo(GoodsList);
