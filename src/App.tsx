d94ae58db5bb3a352b250daa88af19f25d4f2eb3
Yaroslav Baiduk <byaa99@gmail.com>
2022-07-15 12:39:49 +0300

    Compleated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import classNames from 'classnames';
 import React from 'react';
 import './App.css';
+import GoodsList from './Components/GoodsList/GoodsList';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +16,153 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+type State = {
+  isStarted: boolean,
+  goods: string[],
+  isReversed: boolean,
+  isSortedAlph: boolean,
+  isSortedLength: boolean,
+  length: number,
+};
+
+class App extends React.Component<{}, State> {
+  state = {
+    isStarted: false,
+    goods: goodsFromServer,
+    isReversed: false,
+    isSortedAlph: false,
+    isSortedLength: false,
+    length: 1,
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  sortByAlph = () => {
+    this.setState(state => ({
+      isSortedLength: false,
+      isSortedAlph: !state.isSortedAlph,
+    }));
+  };
+
+  sortByLength = () => {
+    this.setState(state => ({
+      isSortedAlph: false,
+      isSortedLength: !state.isSortedLength,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      isSortedLength: false,
+      isSortedAlph: false,
+      isReversed: false,
+      length: 1,
+    });
+  };
+
+  render() {
+    const {
+      isStarted,
+      goods,
+      isReversed,
+      isSortedAlph,
+      isSortedLength,
+      length,
+    } = this.state;
+
+    const visibleGoods = goods.filter(good => good.length >= length);
+
+    if (isSortedAlph) {
+      visibleGoods.sort((a, b) => a.localeCompare(b));
+    }
+
+    if (isSortedLength) {
+      visibleGoods.sort((a, b) => a.length - b.length);
+    }
+
+    if (isReversed) {
+      visibleGoods.reverse();
+    }
+
+    return (
+      <div className="App">
+        <h1 className="title">Goods</h1>
+
+        {isStarted ? (
+          <>
+            <GoodsList goods={visibleGoods} />
+
+            <div className="select is-success">
+              <select
+                name="length"
+                value={length}
+                onChange={(event) => this.setState({
+                  length: +event.target.value,
+                })}
+              >
+                {[...new Array(10)].map((_, i) => (
+                  <option value={i + 1}>{i + 1}</option>
+                ))}
+              </select>
+            </div>
+
+            <button
+              type="button"
+              onClick={this.reverse}
+              className={classNames(
+                'button is-info',
+                { 'is-success': isReversed },
+              )}
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              onClick={this.sortByAlph}
+              className={classNames(
+                'button is-info',
+                { 'is-success': isSortedAlph },
+              )}
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              onClick={this.sortByLength}
+              className={classNames(
+                'button is-info',
+                { 'is-success': isSortedLength },
+              )}
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              onClick={this.reset}
+              className="button is-danger"
+            >
+              Reset
+            </button>
+          </>
+        ) : (
+          <button
+            type="button"
+            onClick={() => this.setState({ isStarted: true })}
+            className="button is-primary"
+          >
+            Start
+          </button>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
