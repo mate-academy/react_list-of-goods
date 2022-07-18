5ce44b30652c39a9c056a6d77c25a270e3b6612e
SAMAEV <andreysamaev@gmail.com>
2022-07-18 15:17:12 +0300

    fix after Mreview

--- src/App.tsx
+++ src/App.tsx
@@ -18,19 +18,58 @@ type State = {
   goods: string[],
   isVisible: boolean,
   isReversed: boolean,
-  sortBy: string,
+  sortBy: SortType,
+  isSorted: boolean,
 };
 
+enum SortType {
+  ALPHABET,
+  LENGTH,
+  NONE,
+}
+
+function getOrderedGoods(
+  goods: string[],
+  sortBy: SortType,
+  isSorted: boolean,
+  isReversed: boolean,
+) {
+  const copyGoods = [...goods];
+
+  if (isSorted) {
+    copyGoods.sort((good1, good2) => {
+      switch (sortBy) {
+        case SortType.LENGTH:
+          return good1.length - good2.length;
+        case SortType.ALPHABET:
+          return good1.localeCompare(good2);
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  if (isReversed) {
+    copyGoods.reverse();
+  }
+
+  return copyGoods;
+}
+
 class App extends React.Component<{}, State> {
-  state = {
+  state: Readonly<State> = {
     goods: goodsFromServer,
     isVisible: true,
     isReversed: false,
-    sortBy: '',
+    isSorted: false,
+    sortBy: SortType.NONE,
   };
 
   toShowGoods = () => {
-    this.setState({ isVisible: false });
+    this.setState((state) => ({
+      isVisible: !state.isVisible,
+    }));
   };
 
   reverse = () => {
@@ -40,42 +79,36 @@ class App extends React.Component<{}, State> {
   };
 
   sortAlphabetically = () => {
-    this.setState({ sortBy: 'alphabet' });
+    this.setState({
+      sortBy: SortType.ALPHABET,
+      isSorted: true,
+    });
   };
 
   sortByLength = () => {
-    this.setState({ sortBy: 'length' });
+    this.setState({
+      sortBy: SortType.LENGTH,
+      isSorted: true,
+    });
   };
 
   reset = () => {
-    this.setState({ isReversed: false, sortBy: '' });
+    this.setState({
+      isReversed: false,
+      sortBy: SortType.NONE,
+      isSorted: false,
+    });
   };
 
   render() {
-    const {
-      goods,
-      isReversed,
-      sortBy,
-      isVisible,
-    } = this.state;
+    const { isVisible } = this.state;
 
-    const copyGoods = [...goods];
-
-    copyGoods.sort((good1, good2) => {
-      switch (sortBy) {
-        case 'length':
-          return good1.length - good2.length;
-        case 'alphabet':
-          return good1.localeCompare(good2);
-
-        default:
-          return 0;
-      }
-    });
-
-    if (isReversed) {
-      copyGoods.reverse();
-    }
+    const sortedGoods = getOrderedGoods(
+      this.state.goods,
+      this.state.sortBy,
+      this.state.isSorted,
+      this.state.isReversed,
+    );
 
     return (
       <div className="App box">
@@ -83,22 +116,22 @@ class App extends React.Component<{}, State> {
         {isVisible && (
           <div>
             <h3 className="message-body">
-            Everything starts here.
-            Just push the button
-          </h3>
-          <button
-            className="button button__start"
-            type="button"
-            onClick={this.toShowGoods}
-          >
-            Start
-          </button>
+              Everything starts here.
+              Just push the button
+            </h3>
+            <button
+              className="button button__start"
+              type="button"
+              onClick={this.toShowGoods}
+            >
+              Start
+            </button>
           </div>
         )}
         {!isVisible && (
           <div className="wrapper">
             <ul className="list">
-              {copyGoods.map(good => (
+              {sortedGoods.map(good => (
                 <li
                   className="list__item"
                   key={good}
