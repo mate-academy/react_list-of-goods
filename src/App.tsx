a7594cf3a54534dabf5484afb45c062e163cdb32
PavloMarkov <r9markovpn@gmail.com>
2022-08-10 18:48:28 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import { GoodsList } from './GoodsList';
 import './App.css';
 
-const goodsFromServer = [
+export const goodsFromServer = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -22,54 +23,178 @@ enum SortType {
 }
 
 // Use this function in the render method
-function getReorderedGoods(
-  goods: string[],
-  sortType: SortType,
-  isReversed: boolean,
-) {
-  // Not to mutate the original array
-  const visibleGoods = [...goods];
 
-  // Sort and reverse goods if needed
-  // ...
-
-  return visibleGoods;
-}
-
-// DON'T save goods to the state
 type State = {
   isStarted: boolean,
   isReversed: boolean,
   sortType: SortType,
+  minLength: number,
 };
 
-export const App = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+export class App extends React.Component<{}, State> {
+  state = {
+    isStarted: false,
+    isReversed: false,
+    sortType: 0,
+    minLength: 1,
+  };
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+  getReorderedGoods = (
+    goods: string[],
+    sortType: SortType,
+    isReversed: boolean,
+    minLength: number,
+  ) => {
+    const visibleGoods = [...goods];
 
-    <button type="button">
-      Sort by length
-    </button>
+    visibleGoods.sort((a, b) => {
+      switch (sortType) {
+        case 1:
+          return a.localeCompare(b);
 
-    <button type="button">
-      Reverse
-    </button>
+        case 2:
+          return a.length - b.length;
 
-    <button type="button">
-      Reset
-    </button>
+        default:
+          return 0;
+      }
+    });
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+    return isReversed
+      ? visibleGoods.filter(good => (
+        good.length >= minLength
+      )).reverse()
+      : visibleGoods.filter(good => (
+        good.length >= minLength
+      ));
+  };
+
+  clickOnStart = () => {
+    this.setState(state => ({
+      isStarted: !state.isStarted,
+    }));
+  };
+
+  sortAlphabetically = () => {
+    this.setState({
+      sortType: 1,
+    });
+  };
+
+  sortByLength = () => {
+    this.setState({
+      sortType: 2,
+    });
+  };
+
+  reverseSort = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  // sortLength = (event) => {
+  //   this.setState({
+  //     minLength:
+  //   })
+  // }
+
+  reset = () => {
+    this.setState({
+      sortType: 0,
+      isReversed: false,
+      minLength: 1,
+    });
+  };
+
+  render() {
+    const {
+      isStarted,
+      sortType,
+      isReversed,
+      minLength,
+    } = this.state;
+
+    return (
+      <div className="App">
+        {!isStarted && (
+          <button
+            type="button"
+            className="button is-success"
+            onClick={this.clickOnStart}
+          >
+            Start
+          </button>
+        )}
+
+        {isStarted && (
+          <>
+            <button
+              type="button"
+              className="button is-info"
+              onClick={this.sortAlphabetically}
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              className="button is-link"
+              onClick={this.sortByLength}
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              className="button is-warning"
+              onClick={this.reverseSort}
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              className="button is-danger"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+
+            <GoodsList goods={
+              this.getReorderedGoods(
+                goodsFromServer, sortType, isReversed, minLength,
+              )
+            }
+            />
+
+            <p>Select min size of word to sort</p>
+
+            <select
+              name="minLength"
+              id="minLength"
+              className="select is-link"
+              value={minLength}
+              onChange={(event) => (
+                this.setState({
+                  minLength: +event.currentTarget.value,
+                })
+              )}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </>
+        )}
+      </div>
+    );
+  }
+}
