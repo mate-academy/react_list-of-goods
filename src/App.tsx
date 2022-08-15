4699b26e84019a904b7d3fc3cf43568dff717343
Roman Nikitenko <Lomkaaa@ukr.net>
2022-08-15 19:36:31 +0300

    Task complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './App.css';
 
 const goodsFromServer = [
@@ -15,6 +15,8 @@ const goodsFromServer = [
   'Garlic',
 ];
 
+const selectedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 enum SortType {
   NONE,
   ALPABET,
@@ -26,50 +28,157 @@ function getReorderedGoods(
   goods: string[],
   sortType: SortType,
   isReversed: boolean,
+  minLength: number,
 ) {
-  // Not to mutate the original array
   const visibleGoods = [...goods];
 
-  // Sort and reverse goods if needed
-  // ...
+  visibleGoods.sort((a, b) => {
+    switch (sortType) {
+      case 1:
+        return a[sortType].localeCompare(b[sortType]);
+      case 2:
+        return a.length - b.length;
+      default:
+        return 0;
+    }
+  });
 
-  return visibleGoods;
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
+
+  return visibleGoods.splice(0, minLength);
 }
 
-// DON'T save goods to the state
 type State = {
   isStarted: boolean,
   isReversed: boolean,
   sortType: SortType,
+  minLength: number,
 };
 
-export const App = () => (
-  <div className="App">
-    <button type="button">
-      Start
-    </button>
+export class App extends React.Component<{}, State> {
+  state = {
+    isReversed: false,
+    sortType: 0,
+    isStarted: false,
+    minLength: 1,
+  };
 
-    <button type="button">
-      Sort alphabetically
-    </button>
+  start = () => {
+    this.setState(state => ({
+      isStarted: !state.isStarted,
+    }));
+  };
 
-    <button type="button">
-      Sort by length
-    </button>
+  maxLengthGoods = (e: ChangeEvent<HTMLSelectElement>) => {
+    this.setState({ minLength: +e.currentTarget.value });
+  };
 
-    <button type="button">
-      Reverse
-    </button>
+  sortAlphabetically = () => {
+    this.setState({ sortType: 1 });
+  };
 
-    <button type="button">
-      Reset
-    </button>
+  sortByLength = () => {
+    this.setState({ sortType: 2 });
+  };
 
-    <ul className="Goods">
-      <li className="Goods__item">Dumplings</li>
-      <li className="Goods__item">Carrot</li>
-      <li className="Goods__item">Eggs</li>
-      <li className="Goods__item">...</li>
-    </ul>
-  </div>
-);
+  reset = () => {
+    this.setState({
+      isReversed: false,
+      sortType: 0,
+    });
+  };
+
+  reverse = () => {
+    this.setState(state => ({
+      isReversed: !state.isReversed,
+    }));
+  };
+
+  render() {
+    const {
+      isReversed,
+      sortType,
+      isStarted,
+      minLength,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          type="button"
+          className="button is-info"
+          onClick={this.start}
+        >
+          {isStarted ? 'End' : 'Start'}
+        </button>
+
+        {isStarted && (
+          <div className="Content">
+            <button
+              type="button"
+              className="button is-info"
+              onClick={this.sortAlphabetically}
+            >
+              Sort alphabetically
+            </button>
+
+            <button
+              type="button"
+              className="button is-info"
+              onClick={this.sortByLength}
+            >
+              Sort by length
+            </button>
+
+            <button
+              type="button"
+              className="button is-info"
+              onClick={this.reverse}
+            >
+              Reverse
+            </button>
+
+            <button
+              type="button"
+              className="button is-info"
+              onClick={this.reset}
+            >
+              Reset
+            </button>
+
+            <div className="select is-info">
+              <select onChange={(e) => this.maxLengthGoods(e)}>
+                {selectedNumbers.map((number) => (
+                  <option
+                    key={`selectNumber${number}`}
+                    value={number}
+                  >
+                    {number}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <ol className="Goods">
+              {getReorderedGoods(
+                goodsFromServer,
+                sortType,
+                isReversed,
+                minLength,
+              ).map(good => (
+                <li
+                  key={good}
+                  className="Goods__item"
+                >
+                  {good}
+                </li>
+              ))}
+            </ol>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
