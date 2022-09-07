fd930fb7ee031e36ae37fc6bdc37b6533a7e45bb
Kostya Konev <kosgoh.214@gmail.com>
2022-09-07 18:13:49 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { GoodsList } from './GoodsList';
+import './App.scss';
 
 const goodsFromServer: string[] = [
   'Dumplings',
@@ -14,11 +15,57 @@ const goodsFromServer: string[] = [
   'Garlic',
 ];
 
-const App: React.FC = () => (
-  <div className="App">
-    <h1>Goods</h1>
-    {goodsFromServer.length}
-  </div>
-);
+const App: React.FC = () => {
+  const [isShown, setIsShown] = useState(false);
+  const [goods, setGoods] = useState(goodsFromServer);
+  const [length, setLength] = useState('1');
+
+  const reverse = () => {
+    setGoods([...goods].reverse().filter(good => good.length >= +length));
+  };
+
+  const sort = () => {
+    setGoods([...goods]
+      .sort((a, b) => a.localeCompare(b))
+      .filter(good => good.length >= +length));
+  };
+
+  const reset = () => {
+    setGoods(goodsFromServer);
+    setLength('1');
+  };
+
+  const sortByLength = () => {
+    setGoods([...goods].sort((a, b) => b.length - a.length));
+  };
+
+  if (!isShown) {
+    return (
+      <div className="App">
+        <button
+          type="button"
+          className="button"
+          onClick={() => setIsShown(true)}
+        >
+          Start
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <GoodsList
+        goods={goods}
+        onReverse={reverse}
+        onSort={sort}
+        onReset={reset}
+        onSortByLength={sortByLength}
+        setLength={setLength}
+        length={length}
+      />
+    </div>
+  );
+};
 
 export default App;
