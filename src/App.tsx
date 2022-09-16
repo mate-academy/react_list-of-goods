fdf65185324ff7b1f46e8f06e0b1ca2ad6ab462a
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-09-16 12:15:02 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable react/no-unused-state */
 import { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 import classNames from 'classnames';
-// import { basename } from 'node:path/posix';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -29,15 +27,12 @@ type ReorderOptions = {
   isReversed: boolean,
 };
 
-// Use this function in the render to prepare goods
 export function getReorderedGoods(
   goods: string[],
   { sortType, isReversed }: ReorderOptions,
 ) {
-  // To avoid the original array mutation
   const visibleGoods = [...goods];
 
-  // eslint-disable-next-line array-callback-return
   visibleGoods.sort((good1, good2) => {
     switch (sortType) {
       case SortType.ALPABET:
@@ -59,7 +54,6 @@ export function getReorderedGoods(
     visibleGoods.reverse();
   }
 
-  // Sort and reverse goods if needed
   // eslint-disable-next-line no-console
   console.log(sortType, isReversed);
 
@@ -71,9 +65,7 @@ type State = {
   sortType: SortType,
 };
 
-type Props = {
-
-};
+type Props = {};
 
 export class App extends Component<Props, State> {
   state: State = {
@@ -81,21 +73,21 @@ export class App extends Component<Props, State> {
     sortType: SortType.NONE,
   };
 
-  sortByAlphabet = () => {
+  handleSortAlphabet = () => {
     this.setState({ sortType: SortType.ALPABET });
   };
 
-  sortByLength = () => {
+  handleSortLength = () => {
     this.setState({ sortType: SortType.LENGTH });
   };
 
-  revers = () => {
+  handleRevers = () => {
     this.setState(state => ({
       isReversed: !state.isReversed,
     }));
   };
 
-  reset = () => {
+  handleReset = () => {
     this.setState({ sortType: SortType.NONE, isReversed: false });
   };
 
@@ -111,27 +103,33 @@ export class App extends Component<Props, State> {
         <div className="buttons">
           <button
             type="button"
-            className={classNames('button is-info',
-              { 'button is-info is-light': sortType !== SortType.ALPABET })}
-            onClick={this.sortByAlphabet}
+            className={classNames(
+              'button is-info',
+              { 'button is-info is-light': sortType !== SortType.ALPABET },
+            )}
+            onClick={this.handleSortAlphabet}
           >
             Sort alphabetically
           </button>
 
           <button
             type="button"
-            className={classNames('button is-success',
-              { 'button is-success is-light': sortType !== SortType.LENGTH })}
-            onClick={this.sortByLength}
+            className={classNames(
+              'button is-success',
+              { 'button is-success is-light': sortType !== SortType.LENGTH },
+            )}
+            onClick={this.handleSortLength}
           >
             Sort by length
           </button>
 
           <button
             type="button"
-            className={classNames('button is-warning',
-              { 'button is-warning is-light': !isReversed })}
-            onClick={this.revers}
+            className={classNames(
+              'button is-warning',
+              { 'button is-warning is-light': !isReversed },
+            )}
+            onClick={this.handleRevers}
           >
             Reverse
           </button>
@@ -140,7 +138,7 @@ export class App extends Component<Props, State> {
             <button
               type="button"
               className="button is-danger is-light"
-              onClick={this.reset}
+              onClick={this.handleReset}
             >
               Reset
             </button>
