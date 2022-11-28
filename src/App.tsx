413625d171b804eb4c61cdbb5621b368afb846f3
Alex Tsimba <alexander.tsimba@gmail.com>
2022-11-28 19:22:24 +0200

    one more try to fix github-lint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
-import { ListOfGoods } from './components/ListOfGoods/ListOfGoods';
+import { ListOfGoods } from './ListOfGoods';
 
 export const goodsFromServer = [
   'Dumplings',
