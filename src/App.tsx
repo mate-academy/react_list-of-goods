1771939fcee1f67441c0086c46f278cb7fb44988
Diana Demkiv <dianademkiv789@gmail.com>
2023-01-09 02:11:21 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -33,18 +33,18 @@ export function getReorderedGoods(
 ) {
   const visibleGoods = [...goods];
 
-  visibleGoods.sort((good1: string, good2: string): number => {
-    switch (sortType) {
-      case SortType.ALPHABET:
-        return good1.localeCompare(good2);
+  switch (sortType) {
+    case SortType.ALPHABET:
+      visibleGoods.sort((good1, good2) => good1.localeCompare(good2));
+      break;
 
-      case SortType.LENGTH:
-        return good1.length - good2.length;
+    case SortType.LENGTH:
+      visibleGoods.sort((good1, good2) => good1.length - good2.length);
+      break;
 
-      default:
-        return 0;
-    }
-  });
+    default:
+      break;
+  }
 
   if (isReversed) {
     visibleGoods.reverse();
