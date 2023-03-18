dd12244e3527f918257f841f64a22806f3e1a1ef
Oksana Shvets <ksenyasvatko1993@gmail.com>
2023-03-18 21:51:16 +0100

    Solution1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 import classNames from 'classnames';
-import { GoodsList } from './components/GoodsList';
+import { GoodsList } from './components/GoodsList/GoodsList';
 
 export const goodsFromServer = [
   'Dumplings',
