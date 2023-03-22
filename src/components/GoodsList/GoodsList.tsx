b431c27972c499b2863e100fc6680293e73a4838
Maksym <maksymmaliuk98@gmail.com>
2023-03-22 17:17:33 +0200

    added components directory

---
+++ src/components/GoodsList/GoodsList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type Props = {
+  goods: string[]
+};
+
+export const GoodsList: React.FC<Props> = ({ goods }) => (
+  <ul>
+    {goods.map((good) => (
+      <li
+        key={good}
+        data-cy="Good"
+      >
+        {good}
+      </li>
+    ))}
+  </ul>
+);
