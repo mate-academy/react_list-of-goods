2830d28500096e26d510cd01af81f0c2b19368a1
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-08-09 14:38:44 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,9 @@ export const goodsFromServer = [
 ];
 
 enum SortType {
-  NONE = 'none',
-  ALPHABET = 'alphabet',
-  LENGTH = 'length',
+  NONE,
+  ALPHABET,
+  LENGTH,
 }
 
 type ReorderOptions = {
@@ -54,24 +54,21 @@ export class App extends React.PureComponent {
     return visibleGoods;
   };
 
+  sortAlphabet = () => {
+    this.setState({ sortType: SortType.ALPHABET });
+  };
+
+  sortLength = () => {
+    this.setState({ sortType: SortType.LENGTH });
+  };
+
+  reverse = () => {
+    this.setState({ isReversed: !this.state.isReversed });
+  };
+
   reset = () => {
     this.setState({ isReversed: false });
     this.setState({ sortType: SortType.NONE });
-    const alphabetButton = document.querySelector('.is-info');
-    const lengthButton = document.querySelector('.is-success');
-    const reverseButton = document.querySelector('.is-warning');
-
-    if (alphabetButton) {
-      alphabetButton.classList.add('is-light');
-    }
-
-    if (lengthButton) {
-      lengthButton.classList.add('is-light');
-    }
-
-    if (reverseButton) {
-      reverseButton.classList.add('is-light');
-    }
   };
 
   render() {
@@ -96,47 +93,30 @@ export class App extends React.PureComponent {
         <div className="buttons">
           <button
             type="button"
-            className="button is-info is-light"
-            onClick={event => {
-              this.setState({ sortType: SortType.ALPHABET });
-              const button = event.target as HTMLButtonElement;
-              const seccondButton = document.querySelector('.is-success');
-
-              button.classList.remove('is-light');
-              seccondButton?.classList.add('is-light');
-            }}
+            className={sortType === SortType.ALPHABET
+              ? 'button is-info'
+              : 'button is-info is-light'}
+            onClick={this.sortAlphabet}
           >
             Sort alphabetically
           </button>
 
           <button
             type="button"
-            className="button is-success is-light"
-            onClick={event => {
-              this.setState({ sortType: SortType.LENGTH });
-              const button = event.target as HTMLButtonElement;
-              const seccondButton = document.querySelector('.is-info');
-
-              button.classList.remove('is-light');
-              seccondButton?.classList.add('is-light');
-            }}
+            className={sortType === SortType.LENGTH
+              ? 'button is-success'
+              : 'button is-success is-light'}
+            onClick={this.sortLength}
           >
             Sort by length
           </button>
 
           <button
             type="button"
-            className="button is-warning is-light"
-            onClick={event => {
-              this.setState({ isReversed: !this.state.isReversed });
-              const button = event.target as HTMLButtonElement;
-
-              if (this.state.isReversed) {
-                button.classList.add('is-light');
-              } else {
-                button.classList.remove('is-light');
-              }
-            }}
+            className={isReversed === true
+              ? 'button is-warning'
+              : 'button is-warning is-light'}
+            onClick={this.reverse}
           >
             Reverse
           </button>
