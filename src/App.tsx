b955c1c5894ab1aa03e96c0a78885755d6de4f74
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-12-02 01:57:10 +0100

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -48,48 +48,112 @@ export function getReorderedGoods(
 // };
 
 export const App: React.FC = () => {
+  const [state, setState] = useState({
+    isReversed: false,
+    sortType: 'NONE',
+  });
+
+  let listOfGoods = [...goodsFromServer];
+
+  const sortAlphabetically = () => {
+    setState((prevState) => ({
+      ...prevState,
+      sortType: 'ALPHABET',
+    }));
+  };
+
+  const sortByLength = () => {
+    setState((prevState) => ({
+      ...prevState,
+      sortType: 'LENGTH',
+    }));
+  };
+
+  const reverseSort = () => {
+    setState((prevState) => ({
+      ...prevState,
+      isReversed: !prevState.isReversed,
+    }));
+  };
+
+  const reset = () => {
+    listOfGoods = [...goodsFromServer];
+    setState((prevState) => ({
+      ...prevState,
+      sortType: 'NONE',
+      isReversed: false,
+    }));
+  };
+
+  const addReset = () => {
+    for (let i = 0; i < goodsFromServer.length; i += 1) {
+      if (listOfGoods[i] !== goodsFromServer[i]) {
+        return (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        );
+      }
+    }
+
+    return (
+      <></>
+    );
+  };
+
+  if (state.sortType === 'NONE') {
+    listOfGoods = [...goodsFromServer];
+  } else if (state.sortType === 'ALPHABET') {
+    listOfGoods = [...goodsFromServer].sort();
+  } else if (state.sortType === 'LENGTH') {
+    listOfGoods = [...goodsFromServer]
+      .sort((el, el2) => (el.length - el2.length));
+  }
+
+  if (state.isReversed) {
+    listOfGoods = [...listOfGoods].reverse();
+  }
+
   return (
     <div className="section content">
       <div className="buttons">
         <button
           type="button"
-          className="button is-info is-light"
+          className={`button is-info ${state.sortType === 'ALPHABET' ? '' : 'is-light'}`}
+          onClick={sortAlphabetically}
         >
           Sort alphabetically
         </button>
 
         <button
           type="button"
-          className="button is-success is-light"
+          className={`button is-success ${state.sortType === 'LENGTH' ? '' : 'is-light'}`}
+          onClick={sortByLength}
         >
           Sort by length
         </button>
 
         <button
           type="button"
-          className="button is-warning is-light"
+          className={`button is-warning ${state.isReversed === true ? '' : 'is-light'}`}
+          onClick={reverseSort}
         >
           Reverse
         </button>
 
-        <button
-          type="button"
-          className="button is-danger is-light"
-        >
-          Reset
-        </button>
+        {addReset()}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {listOfGoods.map((element) => (
+          <li data-cy="Good" key={element}>{element}</li>
+        ))}
       </ul>
+
     </div>
   );
 };
