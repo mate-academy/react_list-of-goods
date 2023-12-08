26d0399cc167e12c308cc69daf5cb2fa58d19a3e
Kamil <anu88kl@gmail.com>
2023-12-08 16:00:57 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -34,7 +34,17 @@ export function getReorderedGoods(
   // To avoid the original array mutation
   const visibleGoods = [...goods];
 
+  if (sortType === SortType.ALPHABET) {
+    visibleGoods.sort();
+  } else if (sortType === SortType.LENGTH) {
+    visibleGoods.sort((a, b) => a.length - b.length);
+  }
+
+  if (isReversed) {
+    visibleGoods.reverse();
+  }
   // Sort and reverse goods if needed
+
   // eslint-disable-next-line no-console
   console.log(sortType, isReversed);
 
@@ -47,49 +57,89 @@ export function getReorderedGoods(
 //   sortType: SortType,
 // };
 
-export const App: React.FC = () => {
+export const App = () => {
+  const [reorderOptions, setReorderOptions] = useState<ReorderOptions>({
+    sortType: SortType.NONE,
+    isReversed: false,
+  });
+
+  const handleSortAlphabetically = () => {
+    setReorderOptions({
+      ...reorderOptions,
+      sortType: SortType.ALPHABET,
+    });
+  };
+
+  const handleSortByLength = () => {
+    setReorderOptions({
+      ...reorderOptions,
+      sortType: SortType.LENGTH,
+    });
+  };
+
+  const handleReverse = () => {
+    setReorderOptions({
+      ...reorderOptions,
+      isReversed: !reorderOptions.isReversed,
+    });
+  };
+
+  const handleReset = () => {
+    setReorderOptions({
+      sortType: SortType.NONE,
+      isReversed: false,
+    });
+  };
+
+  const visibleGoods = getReorderedGoods(goodsFromServer, reorderOptions);
+  const alphabetButtonClass
+  = reorderOptions.sortType
+  === SortType.ALPHABET ? 'button is-info' : 'button is-info is-light';
+  const reverseButtonClass
+  = reorderOptions.isReversed
+    ? 'button is-warning' : 'button is-warning is-light';
+  const lengthButtonClass
+  = reorderOptions.sortType
+  === SortType.LENGTH ? 'button is-success' : 'button is-success is-light'
+
   return (
-    <div className="section content">
-      <div className="buttons">
+    <div>
+      <button
+        type="button"
+        onClick={handleSortAlphabetically}
+        className={alphabetButtonClass}
+      >
+        Sort alphabetically
+      </button>
+      <button
+        type="button"
+        onClick={handleSortByLength}
+        className={lengthButtonClass}
+      >
+        Sort by length
+      </button>
+      <button
+        type="button"
+        onClick={handleReverse}
+        className={reverseButtonClass}
+      >
+        Reverse
+      </button>
+      {(reorderOptions.sortType
+      !== SortType.NONE || reorderOptions.isReversed) && (
         <button
           type="button"
-          className="button is-info is-light"
-        >
-          Sort alphabetically
-        </button>
-
-        <button
-          type="button"
-          className="button is-success is-light"
-        >
-          Sort by length
-        </button>
-
-        <button
-          type="button"
-          className="button is-warning is-light"
-        >
-          Reverse
-        </button>
-
-        <button
-          type="button"
+          onClick={handleReset}
           className="button is-danger is-light"
         >
           Reset
         </button>
-      </div>
-
+      )}
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {visibleGoods.map(good => <li key={good} data-cy="Good">{good}</li>)}
       </ul>
     </div>
   );
 };
+
+export default App;
