7227c02b15dc8915e8448a7e1152d0d12fdc9d66
LabPetro <lamppetro30@outlook.com>
2024-04-01 15:30:08 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import cn from 'classnames';
 import React from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { GoodsList } from './components/GoodsList';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -31,7 +33,6 @@ export function getReorderedGoods(
   goods: string[],
   { sortType, isReversed }: ReorderOptions,
 ) {
-  // To avoid the original array mutation
   const visibleGoods = [...goods];
 
   // Sort and reverse goods if needed
@@ -47,37 +48,122 @@ export function getReorderedGoods(
 //   sortType: SortType,
 // };
 
-export const App: React.FC = () => {
-  return (
-    <div className="section content">
-      <div className="buttons">
-        <button type="button" className="button is-info is-light">
-          Sort alphabetically
-        </button>
-
-        <button type="button" className="button is-success is-light">
-          Sort by length
-        </button>
-
-        <button type="button" className="button is-warning is-light">
-          Reverse
-        </button>
-
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
-      </div>
-
-      <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
-      </ul>
-    </div>
-  );
+type State = {
+  goods: string[];
+  originalGoods: string[];
 };
+
+export class App extends React.Component<{}, State> {
+  state = { goods: goodsFromServer, originalGoods: goodsFromServer.slice() };
+
+  sortAlphabetical = () => {
+    const newGoods = [...goodsFromServer];
+
+    newGoods.sort((g1: string, g2: string) => {
+      return g1.localeCompare(g2);
+    });
+    this.setState({ goods: newGoods });
+  };
+
+  sortLength = () => {
+    const lengthGoods = [...goodsFromServer];
+
+    lengthGoods.sort((a, b) => {
+      return a.length - b.length;
+    });
+
+    this.setState({ goods: lengthGoods });
+  };
+
+  sortReverse = () => {
+    const reverseGoods = [...this.state.goods];
+
+    reverseGoods.reverse();
+
+    this.setState({ goods: reverseGoods });
+  };
+
+  sortCustom = (opts: ReorderOptions) => {
+    if (opts.sortType === SortType.ALPHABET) {
+      this.sortAlphabetical();
+    } else if (opts.sortType === SortType.LENGTH) {
+      this.sortLength();
+    } else if (opts.isReversed) {
+      this.sortReverse();
+    } else {
+      const goodsReturned = getReorderedGoods(goodsFromServer, opts);
+
+      this.setState({ goods: goodsReturned });
+    }
+  };
+
+  sortReset = () => {
+    this.setState({ goods: this.state.originalGoods });
+  };
+
+  render() {
+    const { goods, originalGoods } = this.state;
+    const isOriginal = JSON.stringify(goods) === JSON.stringify(originalGoods);
+
+    return (
+      <div className="section content">
+        <div className="buttons">
+          <button
+            type="button"
+            className={cn('button', {
+              'is-info': true,
+              'is-light': isOriginal,
+            })}
+            onClick={() => {
+              const opts = { sortType: SortType.ALPHABET, isReversed: false };
+
+              this.sortCustom(opts);
+            }}
+          >
+            Sort alphabetically
+          </button>
+
+          <button
+            type="button"
+            className={cn('button', {
+              'is-success': true,
+              'is-light': isOriginal,
+            })}
+            onClick={() => {
+              const opts = { sortType: SortType.LENGTH, isReversed: false };
+
+              this.sortCustom(opts);
+            }}
+          >
+            Sort by length
+          </button>
+
+          <button
+            type="button"
+            className={cn('button', {
+              'is-warning': true,
+              'is-light': isOriginal,
+            })}
+            onClick={() => {
+              const opts = { sortType: SortType.NONE, isReversed: true };
+
+              this.sortCustom(opts);
+            }}
+          >
+            {isOriginal ? 'Reverse' : 'Reset'}
+          </button>
+          {!isOriginal && (
+            <button
+              type="button"
+              className="button is-danger"
+              onClick={this.sortReset}
+            >
+              Reset
+            </button>
+          )}
+        </div>
+        <GoodsList goodsList={this.state.goods} />
+      </div>
+    );
+  }
+}
